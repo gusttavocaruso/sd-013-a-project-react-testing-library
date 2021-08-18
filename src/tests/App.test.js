8aa0f2a18d14995d65d1092fa9157ce3ee49017c
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-08-18 18:42:13 -0300

    Acho que o requisito 01 foi atendido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Verifica App.js ', () => {
+  test('Verifica se exite um link para pagina Home', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+
+    );
+    const linkToHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkToHome).toBeInTheDocument();
+    userEvent.click(linkToHome);
+  });
+
+  test('Verifica se exite um link para pagina About', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+
+    );
+    const linkToAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkToAbout).toBeInTheDocument();
+    userEvent.click(linkToAbout);
+  });
+
+  test('Verifica se exite um link para pagina Favorite Pokémons', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+
+    );
+    const linkFavoritePokémons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavoritePokémons).toBeInTheDocument();
+    userEvent.click(linkFavoritePokémons);
+  });
+});
