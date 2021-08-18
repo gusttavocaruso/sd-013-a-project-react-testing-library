0f74c5a703ba0b36b032b6abd0390723523301ce
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:18:32 -0300

    Estruturando os describes no requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -5,3 +5,7 @@ import renderWithRouter from './renderWithRouter';
 
 describe('Requisito 04 - Teste o componente NotFound.js', () => {
   test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
+    // Renderizo na tela as rotas do componente NotFound
+    renderWithRouter(<NotFound />);
+    // getByRole um elemento 'heading'(h1/h2/h3/h4...) busca um título, ou qualquer nível de título, com o name de "Page requested not found".
+    const message = screen.getByRole('heading', { name: /Page requested not found/i });
