0b58ff644720e6d101a6787c875b9687f451d78d
Cassyel <cassyel1967@gmail.com>
2021-08-18 15:50:12 -0300

    Realiza Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o Componente <FavoritePokemons.js />.', () => {
+  describe('Testa implementação da página".', () => {
+    it('Test ase é exibido "No favorite Pokemon found".', () => {
+      render(<FavoritePokemons />);
+
+      const favoritePokemons = screen.getByText(/no favorite pokemon/i);
+      expect(favoritePokemons).toBeInTheDocument();
+    });
+  });
+});
