a2f97e89e8a4c534527947f2ea8ccf139cc0b498
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-18 16:32:37 -0300

    Req 1 parte 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,22 @@
-test('', () => {});
-// kjkj
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('App.js tests', () => {
+  test('Renderiza conjunto de links', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const linkHome = screen.getByText('Home');
+    const linkAbout = screen.getByText('About');
+    const linkFavorite = screen.getByText('Favorite Pokémons');
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+});
