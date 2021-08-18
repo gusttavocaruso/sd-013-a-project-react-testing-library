f2f9ce07b8f739560d2fdd9896d3617dd3450fd5
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:18:04 -0300

    Efetuando comentários no requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,3 +2,6 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 04 - Teste o componente NotFound.js', () => {
+  test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
