3e9b276f75e20d069232f035d20f4995e4d2be72
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-08-18 20:42:08 -0300

    Os links devem possuir o texto Home, About e Favorite Pokemons

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
+
+/*  Passos a verificar com testes em React:
+  1 - Acessar os Elementos renderizados na tela;
+  2 - Interagir com eles caso haja necessidade;
+  3 - Fazer o teste; */
+
+describe('Teste se o topo da aplicação contém links de navegação.', () => {
+  test('Os links devem possuir o texto Home, About e Favorite Pokemons', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /Home/ });
+    expect(home).toBeInTheDocument();
+    const about = screen.getByRole('link', { name: /About/ });
+    expect(about).toBeInTheDocument();
+    const favoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/ });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
