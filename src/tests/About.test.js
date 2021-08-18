da62137e7cce016fa09d399bc20139cfd8498fba
sonkai <mfilho86@gmail.com>
2021-08-18 18:07:12 -0300

    req 2 finalmente

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,21 @@
-test('', () => {});
-// vamo continuar sem o evaluator
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste do About', () => {
+  test('Teste', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/about');
+    const aboutH = screen.getByRole('heading', { name: 'About Pokédex' });
+    const aboutP1 = screen.getByText(/This application/i);
+    const aboutP2 = screen.getByText(/Pokémons by type/i);
+    const aboutImg = screen.getByRole('img', { name: 'Pokédex' });
+
+    expect(aboutH).toBeInTheDocument();
+    expect(aboutP1).toBeInTheDocument();
+    expect(aboutP2).toBeInTheDocument();
+    expect(aboutImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
