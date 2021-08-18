3774db7e8626e1f2bf29bace82395c03be6c46c3
Cássio Pereira <cassiorodp@gmail.com>
2021-08-18 15:22:28 -0300

    Requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test notFound page', () => {
+  it('should have a heading text', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/non-valid-url');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('should have a image text', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/non-valid-url');
+
+    const notFoundImg = screen.getByRole('img', { name: /pikachu crying/i });
+    const { src } = notFoundImg;
+
+    expect(src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
