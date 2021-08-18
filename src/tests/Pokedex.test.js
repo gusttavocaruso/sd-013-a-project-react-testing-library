b480c3ffd8f561d0777ed3ae9f389d6ac38cbb89
sonkai <mfilho86@gmail.com>
2021-08-18 20:47:45 -0300

    req 5 - teste 1 - teste do h2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('teste do Pokedex', () => {
+  test('Teste do h2 com Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const header2 = screen.getByRole('heading', { name: /Encountered/i });
+
+    expect(header2).toBeInTheDocument();
+  });
+});
