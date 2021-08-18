6e0a7f82477e76d03a470e94198002fb4cdfe039
Julia Baptista <baptista.julia@yahoo.com.br>
2021-08-18 19:26:38 -0300

    Quesito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+// import NotFound from '../components/NotFound';
+
+describe('Teste o componente Not Found', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const noMatchText = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(noMatchText).toBeInTheDocument();
+  });
+  it('Teste se página de testes mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const noMatchImg = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(noMatchImg.src).toBe(src);
+  });
+});
