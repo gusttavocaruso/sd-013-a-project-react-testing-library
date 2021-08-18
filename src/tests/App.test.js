f0404808f70f6fd0a4ffbbacf171bbc0fe8d69dd
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-08-18 16:54:05 -0300

    Adiciona Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,95 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('App.js tests', () => {
+  test('O topo da aplicação possui um conjuto fixo de links em ordem', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const homeLink = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritePokemons = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  test('Aplicação redireciona p/ a página inicial ao clicar "Home"', () => {
+    const historyMock = createMemoryHistory();
+
+    render(
+      <Router history={ historyMock }>
+        <App />
+      </Router>,
+    );
+
+    historyMock.push('/');
+
+    const renderizaHome = screen.getByRole('heading', {
+      name: /pokédex/i,
+    });
+    expect(renderizaHome).toBeInTheDocument();
+  });
+
+  test('Aplicação é redirecionada para URL/about ao clicar "About"', () => {
+    const historyMock = createMemoryHistory();
+
+    render(
+      <Router history={ historyMock }>
+        <App />
+      </Router>,
+    );
+
+    historyMock.push('/about');
+
+    const renderizaAbout = screen.getByRole('img');
+    expect(renderizaAbout).toBeInTheDocument();
+  });
+
+  test('Aplicação redireciona p/ URL/favorites ao clicar "Favorites Pokémons"', () => {
+    const historyMock = createMemoryHistory();
+
+    render(
+      <Router history={ historyMock }>
+        <App />
+      </Router>,
+    );
+
+    historyMock.push('/favorites');
+
+    const renderizaFavorites = screen.getByRole('heading', {
+      name: /favorite pokémons/i,
+    });
+    expect(renderizaFavorites).toBeInTheDocument();
+  });
+
+  test('Aplicação redireciona para a página Not Found com URL desconhecida', () => {
+    const historyMock = createMemoryHistory();
+
+    render(
+      <Router history={ historyMock }>
+        <App />
+      </Router>,
+    );
+
+    historyMock.push('/rota-desconhecida');
+
+    const renderizaFavorites = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(renderizaFavorites).toBeInTheDocument();
+  });
+});
