f7426fd4d4d7773cdd4014ec843ea4b7859a7a16
ALINE <hoshinoaline@yahoo.com.br>
2021-08-18 19:58:36 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do notFound', () => {
+  it('Página possui um h2 com o texto Page requested not found ', () => {
+    render(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      name: /Page requested not found /,
+      level: 2,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('testa se tem imagem', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
