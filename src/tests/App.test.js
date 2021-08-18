0591297137a70d3009d1eb0236cb9149a97ef69d
sonkai <mfilho86@gmail.com>
2021-08-18 16:45:07 -0300

    sera que é por ter um arquivo que não é de test na pasta test?

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, screen } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Test do App', () => {
