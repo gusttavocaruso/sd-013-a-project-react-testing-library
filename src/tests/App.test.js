a8e8082d0c17d46f8d55a08d748e4b6db535edf4
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-08-18 16:34:53 -0300

    passando no stryker localmente

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -17,43 +17,39 @@ describe('Testa o componente App.js', () => {
       expect(favoritePokemon).toBeInTheDocument();
     });
 
-  it(`Testa se a aplicação é redirecionada para a página inicial,
-    na URL / ao clicar no link Home da barra de navegação.`,
-  () => {
-    const { history } = renderWithRouter(<App />);
-    const home = screen.getByText(/Home/i);
+  it('Testa se a aplicação é redirecionada para a página inicial.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const home = screen.getByText(/Home/i);
 
-    userEvent.click(home);
-    expect(history.location.pathname).toBe('/');
-  });
+      userEvent.click(home);
+      expect(history.location.pathname).toBe('/');
+    });
 
-  it(`Testa se a aplicação é redirecionada para a página de About,
-  na URL /about, ao clicar no link About da barra de navegação.`,
-  () => {
-    const { history } = renderWithRouter(<App />);
-    const about = screen.getByText(/About/i);
+  it('Testa se a aplicação é redirecionada para a página de About.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const about = screen.getByText(/About/i);
 
-    userEvent.click(about);
-    expect(history.location.pathname).toBe('/about');
-  });
+      userEvent.click(about);
+      expect(history.location.pathname).toBe('/about');
+    });
 
-  it(`Testa se a aplicação é redirecionada para a página de Pokémons Favoritados,
-  na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`,
-  () => {
-    const { history } = renderWithRouter(<App />);
-    const favoritePokemons = screen.getByText(/Favorite Pokémons/i);
+  it('Testa se a aplicação é redirecionada para a página de Pokémons Favoritados.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const favoritePokemons = screen.getByText(/Favorite Pokémons/i);
 
-    userEvent.click(favoritePokemons);
-    expect(history.location.pathname).toBe('/favorites');
-  });
+      userEvent.click(favoritePokemons);
+      expect(history.location.pathname).toBe('/favorites');
+    });
 
-  it(`Teste se a aplicação é redirecionada para a página Not Found
-  ao entrar em uma URL desconhecida.`,
-  async () => {
-    const { history } = renderWithRouter(<App />);
+  it('Testa se a aplicação é redirecionada para a página Not Found.',
+    async () => {
+      const { history } = renderWithRouter(<App />);
 
-    history.push('/any');
-    const notFound = screen.getByTestId('not-found-test');
-    expect(notFound).toBeInTheDocument();
-  });
+      history.push('/any');
+      const notFound = screen.getByText(/Page requested not found/i);
+      expect(notFound).toBeInTheDocument();
+    });
 });
