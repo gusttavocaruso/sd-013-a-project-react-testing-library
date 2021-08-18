e9efcce9281c5c4a8df70813b4eecdbd93a1b3ae
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:07:36 -0300

    Efetuando comentários no requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 03 - Teste o componente <FavoritePokemons.js />', () => {
+  test('Testa se é exibido "No favorite pokemon found", se não tiver pokémons favoritos',
+    () => {
