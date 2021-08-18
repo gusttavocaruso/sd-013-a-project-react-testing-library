27445011a870ac313300d269ef2d014650aee872
sonkai <mfilho86@gmail.com>
2021-08-18 15:58:53 -0300

    req 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,34 @@
-test('', () => {});
-// bora la
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test do App', () => {
+  test('Tesde dos links', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemon).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Teste dos clicks', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favoritePokemon = screen.getByText('Favorite Pokémons');
+
+    fireEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+
+    fireEvent.click(favoritePokemon);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
