2535db4b6e09c37f6fe2ddaa9faf84e7242f47d9
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-18 16:14:44 -0300

    Iniciando Project Req1 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+// import { render, screen } from '@testing-library/react';
+// import About from '../components/About';
+
+// test('A página contém as informações sobre a Pokédex', () => {
+//   render(<About />);
+
+//   const info = screen.get;
+// });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
 
 it('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
   render(
@@ -19,4 +21,51 @@ it('O topo da aplicação contém um conjunto fixo de links de navegação.', ()
   expect(navHome).toBeInTheDocument(/home/i);
   expect(navAbout).toBeInTheDocument(/about/i);
   expect(navFav).toBeInTheDocument(/favorite pokémons/i);
+});
+
+it('A aplicação é redirecionada para a página inicial', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const navHome = screen.getByRole('link', {
+    name: /home/i,
+  });
+  userEvent.click(navHome);
+  expect(navHome).toBeInTheDocument();
+  expect(history.location.pathname).toBe('/');
+});
+
+it('A aplicação é redirecionada para a página de About', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const navAbout = screen.getByRole('link', {
+    name: /About/i,
+  });
+  userEvent.click(navAbout);
+  expect(navAbout).toBeInTheDocument();
+  expect(history.location.pathname).toBe('/about');
+});
+
+it('A aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const navFav = screen.getByRole('link', {
+    name: /favorite pokémons/i,
+  });
+  userEvent.click(navFav);
+  expect(navFav).toBeInTheDocument();
+  expect(history.location.pathname).toBe('/favorites');
+});
+
+it('A aplicação é redirecionada para a página Not Found', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/rota-que-nao-existe');
+
+  const notFound = screen.getByRole('heading', {
+    name: /Page requested not found /i,
+    level: 2,
+  });
+
+  expect(history.location.pathname).toBe('/rota-que-nao-existe');
+  expect(notFound).toBeInTheDocument();
 });
