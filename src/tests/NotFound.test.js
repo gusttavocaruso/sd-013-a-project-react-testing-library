852074d2f6a375ac48b45c5675c20f12ed166b8e
Debora Teodorico <debora.steodorico@gmail.com>
2021-08-18 18:36:04 -0300

    Adicionando a quarta questao

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import NotFound from '../components/NotFound';
+// import pokemonsData from '../data';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Verifica se página tem um h2 com o texto "Page requested not found 😭"', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/NotFound/');
+    const notFound = screen.getByText(/Page requested not found/i);
+    const emoji = screen.getByText('😭');
+    expect(notFound).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
+  });
+  it('Verifica se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const frase = 'Pikachu crying because the page requested was not found';
+    const srcAlt = screen.getByAltText(frase);
+    expect(srcAlt).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
