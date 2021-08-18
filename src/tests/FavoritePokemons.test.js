a6f3e0b403fcff75967f3f1c8125a2e8fd0f9fc4
Debora Teodorico <debora.steodorico@gmail.com>
2021-08-18 17:53:35 -0300

    Adicionando a terceiraa questao

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemonsData from '../data';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Verifica se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    const pokemons = pokemonsData;
+
+    renderWithRouter(
+      <FavoritePokemons
+        pokemons={ pokemons }
+      />,
+    );
+
+    pokemons.forEach((pokemon) => {
+      const card = screen.getByText(pokemon.name);
+
+      expect(card).toBeInTheDocument();
+    });
+  });
+});
