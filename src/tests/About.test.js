9b870389906adf97ae6b5ef77b2a5f2cf83ad4d3
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 13:42:36 -0300

    Iniciando requisito 01

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,3 +4,7 @@ import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
 // Guarda na variável pokemons a relação de pokemons do data.js
+import pokemons from '../data';
+
+// Guarda no firstPokemon o primeiro pokemon do array
+const firstPokemon = pokemons[0];
