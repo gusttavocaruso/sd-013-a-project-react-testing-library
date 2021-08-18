ac18fc8b9fb627fdb03c68b5cd4fd0d55b7259e2
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-18 15:11:15 -0300

    Iniciando Project Req1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+it('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const navHome = screen.getByText('Home');
+
+  const navAbout = screen.getByText('About');
+
+  const navFav = screen.getByText('Favorite Pokémons');
+
+  expect(navHome).toBeInTheDocument(/home/i);
+  expect(navAbout).toBeInTheDocument(/about/i);
+  expect(navFav).toBeInTheDocument(/favorite pokémons/i);
+});
