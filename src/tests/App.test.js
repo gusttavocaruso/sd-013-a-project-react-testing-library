e17076ddf4222fb2fc13fc6381b9e5ccb0d9c0dc
Wesley Maia <maia.weol@gmail.com>
2021-08-18 17:04:59 -0300

    [LINT] correção requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -16,39 +16,39 @@ describe('Testar o App', () => {
   });
 
   test('Teste o redirecionamento para a Home.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/Home/i));
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Home/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/');
-    const aboutAll = getByText(/Encountered pokémons/);
+    const aboutAll = screen.getByText(/Encountered pokémons/);
     expect(aboutAll).toBeInTheDocument();
   });
 
   test('Teste o redirecionamento para o About.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText(/About/i));
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/about');
-    const aboutAll = getByText(/About Pokédex/);
+    const aboutAll = screen.getByText(/About Pokédex/);
     expect(aboutAll).toBeInTheDocument();
   });
 
   test('Teste o redirecionamento para o Favorite Pokémons.', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
-    fireEvent.click(getByText(/Favorite pokémons/i));
+    fireEvent.click(screen.getByText(/Favorite pokémons/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/favorites');
-    const aboutAll = getByText(/Favorite pokémons/);
+    const aboutAll = screen.getByText(/Favorite pokémons/);
     expect(aboutAll).toBeInTheDocument();
   });
 
   test('Renderiza mensagem de "Página não encontrada"', () => {
-    const { getByRole, history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
     history.push('/rota-que-nao-existe');
 
-    const notFoundText = getByRole('heading', {
+    const notFoundText = screen.getByRole('heading', {
       name: /Page requested not found/i,
       level: 2,
     });
