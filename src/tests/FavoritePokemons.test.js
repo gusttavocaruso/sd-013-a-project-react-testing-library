f2397eddd4b4f853b683bb7fb84e9631ebfd5d0a
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-18 16:04:58 -0300

    Resolve requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testa o componente "FavoritePokemons"', () => {
+  it('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFoundFavorites = screen.getByText(/no favorite pokemon found/i);
+
+    expect(notFoundFavorites).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards dos pokemons favoritos', () => {
+    renderWithRouter(<App />);
+
+    const linkMoreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+
+    userEvent.click(linkMoreDetails);
+
+    const checkboxFavorite = screen.getByRole('checkbox');
+    const favoritePokemonLink = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+
+    userEvent.click(checkboxFavorite);
+    userEvent.click(favoritePokemonLink);
+
+    const cardText = screen.getByText(/electric/i);
+
+    expect(cardText).toBeInTheDocument();
+  });
+});
