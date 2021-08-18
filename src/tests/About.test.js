c450f535e355d02366b9933f14df517ba5bc56b0
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 13:58:27 -0300

    Corrigindo importes dos requisitos 01 e 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,12 +3,6 @@ import { screen } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
-// Guarda na variável pokemons a relação de pokemons do data.js
-import pokemons from '../data';
-
-// Guarda no firstPokemon o primeiro pokemon do array
-const firstPokemon = pokemons[0];
-
 describe('Requisito 02 - Testa o componente About.js', () => {
   test('Teste se a página contém as informações sobre a Pokédex', () => {
     // Renderizo na tela as rotas do componente About
