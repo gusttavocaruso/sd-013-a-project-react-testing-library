ef41ce17d83b8aedb7e0b49f355aeb3135afab08
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-18 15:34:21 -0300

    Testando o primeiro item

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o component App', () => {
+  renderWithRouter(<App />);
+  it('O primeiro link deve possuir o texto "Home"', () => {
+    const linkHome = screen.getByText('Home');
+    expect(linkHome).toBeInTheDocument();
+  });
+  it('O segundo link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('About');
+    expect(linkHome).toBeInTheDocument();
+  });
+  it('O segundo link deve possuir o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('Favorite Pokémons');
+    expect(linkHome).toBeInTheDocument();
+  });
+});
