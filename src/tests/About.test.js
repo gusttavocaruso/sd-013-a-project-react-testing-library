739291ce453838816f9b70562cbdf87f7c6718ed
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-08-18 17:22:32 -0300

    cumpre requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente About.js', () => {
+  beforeEach(() => renderWithRouter(<About />));
+
+  it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const heading = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const paragraphs = screen.getAllByTestId('paragraph-test');
+    expect(paragraphs.length).toEqual(2);
+  });
+
+  it('Teste se a página contém a imagem específica de uma Pokédex.', () => {
+    const image = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image).toHaveAttribute('src', src);
+  });
+});
