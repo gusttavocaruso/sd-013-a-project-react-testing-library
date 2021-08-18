ac6ef806c7ab53f00b8ac612155ee92dc1452bf6
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:08:23 -0300

    Estruturando os describes no requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,3 +7,9 @@ import renderWithRouter from './renderWithRouter';
 describe('Requisito 03 - Teste o componente <FavoritePokemons.js />', () => {
   test('Testa se é exibido "No favorite pokemon found", se não tiver pokémons favoritos',
     () => {
+      // Desconstroi o history do renderWithRouter do componente App
+      const { history } = renderWithRouter(<App />);
+      // Busca um elemento link com o endereço "/favorite pokémons" e guarda na variável favoritePokemons
+      const favoritePokemons = screen.getByRole('link',
+        { name: /favorite pokémons/i });
+      
