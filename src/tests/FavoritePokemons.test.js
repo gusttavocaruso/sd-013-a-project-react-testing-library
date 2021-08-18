80a82fc4c476f91b48224854cdb405949820a244
Felipe Lima <felipe_lima10@icloud.com>
2021-08-18 17:15:17 -0300

    3. Teste o componente `<FavoritePokemons.js />`

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  it('will test if the page when dont have a favorite pokemon', () => {
+    render(<FavoritePokemons />);
+
+    const titleFav = screen.getByText('No favorite pokemon found');
+    expect(titleFav).toBeInTheDocument();
+  });
+
+  it('will test if the page have a image from a pokedex', () => {
+    renderWithRouter(<App />);
+
+    userEvent.click(screen.getByRole('link', {
+      name: 'More details',
+    }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    const namePokemon = screen.getByTestId('pokemon-name');
+
+    expect(namePokemon).toBeInTheDocument();
+  });
+});
