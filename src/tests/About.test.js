41a9f0453315bfb5100ef39f1fb9281d01dc4ced
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-18 16:46:39 -0300

    Req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,8 +1,30 @@
-// import { render, screen } from '@testing-library/react';
-// import About from '../components/About';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
 
-// test('A página contém as informações sobre a Pokédex', () => {
-//   render(<About />);
+test('A página contém um heading h2', () => {
+  render(<About />);
 
-//   const info = screen.get;
-// });
+  const info = screen.getByRole('heading', {
+    name: 'About Pokédex',
+    level: 2,
+  });
+  expect(info).toBeInTheDocument();
+});
+
+test('A página contém dois parágrafos', () => {
+  render(<About />);
+
+  const textParam = screen.getByText(/This application simulates a Pokédex/);
+  expect(textParam).toBeInTheDocument();
+
+  const textParam2 = screen.getByText(/One can filter Pokémons by type/);
+  expect(textParam2).toBeInTheDocument();
+});
+
+test('A página contém a seguinte imagem de uma Pokédex:', () => {
+  render(<About />);
+
+  const img = screen.getByRole('img');
+  expect(img.src).toEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
