7b96fb05ecbe0e202a4305d3ad3b7871ca275816
Murilo Rainho <murilorainho01@gmail.com>
2021-08-18 15:23:25 -0300

    requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 
 import App from '../App';
 
-describe('', () => {
-  it('should ', () => {});
+describe('Testing \'NotFound\' component', () => {
+  it('should render \'a not found page\' with an \'h2\'', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/oksdakosdakodkodsakosadko');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('should render \'a not found page\' with an image', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/oksdakosdakodkodsakosadko');
+
+    const notFoundImage = screen.getByRole('img', { name: /pikachu crying/i });
+
+    // Sugestão do Cássio da turma 13 - a
+    const { src } = notFoundImage;
+
+    expect(src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
