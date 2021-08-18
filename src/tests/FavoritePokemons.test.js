43a325b63d7ca34a8ae657e4bdf3ae84f7f4da18
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-18 18:33:40 -0300

    Req3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWhithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Requisito 3', () => {
+  test('É exibido na tela a mensagem "No favorite pokemon found"?', () => {
+    renderWhithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test('É exibido todos os cards de pokémons favoritados.', () => {
+    renderWhithRouter(<App />);
+
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+
+    const textFavorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(textFavorite);
+
+    const favoritePokemon = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(favoritePokemon);
+
+    const card = screen.getByTestId('pokemon-name');
+    expect(card).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { render } from 'react-dom';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Requisito 4', () => {
+  test('A página contém um heading "h2"', () => {
+    renderWithRouter(<NotFound />);
+
+    const header = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+
+    expect(header).toBeInTheDocument();
+  });
+
+  it('A página mostra imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(img.src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
