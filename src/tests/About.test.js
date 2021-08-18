8e2a6010829a7efa1e649fff4407ac3d59fe7b06
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-08-18 17:38:08 -0300

    Requirement 2 finished

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { About } from '../components';
 import renderWithRouter from './renderWithRouter';
 
 describe('Test "About" page', () => {
-  it('Renders pokedex informations', () => {
-    renderWithRouter(<About />);
+  beforeEach(() => renderWithRouter(<About />));
 
+  it('Renders pokedex informations', () => {
     const pokedexText = screen.getByRole('heading', { level: 2 });
     expect(pokedexText).toHaveTextContent('About Pokédex');
   });
 
   it('Renders two paragraphs about pokedex', () => {
-    renderWithRouter(<About />);
-
     const firstParagraph = screen.getByText((content) => content.startsWith('This app'));
     expect(firstParagraph).toBeInTheDocument();
     expect(firstParagraph).toHaveTextContent('encyclopedia containing all Pokémons');
@@ -25,8 +22,6 @@ describe('Test "About" page', () => {
   });
 
   it('Renders an img element', () => {
-    renderWithRouter(<About />);
-
     const notFoundImg = screen.getByAltText('Pokédex');
     expect(notFoundImg).toBeInTheDocument();
     expect(notFoundImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test "FavoritePokemons" page', () => {
+  it('Renders a text if there is no pokemon favorited', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavPokeText = screen.getByText('No favorite pokemon found');
+    expect(noFavPokeText).toBeInTheDocument();
+  });
+
+  it('Renders favorited pokemons', () => {
+    renderWithRouter(<App />);
+
+    const moreDetailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsLink);
+
+    const favCheck = screen.getByRole('checkbox');
+    userEvent.click(favCheck);
+
+    const favLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favLink);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    expect(pokeName).toBeInTheDocument();
+  });
+});
