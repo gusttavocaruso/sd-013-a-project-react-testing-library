d76a3cf3a11c267af186c2fd6e0dbce4dcfcff66
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 15:50:26 -0300

    Corrigindo lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 // npx stryker run ./stryker/PokemonDetails.conf.json para confirmar que os testes foram realizados em 100%
 
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
