c20e31ea94a0192d1d63ced44dea9e99383dcb47
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:39:12 -0300

    Finalizando primeiro teste no requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -12,3 +12,15 @@ const nameTestId = 'pokemon-name';
 const typeTestId = 'pokemon-type';
 const weightTestId = 'pokemon-weight';
 const firstPokemon = pokemons[0]; // Pega o primeiro pokemon
+
+describe('Requisito 06 - Testa o componente <Pokemon.js />', () => {
+  describe('Teste se é renderizado um card com as informações de determinado pokémon',
+    () => {
+      test('O nome correto do Pokémon deve ser mostrado na tela', () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        // Guarda na variável pokemonName o elemento datatestId com 'pokemon-name'
+        const pokemonName = screen.getByTestId(nameTestId);
+        // Testo se o elemento que está na variável pokemonName possui o texto do primeiro pokemon
+        expect(pokemonName).toHaveTextContent(firstPokemon.name);
+      });
