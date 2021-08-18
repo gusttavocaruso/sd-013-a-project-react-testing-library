eef66ce709ef1065232c3e25e7515837a3c29446
Lanai Conceição <conceicao.lanai@gmail.com>
2021-08-18 18:00:46 -0300

    Ajustando requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,18 +4,39 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
-test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
-  renderWithRouter(<App />);
+describe('Testa o componente App', () => {
+  test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
+    renderWithRouter(<App />);
 
-  const getLinkHome = screen.getByRole('link', { name: /Home/i });
-  userEvent.click(getLinkHome);
-  expect(getLinkHome).toBeInTheDocument();
+    const getLinkHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(getLinkHome);
+    expect(getLinkHome).toBeInTheDocument();
 
-  const getLinkAbout = screen.getByRole('link', { name: /About/i });
-  userEvent.click(getLinkAbout);
-  expect(getLinkAbout).toBeInTheDocument();
+    const getLinkAbout = screen.getByRole('link', { name: /About/i });
+    userEvent.click(getLinkAbout);
+    expect(getLinkAbout).toBeInTheDocument();
 
-  const getLinkFavPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
-  userEvent.click(getLinkFavPokemons);
-  expect(getLinkFavPokemons).toBeInTheDocument();
+    const getLinkFavPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(getLinkFavPokemons);
+    expect(getLinkFavPokemons).toBeInTheDocument();
+  });
+
+  test('Testa se ao clicar nos Links de navegação eles vão para o path certo', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const getLinkHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(getLinkHome);
+    const pathnameHome = history.location.pathname;
+    expect(pathnameHome).toBe('/');
+
+    const getLinkAbout = screen.getByRole('link', { name: /About/i });
+    userEvent.click(getLinkAbout);
+    const pathnameAbout = history.location.pathname;
+    expect(pathnameAbout).toBe('/about');
+
+    const getLinkFavPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(getLinkFavPokemons);
+    const pathnameFavPokemons = history.location.pathname;
+    expect(pathnameFavPokemons).toBe('/favorites');
+  });
 });
