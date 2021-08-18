6c6bf70a792c48b9b2751100820979b0781a10c7
ivandosss <ivanndosssilva9389@gmail.com>
2021-08-18 19:28:00 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('App.js tests', () => {
+  test('Verifica se aplicação contém um conjunto fixo de link de navegação.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const link = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    expect(link).toBeInTheDocument();
+
+    const about = screen.getByRole('link', {
+      name: /Abou/i,
+    });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
