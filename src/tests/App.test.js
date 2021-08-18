4fa8cb8b34e2f96fccc6fe3341eadbfbd3ecec41
Gessé Carlos <gesse.dev@outlook.com>
2021-08-18 14:09:32 -0300

    requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import RouterRender from './RouterRender';
+
+describe('Tests App.js', () => {
+  describe('Tests if Home, About and Favorite navigation links exist', () => {
+    it('tests if there is a fixed set of links: "Home", "About", "Favorite" ', () => {
+      RouterRender(<App />);
+
+      const getAllLinks = screen.getAllByRole('link');
+      expect(getAllLinks[0]).toHaveTextContent('Home');
+      expect(getAllLinks[1]).toHaveTextContent('About');
+      expect(getAllLinks[2]).toHaveTextContent('Favorite Pokémons');
+    });
+  });
+});
+
+describe('Tests the behavior of links', () => {
+  let setHistory;
+  beforeEach(() => {
+    setHistory = RouterRender(<App />).history;
+  });
+  it('Tests if when clicked "Home" it will be redirected to /', () => {
+    const getHomeLink = screen.getByRole('link', { name: 'Home' });
+    expect(getHomeLink.innerHTML).toStrictEqual('Home');
+
+    userEvent.click(getHomeLink);
+    const homePage = setHistory.location.pathname;
+    expect(homePage).toStrictEqual('/');
+  });
+  it('Tests if when clicked "About" it will be redirected to /about', () => {
+    const getAboutLink = screen.getByRole('link', { name: 'About' });
+    expect(getAboutLink.innerHTML).toStrictEqual('About');
+
+    userEvent.click(getAboutLink);
+    const aboutPage = setHistory.location.pathname;
+    expect(aboutPage).toStrictEqual('/about');
+  });
+  it('Tests if when clicked "Favorite Pokémons" it will be redirected to /favorites',
+    () => {
+      const getFavoritesLink = screen.getByRole('link', { name: /Favorite Pokémons/ });
+      expect(getFavoritesLink.innerHTML).toStrictEqual('Favorite Pokémons');
+
+      userEvent.click(getFavoritesLink);
+      const favoritesPage = setHistory.location.pathname;
+      expect(favoritesPage).toStrictEqual('/favorites');
+    });
+});
