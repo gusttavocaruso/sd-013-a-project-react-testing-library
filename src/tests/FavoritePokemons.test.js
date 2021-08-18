ad8a975370f89e5239d86be0200fd27826a64f58
pedroalles <pedroalles@gmail.com>
2021-08-18 14:01:54 -0300

    Realizado requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import mockedPokemon from './helpers/mockPokemons';
+
+describe('Testando FavoritePokemons.js', () => {
+  test('se é exibido na tela "No favorite pokemon found" caso não haja favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+  test('se são exibidos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [mockedPokemon] } />);
+    const pokemon = screen.getByText(/alakazam/i);
+    expect(pokemon).toBeInTheDocument();
+  });
+});
