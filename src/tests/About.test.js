175c5a5fb4c60ae2d8e4228b6d8d5f28e97c35b4
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 13:41:57 -0300

    Preparando os requisitos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
+
+// Guarda na variável pokemons a relação de pokemons do data.js
