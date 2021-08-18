4e23196a20e548bcb372e7de13fae5d38b3f8253
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-08-18 18:11:43 -0300

    Requirement 4 finished

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test "NotFound" page', () => {
+  it('Renders "page not found" text', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', { level: 2 });
+    expect(notFoundText).toBeInTheDocument();
+    expect(notFoundText).toHaveTextContent('Page requested not found 😭');
+  });
+
+  it('Renders "page not found" image', () => {
+    renderWithRouter(<NotFound />);
+
+    const altText = 'Pikachu crying because the page requested was not found';
+    const notFoundImg = screen.getByAltText(altText);
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
