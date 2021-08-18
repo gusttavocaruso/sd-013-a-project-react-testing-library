0987ed7751fc7c5b8f24e9f0cb642a215f8db902
Walace Nascimento <wlacens@hotmail.com>
2021-08-18 20:59:25 -0300

    Resolve o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste o componente "FavoritePokemons"', () => {
+  test('Testa se é exibido na tela a mensagem No favorite pokemon found,', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFoundFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFoundFavorite).toBeInTheDocument();
+  });
+
+  test('Testa se todos os cards são renderizados', () => {
+    renderWithRouter(<App />);
+    const favoriteDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(favoriteDetails);
+
+    const checkFavorite = screen.getByRole('checkbox');
+    const favoritePokemon = screen.getByRole('link', { name: /favorite pokémons/i });
+
+    userEvent.click(checkFavorite);
+    userEvent.click(favoritePokemon);
+
+    const cardText = screen.getByText(/electric/i);
+    expect(cardText).toBeInTheDocument();
+  });
+});
