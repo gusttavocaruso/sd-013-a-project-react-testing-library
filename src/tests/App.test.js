54273932660fb205b9525df7477e7902ae647cc0
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-18 16:23:16 -0300

    Req 1 completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent, cleanup } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
+afterEach(cleanup);
+
 describe('Testando o component App', () => {
-  renderWithRouter(<App />);
-  it('O primeiro link deve possuir o texto "Home"', () => {
+  it('O link "Home" leva para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
     const linkHome = screen.getByText('Home');
     expect(linkHome).toBeInTheDocument();
+
+    fireEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
-  it('O segundo link deve possuir o texto "About"', () => {
-    renderWithRouter(<App />);
-    const linkHome = screen.getByText('About');
-    expect(linkHome).toBeInTheDocument();
+
+  it('O link "About" leva para página sobre', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByText('About');
+    expect(linkAbout).toBeInTheDocument();
+
+    fireEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
   });
-  it('O segundo link deve possuir o texto "Favorite Pokémons"', () => {
-    renderWithRouter(<App />);
-    const linkHome = screen.getByText('Favorite Pokémons');
-    expect(linkHome).toBeInTheDocument();
+
+  it('O link "Favorite Pokémons" leva para página de favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByText('Favorite Pokémons');
+    expect(linkFavorite).toBeInTheDocument();
+
+    fireEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });
