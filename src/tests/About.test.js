a4b5a71e24ad0fc0f4a114543bd7ad9baab6a404
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-18 17:31:44 -0300

    Req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testando a página Sobre', () => {
+  it('A página deve conter um h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const aboutPokedex = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('A página deve conter dois parágrafos de texto', () => {
+    renderWithRouter(<About />);
+
+    const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+    expect(firstParagraph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('A página de conter uma imagem com a url especificada', () => {
+    renderWithRouter(<About />);
+
+    const getImg = screen.getByRole('img');
+    expect(getImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
