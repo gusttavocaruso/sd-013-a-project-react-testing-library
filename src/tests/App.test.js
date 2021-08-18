2ff4a0e70acf03c70cf37f5ddacefcb54c332c42
thomasravache <thomasravache31@gmail.com>
2021-08-18 19:18:04 -0300

    01-Verifica se contem os links de navegacao necessarios e se ao clicar no home e redirecionando para o path principal

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../../renderWithRouter';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../../renderWithRouter';
 import App from '../App';
 
 describe('Testando o componente App.js', () => {
