7b8aa99d39af8fad1d72f98434afd0aead872f7a
junglejf <jfcchagas@id.uff.br>
2021-08-18 20:21:23 -0300

    Criado arquivo renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './util/renderWithRouter';
+
+describe("1- Testando se o topo da aplicação contém um conjunto fixo de links de navegação.", () => {
+  it("O primeiro link deve possuir o texto Home.", () => {
+    renderWithRouter(<App />);
+    const navBar = screen.getByRole('link');
+    expect(navBar).toBeInTheDocument();
+    console.log(navBar);
+  });
+});
