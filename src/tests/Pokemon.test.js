2f9dca9184d86a39bf4bd20fc4fc7bb6bebde26b
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:38:43 -0300

    Alterando textos dos describes e tests do requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,3 +7,8 @@ import renderWithRouter from './renderWithRouter';
 // Busca a lista de pokemons
 import pokemons from '../data';
 
+// Para corrigir erros de lint foi criado as constantes abaixo apenas com nomes que campos do App para serem chamados no teste
+const nameTestId = 'pokemon-name';
+const typeTestId = 'pokemon-type';
+const weightTestId = 'pokemon-weight';
+const firstPokemon = pokemons[0]; // Pega o primeiro pokemon
