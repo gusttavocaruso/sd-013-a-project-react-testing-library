d58349b4cde96852ef45f79fa5f7256255ce0aa0
thomasravache <thomasravache31@gmail.com>
2021-08-18 18:48:24 -0300

    Iniciando o projeto :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testando o componente App.js', () => {
+  it('Deve conter o link "Home", "About" e "Favorite Pokémons"', () => {
+    render(<App />);
+
+    const linkHome = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    const linkAbout = screen.getByRole('link', {
+      name: /about/i,
+    });
+    const linkFavoritePokemons = screen.getByRole('link', {
+      name: /favorite pokemons/i,
+    });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+});
