98c8c8d58e4ba4a5ecbecf9497157bee358b49fb
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 13:45:16 -0300

    Iniciando importes necessários no requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,3 +5,11 @@ import { screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 01 - Teste do componente .js', () => {
+  describe('Testando se a aplicação possui determinados links de navegação.', () => {
+    test('O primeiro link deve possuir o texto Home', () => {
+
+    });
+  });
+});
