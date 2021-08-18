ad920ca47b7d6ded1735a70cf54b834504c27ea8
Lanai Conceição <conceicao.lanai@gmail.com>
2021-08-18 19:11:37 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Testa se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const getTextFavPokemons = screen.getByText(/No favorite pokemon found/i);
+    expect(getTextFavPokemons).toBeInTheDocument();
+  });
+});
