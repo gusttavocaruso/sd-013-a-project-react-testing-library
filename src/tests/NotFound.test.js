b173625b2c2d99e0d9353c8f501111f74fa837cb
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-18 19:07:03 -0300

    Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('Testando a página Not Found', () => {
+  it('Testando se a página tem um h2 com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = /Page requested not found/i
+    const h2Content = screen.getByRole('heading', { name: notFoundText, level: 2 });
+    expect(h2Content).toBeInTheDocument();
+  });
+
+  it('Testando se na página existe a imagem especificada', () => {
+    renderWithRouter(<NotFound />);
+    const getImg = screen.getByRole('img', { name: /Pikachu crying/i});
+    expect(getImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
