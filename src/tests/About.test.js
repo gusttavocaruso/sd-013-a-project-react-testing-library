c42104f0f18d42559c2cdc737a53a92a9c5e6b1f
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-08-18 16:40:10 -0300

    2. Teste o componente <About.js />.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém um h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+    const h2Text = screen.getByRole('heading', {
+      name: /about pokédex/i,
+    });
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    screen.getByText(/This application simulates a Pokédex/i,
+      /a digital encyclopedia containing all Pokémons/i);
+    screen.getByText(/One can filter Pokémons by type/i,
+      /and see more details for each one of them/i);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex.', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByAltText(/Pokédex/i);
+    expect(img.src).toContain(
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
