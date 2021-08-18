b253def0bfd3fa9ab74baf0855849bdf24ce3bb7
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:46:10 -0300

    Estruturando os describes no requisito 07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,3 +4,8 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
+// Guarda na variável pokemons a relação de pokemons do data.js
+import pokemons from '../data';
+
+// Guarda no firstPokemon o primeiro pokemon do array
+const firstPokemon = pokemons[0];
