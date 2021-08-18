6fbb24f0843dc7b22f4788fb2bf608531355c595
sonkai <mfilho86@gmail.com>
2021-08-18 18:48:50 -0300

    req 3 + lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('Teste do About', () => {
