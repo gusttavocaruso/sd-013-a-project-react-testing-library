c8efa6b29fc0b3b2919c3e4e761909e873fcb422
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-08-18 15:34:08 -0300

    Conclusao requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+test('Teste o About.js', () => {
+  render(<About />);
+
+  const headerAbout = screen.getByRole('heading', {
+    name: /about pokédex/i,
+    level: 2,
+  });
+  expect(headerAbout).toBeInTheDocument();
+
+  const imgAbout = screen.getByRole('img');
+
+  expect(imgAbout).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
