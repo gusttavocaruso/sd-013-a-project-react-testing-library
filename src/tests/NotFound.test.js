295aa3d27a3a9230fb46a1c3f9d1cc2f27a52f93
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:19:11 -0300

    Alterando textos dos describes e tests do requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -9,3 +9,12 @@ describe('Requisito 04 - Teste o componente NotFound.js', () => {
     renderWithRouter(<NotFound />);
     // getByRole um elemento 'heading'(h1/h2/h3/h4...) busca um título, ou qualquer nível de título, com o name de "Page requested not found".
     const message = screen.getByRole('heading', { name: /Page requested not found/i });
+    // Busca um elemento img com o name "Crying emoji"
+    const cryingEmoji = screen.getByRole('img', { name: /Crying emoji/i });
+    // Testa se o conteúdo da variável message está no documento
+    expect(message).toBeInTheDocument();
+    // Testa se o conteúdo da variável cryingEmoji está no documento
+    expect(cryingEmoji).toBeInTheDocument();
+  });
+
+  
