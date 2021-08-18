bc80f4798316ec9b4fa6f6f4a76238fc0cf81df1
Lanai Conceição <conceicao.lanai@gmail.com>
2021-08-18 17:28:05 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links.', () => {
+  renderWithRouter(<App />);
+
+  const getLinkHome = screen.getByRole('link', { name: /Home/i });
+  userEvent.click(getLinkHome);
+  expect(getLinkHome).toBeInTheDocument();
+
+  const getLinkAbout = screen.getByRole('link', { name: /About/i });
+  userEvent.click(getLinkAbout);
+  expect(getLinkAbout).toBeInTheDocument();
+
+  const getLinkFavPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  userEvent.click(getLinkFavPokemons);
+  expect(getLinkFavPokemons).toBeInTheDocument();
+});
