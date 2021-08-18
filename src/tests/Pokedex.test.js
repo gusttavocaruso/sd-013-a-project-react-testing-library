b490fd2991a3fcd87ec86d8b099d84e526f5c905
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:34:11 -0300

    Corrigindo lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -161,6 +161,8 @@ describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
           // Através da importação de dados de pokemons na "pokemons" faço um foreach para ler cada um dos pokemons, testando se mostra todos os pokemons
           getAllPokemons(nextPkmBtn);
         });
+
       });
+      
   });
 });
