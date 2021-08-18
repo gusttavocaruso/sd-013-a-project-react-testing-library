7f57d2b8a4e9fcc729d984eed3e9b9e4ce0d746b
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-18 16:45:01 -0300

    Completa o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente NotFound.js', () => {
+  it('Testa se página tem um header com o texto certo', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/teste');
+    const header = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(header).toBeInTheDocument();
+  });
+  it('Testa se página tem um gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/teste');
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toBeInTheDocument();
+  });
+});
