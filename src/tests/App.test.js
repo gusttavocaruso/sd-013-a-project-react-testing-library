0b7155b1607e0f125bbbc41760e377348cf38998
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-08-18 15:05:01 -0300

    Conclusao do requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('Testa o App.js', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+test('Testa o App.js', () => {
+  renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', {
+    name: /home/i,
+  });
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', {
+    name: /about/i,
+  });
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', {
+    name: /favorite pokémons/i,
+  });
+  expect(favoriteLink).toBeInTheDocument();
+});
