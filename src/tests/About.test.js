a104a2c51c1c00a0441ff14f7ff38296e156eea3
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-18 18:46:54 -0300

    Requisto 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import About from '../components/About';
+
+describe('About.js tests', () => {
+  test('Renderiza as informações sobre a Pokédex', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const about = screen.getByText(/encyclopedia containing/i);
+    expect(about).toBeInTheDocument();
+  });
+
+  test('Renderiza um heading h2 com texto correto', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('Renderiza dois paragrafos e seus textos', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const p1 = screen.getByText(/a digital encyclopedia /i);
+    expect(p1).toBeInTheDocument();
+    const p2 = screen.getByText(/see more details for each/i);
+    expect(p2).toBeInTheDocument();
+  });
+
+  test('Renderiza imagem GameBoy', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const img = screen.getByRole('img');
+    expect(img.src).toStrictEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
