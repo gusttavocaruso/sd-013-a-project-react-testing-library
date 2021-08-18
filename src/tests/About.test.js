a6e06f18a55476da9d0b3bd6f9874d91499bcd35
Murilo Rainho <murilorainho01@gmail.com>
2021-08-18 15:00:25 -0300

    realizando requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,10 +1,46 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 
-// import App from '../App';
+import App from '../App';
 
-describe('', () => {
-  it('should ', () => {});
+describe('Testing \'about\' component', () => {
+  it('should have a title about \'pokedex\'', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+
+    const aboutTitle = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('should have informations about \'pokedex\'', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+
+    const firstText = screen.getByText(/this application simulates a pokédex/i);
+    const secondText = screen.getByText(/one can filter pokémons by type/i);
+
+    expect(firstText).toBeInTheDocument();
+    expect(secondText).toBeInTheDocument();
+  });
+
+  it('should have an image in \'about\' component', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+
+    const imageAbout = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(imageAbout.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
