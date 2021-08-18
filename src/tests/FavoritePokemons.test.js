b4ebdb1b16c970d3799c7eebf0ccc7e312dd845f
Gessé Carlos <gesse.dev@outlook.com>
2021-08-18 14:42:32 -0300

    requisito 3 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testing Favorite Pokemons component', () => {
+  beforeEach(() => {
+    render(<FavoritePokemons />);
+  });
+
+  it('Test when there is no favorite pokémon', () => {
+    const message = screen.getByText('No favorite pokemon found');
+    // toBeDefined foi inspirado neste exemplo https://testing-library.com/docs/preact-testing-library/example/
+    expect(message).toBeDefined();
+  });
+});
