f3aae1183a1936a3e97489422e91595eed165f7e
sonkai <mfilho86@gmail.com>
2021-08-18 20:26:28 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/caminhoerrado');
+
+    const notFound = screen.getByRole('heading', { name: /Page requested not/i });
+    const imgNotFound = screen.getByRole('img', { name: /Pikachu crying/i });
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(notFound).toBeInTheDocument();
+    expect(imgNotFound.src).toBe(src);
+  });
+});
