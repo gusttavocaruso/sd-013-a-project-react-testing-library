6da6907cf14ad99143a09b4332d2aca7cd64fe1c
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-18 16:13:50 -0300

    Resolve requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa componente "Not Found"', () => {
+  it('Testa se a página contém um H2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem do "Pikachu"', () => {
+    renderWithRouter(<NotFound />);
+
+    const imagePikachu = screen.getByAltText(/pikachu crying because the page/i);
+
+    expect(imagePikachu).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
