bb883c2128b3850b5baa1ad32038539e92c137e6
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:28:39 -0300

    Alterando textos dos describes e tests do requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -5,3 +5,8 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 import pokemons from '../data';
+
+// Devido erros de lint teve que ser criado estas constantes pois são utilizadas algumas vezes no projeto
+const nameTestId = 'pokemon-name';
+const typeTestId = 'pokemon-type';
+const typeButtonTestId = 'pokemon-type-button';
