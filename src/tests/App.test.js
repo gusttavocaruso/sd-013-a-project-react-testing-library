cee3b6069acb511dc7a5c84f7ce5717e95d86974
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 13:44:24 -0300

    Estruturando requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,6 @@
 
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
-import userEvent from '@testing-library/user-event';
