f1b660066aa4d209dfac845cb27936417c1c44c7
Filipe <filipecandido123@hotmail.com>
2021-08-18 19:48:37 -0300

    requisito 3 completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Verifica o component FavoritePokemons', () => {
+  it(`Verifica se é exibido na tela a mensagem
+  'No favorite pokemon found,' se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const NotFavorited = screen.getByText(/No favorite pokemon found/i);
+    expect(NotFavorited).toBeInTheDocument();
+  });
+  // https://github.com/tryber/sd-013-a-project-react-testing-library/pull/48/files Fonte: Fernando Costa
+  it('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    // Desconstroi o history do renderWithRouter do componente App
+    const { history } = renderWithRouter(<App />);
+    // Busca um elemento link com o endereço "/more details" e guarda na variável moreDetails
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    // Simula o click no elemento que está na variável moreDetails
+    userEvent.click(moreDetails);
+    // history.location.pathname pega o endereço da página.
+    // Verificamos se o texto que aparece quando clicamos nesse link no navegador é o "/pokemons/25".
+    expect(history.location.pathname).toEqual('/pokemons/25');
+    // Consulta na árvore de acessibilidade se há um elemento checkbox e guarda esse elemento na variável checkbox
+    const checkbox = screen.getByRole('checkbox');
+    // Simula o click no elemento que está na variável checkbox
+    userEvent.click(checkbox);
+    // getByAltText: Se o seu elemento é aquele que suporta alttexto ( img, areae input), então você pode usar isso para encontrar esse elemento.
+    // Verificando através do getByAltText se o texto "Pikachu is marked as favorite" e guarda o elemento na variável pikachuStar
+    const pikachuStar = screen.getByAltText(/Pikachu is marked as favorite/i);
+    // Verifica se o elemento que está dentro da varipável pikachuStar está no documento.
+    expect(pikachuStar).toBeInTheDocument();
+    // Busca um elemento link com o endereço "/favorite pokémons" e guarda na variável moreDetails.
+    const favoritePokemons = screen.getByRole('link',
+      { name: /favorite pokémons/i });
+      // Simula o click no elemento que está na variável favoritePokemons
+    userEvent.click(favoritePokemons);
+    // history.location.pathname pega o endereço da página.
+    // Verificamos se o texto que aparece quando clicamos nesse link no navegador é o "/favorites".
+    expect(history.location.pathname).toEqual('/favorites');
+    // Guarda na variável pikachu o elemento com o id 'pokemon-name', isso é possível pois foi criado o data-test-id com esse name abaixo
+    const pikachu = screen.getByTestId('pokemon-name');
+    // Verifica se o elemento guardado na variável pikachu está no documento.
+    expect(pikachu).toBeInTheDocument();
+  });
+});
