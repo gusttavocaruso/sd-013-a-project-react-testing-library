6e0cd5d4e109296cc69ca97238c3200adb262a0b
Cássio Pereira <cassiorodp@gmail.com>
2021-08-18 14:32:30 -0300

    Iniciando projeto :trollface:

---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+
+export default function renderWithRouter(component) {
+  const history = createMemoryHistory();
+  const objRender = render(<Router history={ history }>{component}</Router>);
+  return { ...objRender, history };
+}
+
+// return ({...render(<Router history={ history }>{component}</Router>), history});
