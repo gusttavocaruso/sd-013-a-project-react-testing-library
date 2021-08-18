498079523e7f36248e6a7b6d4687ca2722469189
JeanVPacheco <jeanvitorp@gmail.com>
2021-08-18 19:53:44 -0300

    Adiciona requisito 4, testando NotFound page

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('', () => {
+  it('Checks if theres a particular header', () => {
+    render(<NotFound />);
+    // this h2 should be in the page
+    const messageDisplays = /page requested not found/i;
+    const headingElement = screen.getByRole('heading', {
+      name: messageDisplays,
+      level: 2,
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
+  it('Checks if the Pikachu image is in the page', () => {
+    render(<NotFound />);
+    // This img should be in the page
+    const pikachuImg = screen.getByAltText(/pikachu crying/i);
+    // This should be the source of the image fetched above
+    expect(pikachuImg.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
+
 test('', () => {});
