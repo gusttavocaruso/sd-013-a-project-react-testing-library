96be1c88b458d1f2b7039779f607402b860208b9
luiz furtado <luizfurtadomkt@hotmail.com>
2021-08-18 16:51:27 -0300

    requisito 3 e 4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,8 +4,10 @@ import About from '../components/About';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa o componente <About.js />', () => {
-  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+  beforeEach(() => {
     renderWithRouter(<About />);
+  });
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
     const aboutText = screen.getByRole('heading', {
       name: /About Pokédex/i,
       level: 2,
@@ -14,7 +16,6 @@ describe('Testa o componente <About.js />', () => {
   });
 
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    renderWithRouter(<About />);
     const p1 = screen.getByText(/This application simulates a Pokédex/i);
     expect(p1).toBeInTheDocument();
     const p2 = screen.getByText(/One can filter Pokémons by type/i);
@@ -22,7 +23,6 @@ describe('Testa o componente <About.js />', () => {
   });
 
   it('Testa se a página contém imagem de uma Pokédex', () => {
-    renderWithRouter(<About />);
     const img = screen.getByRole('img');
     expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying/i);
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
