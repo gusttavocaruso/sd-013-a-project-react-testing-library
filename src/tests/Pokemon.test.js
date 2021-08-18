b7c97252b964dd911ad8bee209e288a17a162bf7
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-08-18 13:59:16 -0300

    Changes to Pokemon.test.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,18 +7,20 @@ import Pokemons from '../data';
 
 afterEach(cleanup);
 
+const { id, name, type, averageWeight: { value, measurementUnit }, image } = Pokemons[0];
+
 describe('Tests for Pokemon.js', () => {
   it('Check that a pokemon card is generated', () => {
     renderWithRouter(<Pokemon pokemon={ Pokemons[0] } isFavorite={ false } />);
 
-    const name = screen.getByTestId('pokemon-name');
-    expect(name.innerHTML).toBe('Pikachu');
-    const type = screen.getByTestId('pokemon-type');
-    expect(type.innerHTML).toBe('Electric');
+    const pokeName = screen.getByTestId('pokemon-name');
+    expect(pokeName.innerHTML).toBe(name);
+    const pokeType = screen.getByTestId('pokemon-type');
+    expect(pokeType.innerHTML).toBe(type);
     const weight = screen.getByTestId('pokemon-weight');
-    expect(weight.innerHTML).toBe('Average weight: 6.0 kg');
-    const img = screen.getByAltText('Pikachu sprite');
-    const src = img.src === 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    expect(weight.innerHTML).toBe(`Average weight: ${value} ${measurementUnit}`);
+    const img = screen.getByAltText(`${name} sprite`);
+    const src = img.src === image;
     expect(src).toBeTruthy();
   });
 
@@ -30,15 +32,15 @@ describe('Tests for Pokemon.js', () => {
 
     const link = screen.getByRole('link', { name: 'More details' });
     expect(link).toBeInTheDocument();
-    expect(link.href).toBe('http://localhost/pokemons/25');
+    expect(link.href).toBe(`http://localhost/pokemons/${id}`);
     userEvent.click(link);
-    expect(history.location.pathname).toBe('/pokemons/25');
+    expect(history.location.pathname).toBe(`/pokemons/${id}`);
   });
 
   it('Check for favorite star icon', () => {
     renderWithRouter(<Pokemon pokemon={ Pokemons[0] } isFavorite />);
 
-    const favPoke = screen.getByAltText('Pikachu is marked as favorite');
+    const favPoke = screen.getByAltText(`${name} is marked as favorite`);
     expect(favPoke).toBeInTheDocument();
     expect(favPoke.src).toBe('http://localhost/star-icon.svg');
   });
