7610cd4fc6e1c54d0a84f046efef42f6b654b4b7
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-18 19:08:36 -0300

    Fixed the lint of NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -8,14 +8,14 @@ afterEach(cleanup);
 describe('Testando a página Not Found', () => {
   it('Testando se a página tem um h2 com o texto "Page requested not found 😭"', () => {
     renderWithRouter(<NotFound />);
-    const notFoundText = /Page requested not found/i
+    const notFoundText = /Page requested not found/i;
     const h2Content = screen.getByRole('heading', { name: notFoundText, level: 2 });
     expect(h2Content).toBeInTheDocument();
   });
 
   it('Testando se na página existe a imagem especificada', () => {
     renderWithRouter(<NotFound />);
-    const getImg = screen.getByRole('img', { name: /Pikachu crying/i});
+    const getImg = screen.getByRole('img', { name: /Pikachu crying/i });
     expect(getImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
