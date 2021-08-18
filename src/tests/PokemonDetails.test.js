9a1efe36ae099276f3a5900ea8ce10c9ca24342f
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-18 14:47:19 -0300

    Finalizando terceiro teste do requisito 07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -23,7 +23,7 @@ describe('Requisito 07 - Teste o componente <PokemonDetails.js />', () => {
         // Testo se o elemento heading está no documento
         expect(h2).toBeInTheDocument();
       });
-      
+
       test('Não deve existir link de navegação para detalhes do Pokémon selecionado',
         () => {
           // Renderizo na tela as rotas do componente App.
@@ -34,6 +34,18 @@ describe('Requisito 07 - Teste o componente <PokemonDetails.js />', () => {
           userEvent.click(moreDetails);
           // Testo se o elemento "more details" não está renderizado no documento
           expect(moreDetails).not.toBeInTheDocument();
+        });
+
+        test('A seção de detalhes deve conter um heading h2 com o texto Summary',
+        () => {
+          // Renderizo na tela as rotas do componente App.
+          renderWithRouter(<App />);
+          // Simulo um click no elemento link com name/texto "more details".
+          userEvent.click(screen.getByRole('link', { name: /more details/i }));
+          // Procura um heading(h1,h2,h3...) com name/texto do primeiro pokemon pego pela variável que possui o primeiro pokemon do data.js
+          const h2 = screen.getByRole('heading', { name: /summary/i });
+          // Testo se o elemento heading está no documento
+          expect(h2).toBeInTheDocument();
         });
 
 
