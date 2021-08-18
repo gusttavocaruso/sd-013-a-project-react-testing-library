fb37726b6672f01ba6b39ad1938384dac437b131
Walace Nascimento <wlacens@hotmail.com>
2021-08-18 17:47:35 -0300

    Resolve o requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa se na aplicação existe links de navegação - Requisito1 ', () => {
+  test('Testa se existe link de navegação como o texto "Home"', () => {
+    renderWithRouter(<App />);
+    const linkElementHome = screen.getByRole('link', { name: /home/i });
+    expect(linkElementHome).toBeInTheDocument();
+  });
+
+  test('Testa se existe link de navegação como o texto "About"', () => {
+    renderWithRouter(<App />);
+    const linkElementAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkElementAbout).toBeInTheDocument();
+  });
+
+  test('Testa se existe um link com o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const linkElementFavorite = screen.getByRole('link', { name: /favorite/i });
+    expect(linkElementFavorite).toBeInTheDocument();
+  });
+});
