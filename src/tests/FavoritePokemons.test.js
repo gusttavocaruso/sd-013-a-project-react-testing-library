87f8434db369d0ce3ad5b6fb68add89f9a916f45
Murilo Rainho <murilorainho01@gmail.com>
2021-08-18 15:16:40 -0300

    requisito 4 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,34 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 
-// import App from '../App';
+import App from '../App';
 
-describe('', () => {
-  it('should ', () => {});
+describe('Testing \'FavoritePokemons\' component', () => {
+  it('should no have favorite pokemons', () => {
+    renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoriteLink);
+
+    const noFavorite = screen.getByText(/no favorite pokemon/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('should have favorite pokemons', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const favoriteButton = screen.getByLabelText(/pokémon favoritado/i);
+    userEvent.click(favoriteButton);
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoriteLink);
+
+    const pokeSprite = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(pokeSprite).toBeInTheDocument();
+  });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 
-// import App from '../App';
+import App from '../App';
 
 describe('', () => {
   it('should ', () => {});
