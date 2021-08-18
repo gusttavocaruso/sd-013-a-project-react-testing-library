7e6537f93c8a5e00af69bfff041d982fc2750d2c
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-08-18 17:09:06 -0300

    Conclusao requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Testa FavoritePokemons.js', () => {
+  render(<FavoritePokemons />);
+
+  const text = screen.getByText('No favorite pokemon found');
+
+  expect(text).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+
+test('Testes na pagina NotFound.js', () => {
+  render(<NotFound />);
+
+  const headerNotFound = screen.getByRole('heading', {
+    name: /page requested not found/i,
+    level: 2,
+  });
+  expect(headerNotFound).toBeInTheDocument();
+
+  const imageNotFound = screen.getAllByRole('img');
+  expect(imageNotFound[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
