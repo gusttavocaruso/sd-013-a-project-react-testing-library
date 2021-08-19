1374a01bef9cc04718f489038cb10305228441ae
thomasravache <thomasravache31@gmail.com>
2021-08-19 01:19:44 -0300

    Adicionando alteracoes

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testando componente FavoritePokemons.js', () => {
+  it('É exibida na tela a mensagem "No favorite pokemon found"'
+  + ', se não tiver pokemons favoritos', () => {
+    renderWithRouter(<App />);
+
+  });
+});
