4f5cb4a47452b334808a87a8f556ec92c3b7cb88
Icaro Ferreira <icaro.iky@gmail.com>
2021-08-19 14:27:11 -0300

    Parte 1 Req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('Testando o componente da Pokedex', () => {
+  it('Testando se a página tem h2 com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const textHeader = 'Encountered pokémons';
+    const findHeader = screen.getByRole('heading', { name: textHeader, level: 2 });
+    expect(findHeader).toBeInTheDocument();
+  });
+});
