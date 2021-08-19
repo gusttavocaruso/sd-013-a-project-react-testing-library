6d9429432193fe176c2922d083d7527486b21940
Rafael Victor <rafavictorgs@gmail.com>
2021-08-19 15:35:59 -0300

    Resolve o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do componente <NotFound />', () => {
+  it('Verifica se a página contém um <h2/>', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const headingText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+
+  it('Verifica se a página contém um <img/>', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/banana');
+    const image = screen.getByRole('img', {
+      name: /pikachu/i,
+    });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
