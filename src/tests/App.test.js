59e5009a6f4c2ab231d3578335628cc33de46797
Felipe Dias <felipeferreiradias1@gmail.com>
2021-08-19 00:52:12 -0300

    fix lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,36 +1,34 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import { screen, render } from '@testing-library/react';
 import App from '../App';
 
 // Grupo de testes do requisito 1"
 describe('Teste o componente <App.js/> (req1)', () => {
-   
   it('Primeiro link deve ter o texto "Home"', () => {
     // Acessa os elementos da tela
-    const {getByRole} = renderWithRouter(<App />);
-    const linkHome = getByRole('link', { name: /home/i } );
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
 
-    //Faz o teste
+    // Faz o teste
     expect(linkHome).toBeInTheDocument();
   });
 
   it('Segundo link deve ter o texto "About"', () => {
     // Acessa os elementos da tela
     renderWithRouter(<App />);
-    const linkAbout = screen.getByRole('link', { name: /About/i } );
+    const linkAbout = screen.getByRole('link', { name: /About/i });
 
-    //Faz o teste
+    // Faz o teste
     expect(linkAbout).toBeInTheDocument();
   });
 
   it('Segundo link deve ter o texto "Favorite Pokémons"', () => {
     // Acessa os elementos da tela
     renderWithRouter(<App />);
-    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i } );
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
 
-    //Faz o teste
+    // Faz o teste
     expect(linkFavorite).toBeInTheDocument();
   });
-
 });
