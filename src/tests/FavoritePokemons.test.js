1e745c8752e8337c9165bbf7b47db2a0076d6cc0
junglejf <jfcchagas@id.uff.br>
2021-08-19 01:56:46 -0300

    Requisito 3 - No Favorite Pokemon Test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('É exibido na tela a mensagem - No favorite pokemon '
+  + 'found -, se a pessoa não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+    const noFavoritePokemon = screen.queryByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+    expect(noFavoritePokemon).toHaveTextContent('No favorite pokemon found');
+  });
+});
