ca07df04965a303fa8e6dce028e1a9fd4518cd78
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-08-18 22:20:53 -0300

    resolvendo o primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,59 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('App.js tests', () => {
+  test('Verifica se ao clicar no Home renderiza a página inicial', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    userEvent.click(home);
+
+    const homeText = screen.getByText('Encountered pokémons');
+
+    expect(homeText).toBeInTheDocument();
+  });
+  test('Verifica se ao clicar em About renderiza a página sobre a aplicação', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const aboutText = screen.getByText('About Pokédex');
+
+    expect(aboutText).toBeInTheDocument();
+  });
+  test('Verifica se renderiza a página de pokémons favoritos', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+
+    userEvent.click(favorite);
+
+    const favoriteText = screen.getByText('Favorite pokémons');
+
+    expect(favoriteText).toBeInTheDocument();
+  });
+});
