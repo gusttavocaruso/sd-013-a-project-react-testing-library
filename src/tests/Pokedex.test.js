1d6334c381535eff6c169c7e0489cbadd155fa16
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-08-19 10:35:16 -0300

    Requirement 5 finished

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Test "Pokedex" component', () => {
+  it('Renders text "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const encounteredPokes = screen.getByRole('heading', { level: 2 });
+    expect(encounteredPokes).toHaveTextContent('Encountered pokémons');
+  });
+
+  it('Renders next pokemon when "Próximo pokémon" is clicked', () => {
+    renderWithRouter(<App />);
+
+    const allPokesFilter = screen.getByText(/all/i);
+    userEvent.click(allPokesFilter);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    const nextPokeBtn = screen.getByTestId('next-pokemon');
+
+    pokemons.forEach(({ name }) => {
+      expect(pokeName).toHaveTextContent(name);
+      userEvent.click(nextPokeBtn);
+    });
+
+    expect(pokeName).toHaveTextContent('Pikachu');
+  });
+
+  it('Renders filtered pokémons when any filter button is clicked', () => {
+    renderWithRouter(<App />);
+
+    const typesSet = new Set();
+    pokemons.forEach(({ type }) => typesSet.add(type));
+    const types = [...typesSet];
+    const btnAll = screen.getByText('All');
+
+    const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    typeButtons.forEach((typeBtn, i) => {
+      expect(btnAll).toBeInTheDocument();
+      expect(typeBtn).toHaveTextContent(types[i]);
+    });
+  });
+
+  // it('')
+});
---
+++ src/tests/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
