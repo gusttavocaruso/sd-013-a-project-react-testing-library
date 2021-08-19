318669912572d0d69efd90be6524d92f0bc92d00
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-19 17:25:28 -0300

    desafio 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,45 +1,44 @@
-import React from "react";
-import { screen } from '@testing-library/dom';
+import React from 'react';
+import { screen } from '@testing-library/react';
 import fireEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('testar o App.js', () => {
-    it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-        renderWithRouter(<App />);
-        const home = screen.getByRole('link', { name: 'Home' })
-        expect(home).toBeInTheDocument();
-        const about = screen.getByRole('link', { name: 'About' })
-        const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' })
+  it('Teste os links de navegação', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    expect(home).toBeInTheDocument();
+    const about = screen.getByRole('link', { name: 'About' });
+    expect(about).toBeInTheDocument();
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favorite).toBeInTheDocument();
+  });
+  it('Teste se é redirecionada para pág. inicial ao clicar no link Home barra navegação.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/');
+      fireEvent.click(screen.getByRole('link', { name: 'Home' }));
+      expect(history.location.pathname).toBe('/');
     });
-
-    it('Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.', () => {
-        const { history } = renderWithRouter(<App />);
-        history.push('/');
-        fireEvent.click(screen.getByRole('link', { name: 'Home' }))
-        expect(history.location.pathname).toBe('/');
-    })
-
-    it('Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.', () => {
-        const { history } = renderWithRouter(<App />); 
-        history.push('/about')
-        fireEvent.click(screen.getByRole('link', { name: 'About' }))
-        expect(history.location.pathname).toBe('/about');
-    })
-
-    it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.', () => {
-        const { history } = renderWithRouter(<App />); 
-        history.push('/favotites')
-        fireEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }))
-        expect(history.location.pathname).toBe('/favorites');
-    })
-
-    it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
-        const { history } = renderWithRouter(<App />); 
-        history.push('/nao-encontrada')
-        expect(history.location.pathname).toBe('/nao-encontrada')
-        // const msg = 'Pagina nao encontrada'
-        // const msg2 = screen.getByText(msg)
-        // expect(msg2).toBeInTheDocument();
-    })
+  it('Teste se é redirecionada à pág. About ao clicar no link About barra de navegação.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/about');
+      fireEvent.click(screen.getByRole('link', { name: 'About' }));
+      expect(history.location.pathname).toBe('/about');
+    });
+  it('Teste se é redirecionada à Pokémons Favoritados, ao clicar link Favorite Pokémons.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/favotites');
+      fireEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+      expect(history.location.pathname).toBe('/favorites');
+    });
+  it('Teste se é redirecionada à página Not Found ao entrar em uma URL desconhecida.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/nao-encontrada');
+      expect(history.location.pathname).toBe('/nao-encontrada');
+    });
 });
