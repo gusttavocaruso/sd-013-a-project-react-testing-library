13e3e9c7675f057137e93c6575e574e8ae056816
Murilo Ribeiro <murilorsv14@gmail.com>
2021-08-19 17:41:06 -0300

    requisito 1 e 2 com ajuda da julia baptista

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  test('teste de h2 com texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const textAboutPokemons = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+
+    expect(textAboutPokemons).toBeInTheDocument();
+  });
+
+  test('Testa se há dois paragrafos com texto sobre a pokedex', () => {
+    renderWithRouter(<About />);
+
+    const twoP = screen.getAllByTestId('p-element');
+    expect(twoP.length).toBe(2);
+  });
+
+  test('teste de contenção de imagem pokédex', () => {
+    render(<About />);
+
+    const imgPokedex = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(imgPokedex.src).toBe(src);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('Teste da aplicação de links', () => {
+  test('testa o link home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('testa o link about', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('testa o link Favorites Pokemons', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkfavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkfavorites).toBeInTheDocument();
+    userEvent.click(linkfavorites);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Teste se a aplicação é redirecionada para a página Not Found,'
+    + 'ao entrar em uma URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/2222');
+    const notFount = screen.getByRole(
+      'img', { name: 'Pikachu crying because the page requested was not found' },
+    );
+    expect(notFount).toBeInTheDocument();
+  });
+});
