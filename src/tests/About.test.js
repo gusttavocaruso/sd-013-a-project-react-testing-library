20a399056f75e24c0c9a828c81298ad26c4ef15b
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-08-19 00:48:28 -0300

    resolvido desafio 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,72 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('About.js tests', () => {
+  test('Verifica se página tem um heading h2', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const titleH2 = screen.getByText('About Pokédex');
+    expect(titleH2).toBeInTheDocument();
+  });
+  test('Verifica o primeiro paragrafo', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const paragraph1 = screen.getByText(/this application/i);
+
+    expect(paragraph1).toBeInTheDocument();
+  });
+  test('Verifica o segundo parágrafo', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const paragraph2 = screen.getByText(/one can/i);
+
+    expect(paragraph2).toBeInTheDocument();
+  });
+  test('Verifica se a pagina contem uma imagem', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const img = screen.getByRole('img');
+
+    expect(img).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import NotFound from '../components/NotFound';
+
+function renderWithRouter(component) {
+  const historyMock = createMemoryHistory();
+
+  const view = render(
+    <Router history={ historyMock }>
+      {component}
+    </Router>,
+  );
+
+  return {
+    ...view,
+    history: historyMock,
+  };
+}
+
+describe('App.js tests', () => {
+  test('Verifica se ao clicar no Home renderiza a página inicial', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    userEvent.click(home);
+
+    const homeText = screen.getByText('Encountered pokémons');
+
+    expect(homeText).toBeInTheDocument();
+  });
+  test('Verifica se ao clicar em About renderiza a página sobre a aplicação', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    userEvent.click(about);
+
+    const aboutText = screen.getByText('About Pokédex');
+
+    expect(aboutText).toBeInTheDocument();
+  });
+  test('Verifica se renderiza a página de pokémons favoritos', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+
+    userEvent.click(favorite);
+
+    const favoriteText = screen.getByText('Favorite pokémons');
+
+    expect(favoriteText).toBeInTheDocument();
+  });
+  test('Verifica se carrega pagina Not Found', () => {
+    const { history } = renderWithRouter(<NotFound />);
+
+    history.push('/rota-teste');
+
+    const notFoundText = screen.getByText('Page requested not found');
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
