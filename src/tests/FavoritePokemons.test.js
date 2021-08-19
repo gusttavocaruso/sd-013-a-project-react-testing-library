52fef43f7af06cd446d1be95eae8705257540e52
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-08-19 16:25:50 -0300

    tird requirement solved

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+
+import App from '../App';
+
+describe('test favorite pokemons page', () => {
+  it('should not have any pokemon there', () => {
+    renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoriteLink);
+
+    const emptyFavoriteText = screen.getByText(/no favorite pokemon found/i);
+    expect(emptyFavoriteText).toBeInTheDocument();
+  });
+
+  it('should have a favorite pokemon there', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const favoriteInput = screen.getByLabelText(/pokémon favoritado?/i);
+    userEvent.click(favoriteInput);
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoriteLink);
+
+    const pokemonCard = screen.getAllByRole('img');
+    expect(pokemonCard[0]).toBeInTheDocument();
+  });
+});
