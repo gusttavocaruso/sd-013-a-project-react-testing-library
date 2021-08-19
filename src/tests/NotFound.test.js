f0e2e6aba042433ec424d2021f55f67a84af2da5
Isabella Antunes <bella.antunes2@gmail.com>
2021-08-19 20:37:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Verifica o componente Not Found', () => {
+  it('Verifica se página contém um heading ', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
