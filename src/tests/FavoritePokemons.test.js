a3d12b1863733aeea0c4956af5d104e3896e5fb5
PC <sthecso>
2021-08-19 16:43:14 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from './index';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const textNotFound = screen.getByText('No favorite pokemon found');
+
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const getFavorites = screen.getByRole('heading', {
+      name: /Favorite pokémons/i,
+      level: 2,
+    });
+
+    expect(getFavorites).toBeInTheDocument();
+  });
+});
