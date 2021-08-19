3c64631bd28c6070552cafd4074dd51f0bfd2827
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-08-19 18:27:45 -0300

    Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -6,10 +6,16 @@ import NotFound from '../components/NotFound';
 describe('Teste o componente <NotFound.js />', () => {
   test('A página contém um heading h2 com o texto Page requested not found 😭', () => {
     renderWithRouter(<NotFound />);
-    const notFoundText = screen.getByRole('heading', 'img', {
-      name: /Page requested not found 😭/,
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found /,
       level: 2,
     });
     expect(notFoundText).toBeInTheDocument();
+  });
+  test('Testa se existe uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imageNotFound = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found' });
+    expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
