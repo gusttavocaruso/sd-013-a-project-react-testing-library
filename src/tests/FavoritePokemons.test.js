b7ee514bf7a1a124783925b91efee08794e5402b
thomasravache <thomasravache31@gmail.com>
2021-08-19 01:35:26 -0300

    03-É exibida na tela a mensagem "No favorite pokemon found", se não tiver pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
-import pokemons from '../data';
 
 describe('Testando componente FavoritePokemons.js', () => {
   it('É exibida na tela a mensagem "No favorite pokemon found"'
   + ', se não tiver pokemons favoritos', () => {
     renderWithRouter(<App />);
 
+    const linkFavPokemon = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    userEvent.click(linkFavPokemon);
+
+    const notFoundParagraph = screen.getByText(
+      /no favorite pokemon found/i,
+    );
+
+    expect(notFoundParagraph).toBeInTheDocument();
   });
 });
