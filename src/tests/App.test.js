c530c09f0f3d76fa67e6fc7cbd3455da3b39ace2
junglejf <jfcchagas@id.uff.br>
2021-08-18 22:16:52 -0300

    Implementado 1.1 O primeiro link deve possuir o texto Home.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,11 +3,19 @@ import { screen, render, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './util/renderWithRouter';
 
-describe("1- Testando se o topo da aplicação contém um conjunto fixo de links de navegação.", () => {
-  it("O primeiro link deve possuir o texto Home.", () => {
+describe("1 - Testando se o topo da aplicação contém um conjunto fixo de links de navegação.", () => {
+  it("1.1 - O primeiro link deve possuir o texto Home.", () => {
     renderWithRouter(<App />);
-    const navBar = screen.getByRole('link');
-    expect(navBar).toBeInTheDocument();
-    console.log(navBar);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
+  });
+  it("1.2 - A aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação", () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
   });
 });
