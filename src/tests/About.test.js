84e10dedf5e696c57241163417c412d5a68b17fd
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-19 10:16:24 -0300

    Corrigindo packge-lock

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,29 +2,31 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
-test('A página contém um heading h2', () => {
-  render(<About />);
+describe('Requisito 2', () => {
+  test('A página contém um heading h2', () => {
+    render(<About />);
 
-  const info = screen.getByRole('heading', {
-    name: 'About Pokédex',
-    level: 2,
+    const info = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(info).toBeInTheDocument();
   });
-  expect(info).toBeInTheDocument();
-});
 
-test('A página contém dois parágrafos', () => {
-  render(<About />);
+  test('A página contém dois parágrafos', () => {
+    render(<About />);
 
-  const textParam = screen.getByText(/This application simulates a Pokédex/);
-  expect(textParam).toBeInTheDocument();
+    const textParam = screen.getByText(/This application simulates a Pokédex/);
+    expect(textParam).toBeInTheDocument();
 
-  const textParam2 = screen.getByText(/One can filter Pokémons by type/);
-  expect(textParam2).toBeInTheDocument();
-});
+    const textParam2 = screen.getByText(/One can filter Pokémons by type/);
+    expect(textParam2).toBeInTheDocument();
+  });
 
-test('A página contém a seguinte imagem de uma Pokédex:', () => {
-  render(<About />);
+  test('A página contém a seguinte imagem de uma Pokédex:', () => {
+    render(<About />);
 
-  const img = screen.getByRole('img');
-  expect(img.src).toEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    const img = screen.getByRole('img');
+    expect(img.src).toEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,67 +5,69 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 
-it('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
+describe('Requisito 1', () => {
+  it('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
 
-  const navHome = screen.getByText('Home');
+    const navHome = screen.getByText('Home');
 
-  const navAbout = screen.getByText('About');
+    const navAbout = screen.getByText('About');
 
-  const navFav = screen.getByText('Favorite Pokémons');
+    const navFav = screen.getByText('Favorite Pokémons');
 
-  expect(navHome).toBeInTheDocument(/home/i);
-  expect(navAbout).toBeInTheDocument(/about/i);
-  expect(navFav).toBeInTheDocument(/favorite pokémons/i);
-});
-
-it('A aplicação é redirecionada para a página inicial', () => {
-  const { history } = renderWithRouter(<App />);
-
-  const navHome = screen.getByRole('link', {
-    name: /home/i,
-  });
-  userEvent.click(navHome);
-  expect(navHome).toBeInTheDocument();
-  expect(history.location.pathname).toBe('/');
-});
-
-it('A aplicação é redirecionada para a página de About', () => {
-  const { history } = renderWithRouter(<App />);
-
-  const navAbout = screen.getByRole('link', {
-    name: /About/i,
-  });
-  userEvent.click(navAbout);
-  expect(navAbout).toBeInTheDocument();
-  expect(history.location.pathname).toBe('/about');
-});
-
-it('A aplicação é redirecionada para a página de Pokémons Favoritados', () => {
-  const { history } = renderWithRouter(<App />);
-
-  const navFav = screen.getByRole('link', {
-    name: /favorite pokémons/i,
-  });
-  userEvent.click(navFav);
-  expect(navFav).toBeInTheDocument();
-  expect(history.location.pathname).toBe('/favorites');
-});
-
-it('A aplicação é redirecionada para a página Not Found', () => {
-  const { history } = renderWithRouter(<App />);
-
-  history.push('/rota-que-nao-existe');
-
-  const notFound = screen.getByRole('heading', {
-    name: /Page requested not found /i,
-    level: 2,
+    expect(navHome).toBeInTheDocument(/home/i);
+    expect(navAbout).toBeInTheDocument(/about/i);
+    expect(navFav).toBeInTheDocument(/favorite pokémons/i);
   });
 
-  expect(history.location.pathname).toBe('/rota-que-nao-existe');
-  expect(notFound).toBeInTheDocument();
+  it('A aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const navHome = screen.getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(navHome);
+    expect(navHome).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('A aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const navAbout = screen.getByRole('link', {
+      name: /About/i,
+    });
+    userEvent.click(navAbout);
+    expect(navAbout).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('A aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const navFav = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    userEvent.click(navFav);
+    expect(navFav).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('A aplicação é redirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/rota-que-nao-existe');
+
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found /i,
+      level: 2,
+    });
+
+    expect(history.location.pathname).toBe('/rota-que-nao-existe');
+    expect(notFound).toBeInTheDocument();
+  });
 });
