9303b33fd484c7ecd07c60f6aeca8514ca91b31c
thomasravache <thomasravache31@gmail.com>
2021-08-19 00:38:00 -0300

    02-Deve conter a imagem de uma pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -15,4 +15,34 @@ describe('Testando o componente About.js', () => {
 
     expect(h2Text).toBeInTheDocument();
   });
+
+  it('Deve conter dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+
+    const paragraph1 = screen.getByText(
+      /This application simulates a Pokédex, a digital encyclopedia/i,
+    );
+    const paragraph2 = screen.getByText(
+      /One can filter Pokémons by type, and see more details/i,
+    );
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('Deve conter a imagem de uma pokédex', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+
+    const pokeDexImg = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(pokeDexImg.src).toBe(
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
 });
