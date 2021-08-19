7a6dfbef21245eaad184496c6ebcc652fc1f5db0
turtore <brunosap@gmail.com>
2021-08-18 22:51:33 -0300

    atende requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,63 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { from } from 'array-flatten';
+import React from 'react';
+// import testUtils from 'react-dom/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+// import { createMemoryHistory } from 'history';
+import { About } from '../components';
+
+describe('Teste About.js', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const pokedexInfo = screen.getByText(/About Pokédex/i);
+
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const headerPokemon = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+
+    expect(headerPokemon).toBeInTheDocument();
+  });
+});
+
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  render(
+    <BrowserRouter>
+      <About />
+    </BrowserRouter>,
+  );
+
+  const paragraphPokemon = screen.getByRole('heading', {
+    name: / /i,
+  });
+
+  expect(paragraphPokemon).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+  render(
+    <BrowserRouter>
+      <About />
+    </BrowserRouter>,
+  );
+
+  const imgPokemon = screen.getByRole('img');
+
+  expect(imgPokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+// import React from 'react';
+
+// describe('FavoritePokemons.js testes', () => {
+//   test('este se é exibido na tela a mensagem No favorite pokemon found.', () => {});
+
+//   test('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
+// });
