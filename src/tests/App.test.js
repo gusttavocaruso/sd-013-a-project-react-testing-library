97ca0e073e02ef352454c2602d003ad23ef4bd37
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-19 02:47:06 -0300

    Requisito 1.1.1 1.1.2 1.1.3 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { within, render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe(' Teste o componente "<App.js />"', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const expected = ['Home', 'About', 'Favorite Pokémons'];
+
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const navBar = screen.getByRole('navigation');
+    const listTextContent = within(navBar).getAllByRole('link')
+      .map((i) => i.textContent);
+    expect(listTextContent).toStrictEqual(expected);
+  });
+});
