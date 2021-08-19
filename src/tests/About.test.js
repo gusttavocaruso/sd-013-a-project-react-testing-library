4570d8cf39262fe01122a772d92dd1a374d054d6
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-08-19 19:44:54 -0400

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,29 @@
-// Iniciando o projeto
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testa About.js.', () => {
+  test('A página deve conter um heading h2 com o texto About Pokédex.', () => {
+    render(<About />);
+    const aboutPokedex = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('A página deve conter 2 parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+    const paragraphOne = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragraphOne).toBeInTheDocument();
+    const paragraphTwo = screen.getByText(/One can filter Pokémons by type/i);
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
+  test('A página deve conter a seguinte imagem de uma Pokédex:', () => {
+    render(<About />);
+    const imgPokedex = screen.getByRole('img');
+    expect(imgPokedex).toBeInTheDocument();
+    expect(imgPokedex.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
