5c7a41cc853ab594da44cc70d24a0e71a6543f70
Japa <wesleyy_@live.com>
2021-08-19 18:44:15 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando topo da aplicação', () => {
+  it('deve renderizar o texto Home', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundPokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByText(/More details/i);
+    fireEvent.click(moreDetails);
+
+    const clickCheckbox = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    fireEvent.click(clickCheckbox);
+    expect(clickCheckbox.checked).toBe(true);
+
+    const favoritePokemons = screen.getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoritePokemons);
+    const namePokemon = screen.getByText(/Pikachu/i);
+    expect(namePokemon).toBeInTheDocument();
+  });
+});
