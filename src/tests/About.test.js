fd83d2ea24dfec761034dbb559dec020bbdef7af
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-08-19 04:19:58 -0300

    Atende ao requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Verifica componente About', () => {
+  test('Verifica se a tag <h2> está sendo renderizado', () => {
+    render(<About />);
+    const aboutText = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('Verifica se os dois paragrafos estão sendo renderizados', () => {
+    render(<About />);
+    const firstParagraph = screen.getByTestId('first paragraph');
+    expect(firstParagraph).toBeInTheDocument();
+
+    const secondParagraph = screen.getByTestId('second paragraph');
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Verifica se a imagem correta esta sendo renderizada', () => {
+    render(<About />);
+    const imgAbout = screen.getByRole('img', { name: 'Pokédex' });
+    const srcImgAbout = imgAbout.src.includes('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(imgAbout).toBeInTheDocument();
+    expect(srcImgAbout).toBe(true);
+  });
+});
+
+// No momento de testar a src, fiquei na vida entre toBe, toBeTruthy, e toBeTrue, pesquisei no stackoverflow, e fui testando ate um que aparetemente deu certo
+
+// https://qastack.com.br/programming/32615713/tobetrue-vs-tobetruthy-vs-tobetrue
