4a51846619d5b953b6a9d295b53f29d47197b185
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-08-19 11:26:14 -0400

    Cria função renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '.App./';
+
+describe('Testa se há links  de navegação no topo e se os mesmos redirecionam a aplicação ',
+  () => {
+    test('O primeiro link deve possuir o texto Home', () => {
+      // acessar os elementos da tela
+      render(<App />);
+      const
+      // interagir com eles (se houver necessidade)
+      // Fazer o teste
+
+    })
+  });
