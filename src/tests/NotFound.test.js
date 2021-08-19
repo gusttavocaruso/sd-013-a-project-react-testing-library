17f8a73f37e77d06d5179a180f7a293e589805c7
Diego Vilela <vilelaphp@gmail.com>
2021-08-19 11:37:47 -0300

    Test <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const getH2 = (text) => screen.getByRole('heading', {
+  name: text,
+  level: 2,
+});
+
+describe('Test NotFound.js', () => {
+  it('shows not found page when a user requests a page that doesn\'t exist', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/does-not-exist');
+
+    const heading = getH2('Page requested not found Crying emoji');
+    expect(heading).toBeInTheDocument();
+
+    const img = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
