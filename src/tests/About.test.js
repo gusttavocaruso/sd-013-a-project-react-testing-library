755371be5dbe7cac7814c949290cf35727c02f09
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-08-19 00:38:17 -0300

    resolvendo erro do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -66,6 +66,7 @@ describe('About.js tests', () => {
 
     const img = screen.getByRole('img');
 
-    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
