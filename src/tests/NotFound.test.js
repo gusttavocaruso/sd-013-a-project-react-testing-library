9d46172a302e6f47406ea2832c689ac820bfe92f
thomasravache <thomasravache31@gmail.com>
2021-08-19 02:25:59 -0300

    04-Deve conter um h2 com o texto "Page requested not found"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testando componente NotFound.js', () => {
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/url-nao-existente');
+
+    const notFoundMessage = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+});
