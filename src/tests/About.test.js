3732d6969a5af7813066898942eb571a60a09ab7
thomasravache <thomasravache31@gmail.com>
2021-08-18 23:58:36 -0300

    02-Deve conter um h2 com o texto "About Pokédex"

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente About.js', () => {
+  it('Deve conter um h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+
+    const h2Text = screen.getByRole('heading', { name: /about pokédex/i });
+
+    expect(h2Text).toBeInTheDocument();
+  });
+});
