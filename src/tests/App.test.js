e0162d6775a90f322562655c23a6093f0b4b50d4
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-19 03:08:43 -0300

    Melhorando o requisito 1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,17 +4,29 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 describe(' Teste o componente "<App.js />"', () => {
-  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    const expected = ['Home', 'About', 'Favorite Pokémons'];
-
+  describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    const exepected = 3;
     render(
       <BrowserRouter>
         <App />
       </BrowserRouter>,
     );
-    const navBar = screen.getByRole('navigation');
-    const listTextContent = within(navBar).getAllByRole('link')
-      .map((i) => i.textContent);
-    expect(listTextContent).toStrictEqual(expected);
+
+    const navBarElement = screen.getByRole('navigation');
+    const listTextContent = within(navBarElement).getAllByRole('link')
+      .map((link) => link.textContent);
+
+    it('Testa se o navBar tem 3 link', () => {
+      expect(listTextContent.length).toBe(exepected);
+    });
+    it('O primeiro link deve possuir o texto Home.', () => {
+      expect(listTextContent[0]).toBe('Home');
+    });
+    it('O primeiro link deve possuir o texto About.', () => {
+      expect(listTextContent[1]).toBe('About');
+    });
+    it('O primeiro link deve possuir o texto Favorite Pokémons.', () => {
+      expect(listTextContent[2]).toBe('Favorite Pokémons');
+    });
   });
 });
