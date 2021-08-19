76c7a756418bd144c9ba72128b8c312ac7dab868
thomasravache <thomasravache31@gmail.com>
2021-08-19 02:50:23 -0300

    05-Deve conter um h2 com o texto "Encoutered pokémons"

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testando componente Pokedex.js', () => {
+  it('Deve conter um h2 com o texto "Encoutered pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const h2 = screen.getByRole('heading', { name: /encountered pokémons/i });
+
+    expect(h2).toBeInTheDocument();
+  });
+});
