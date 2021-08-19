d0b5de753e0ec368836147ad03346636716b0019
junglejf <jfcchagas@id.uff.br>
2021-08-19 01:34:46 -0300

    Requisito 2 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Teste o componente <About.js />.', () => {
+  it('A página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const h2 = screen.queryByText(/About Pokédex/i);
+    expect(h2).toContainHTML('</h2>');
+  });
+  it('A página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+    const h2 = screen.queryAllByText(/Pokédex/i);
+    expect(h2.length).toBe(2);
+  });
+  it('A página contém a seguinte imagem de uma Pokédex.', () => {
+    render(<About />);
+    const h2 = screen.queryAllByRole('img');
+    h2.find((tag) => expect(tag).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'));
+  });
+});
