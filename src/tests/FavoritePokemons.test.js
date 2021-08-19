d9c3900fc443aba9c6ca5ea5cda250651dc8404a
turtore <brunosap@gmail.com>
2021-08-18 23:25:54 -0300

    atende requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,18 @@
-// import React from 'react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
 
-// describe('FavoritePokemons.js testes', () => {
-//   test('este se é exibido na tela a mensagem No favorite pokemon found.', () => {});
+describe('FavoritePokemons.js testes', () => {
+  test('este se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+    const noFavorites = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  });
 
-//   test('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
-// });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('NotFound.js testes', () => {
+  test('Teste se página contém um heading h2 com  Page requested not found', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const headingNotFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(headingNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const notFoundImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
