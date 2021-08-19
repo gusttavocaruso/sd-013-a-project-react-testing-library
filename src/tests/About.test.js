9f695cd1300bcce3c0144dbf887ee6bc0e25f6e4
Sethano <vitorcard1720@gmail.com>
2021-08-19 17:54:55 -0300

    First steps

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+describe('About.js` Tests', () => {
+  it('Checks if it cotains a heading level 2 with text "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const Text = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(Text).toBeInTheDocument();
+  });
+
+  it('Checks if there are two paragraphs about the Pokédex', () => {
+    renderWithRouter(<About />);
+    const firstP = /This application simulates a Pokédex, a digital encyclopedia/i;
+    const secondP = /One can filter Pokémons by type, and see more details for each one/i;
+    const pOne = screen.getByText(firstP);
+    expect(pOne).toBeInTheDocument();
+    const pTwo = screen.getByText(secondP);
+    expect(pTwo).toBeInTheDocument();
+  });
+
+  it('Checks if there are a specific image on the screen', () => {
+    renderWithRouter(<About />);
+    const alt = screen.getByAltText('Pokédex');
+    const src = alt.src.includes('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(src).toBeTruthy();
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('App.js` tests', () => {
+  it('Checks if the "home" link redirects to "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Home' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Checks if the "About" link redirects to "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'About' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Checks if the "Favorite Pokemon" link redirects to "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
