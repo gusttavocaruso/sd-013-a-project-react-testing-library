60e1a41e5183d783d4beb91b4e5be9d1a10cb602
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2021-08-19 17:06:02 -0300

    Resolve o requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,55 @@
 test('', () => {});
+
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App.js', () => {
+  it('Se o primeiro link possui o texto "Home" e redireciona para a URL certa',
+     () => {
+      const { history } = renderWithRouter(<App />);
+
+      const home = screen.getByRole('link', { name: /home/i });
+      expect(home).toBeDefined();
+
+      userEvent.click(home);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+  it('Verifica se o segundo link possui o texto "About" e se ao clicar a aplicação é redirecionada para a URL certa',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const about = screen.getByRole('link', { name: /about/i });
+      expect(about).toBeDefined();
+
+      userEvent.click(about);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/about');
+    });
+  it('Verifica se o terceiro link possui o texto "Favorite Pokémons" e se ao clicar a aplicação é redirecionada para a URL certa',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+      expect(favorites).toBeDefined();
+
+      userEvent.click(favorites);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+  it('Verifica se a aplicação é redirecionada para a página "Not Found" ao entrar em uma URL desconhecida',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('/qualquer-caminho');
+
+      const notFound = screen.getByText('Page requested not found');
+      expect(notFound).toBeDefined();
+    });
+});
