e0940c0b53e9ef955b2d0c895584060586525c51
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-19 17:56:56 -0300

    Iniciando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Requisito 1 - App.js', () => {
+  test('Verifica se renderiza o link "Home"', () => {
+    render(<App />);
+    expect(screen.queryByText(/home/i)).toBeDefined();
+  });
+
+  test('Verifica se renderiza o link "About"', () => {
+    render(<App />);
+    const linkForAbout = screen.queryByText(/about/i);
+    expect(linkForAbout).toBeDefined();
+  });
+
+  test('Verifica se renderiza o link "Favorite Pokemons"', () => {
+    render(<App />);
+    const linkForFavPokemons = screen.queryByText(/favorite/i);
+    expect(linkForFavPokemons).toBeDefined();
+  });
+});
