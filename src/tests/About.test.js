ebf18f1dc3e095ba4152c8c591efb0597676bd86
Jackson Santana <onjacksm@gmail.com>
2021-08-19 16:17:35 -0300

    Adiciona funçao renderWithRouter e requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,45 @@
-test('', () => {});
-// Começando o projeto
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../types/renderWithRouter';
+
+describe('Testa o componente About', () => {
+  test('Testa se About contém informações sobre a Pokédex por parágrafos', () => {
+    renderWithRouter(<About />);
+
+    const infoPokedexOne = screen.getByText(/digital encyclopedia/i);
+    const infoPokedextwo = screen.getByText(/filter pokémons by type/i);
+
+    expect(infoPokedexOne).toBeInTheDocument();
+    expect(infoPokedextwo).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém um heading h2', () => {
+    renderWithRouter(<About />);
+
+    const aboutHeading = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+
+    expect(aboutHeading).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    // O teste quebrava falando que esperava receber um array, para receber array se usa getAllBy
+    // Referência: https://testing-library.com/docs/queries/about
+    const allPara = screen.getAllByText(/digital encyclopedia|filter pokémons by type/i);
+
+    expect(allPara).toHaveLength(2);
+  });
+});
+
+test('Testa se a página contém uma imagem de uma Pokédex', () => {
+  renderWithRouter(<About />);
+
+  const pokedexImg = screen.getByRole('img');
+  // Referência, Matheus Camillo pensou nessa possibilidade em call
+  expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import App from '../App';
+import renderWithRouter from '../types/renderWithRouter';
 
 describe('Testa Home, About, Favorite', () => {
   test('Testa se existe o link para Home, About, Favorite', () => {
@@ -13,16 +14,19 @@ describe('Testa Home, About, Favorite', () => {
       </BrowserRouter>,
     );
 
+    // Testa a existência do Link Home
     const homeLink = screen.getByRole('link', {
       name: /home/i,
     });
     expect(homeLink).toBeInTheDocument();
 
+    // Testa a existência do Link About
     const aboutLink = screen.getByRole('link', {
       name: /about/i,
     });
     expect(aboutLink).toBeInTheDocument();
 
+    // Testa a existência do Link Favorite Pokémon
     const favPokeLink = screen.getByRole('link', {
       name: /favorite pokémons/i,
     });
@@ -36,6 +40,7 @@ describe('Testa Home, About, Favorite', () => {
       </BrowserRouter>,
     );
 
+    // Testa o redirecionamento para Home
     const homeLink = screen.getByRole('link', {
       name: /home/i,
     });
@@ -47,6 +52,7 @@ describe('Testa Home, About, Favorite', () => {
     });
     expect(homeRedirect).toBeInTheDocument();
 
+    // Testa o redirecionamento para About
     const aboutLink = screen.getByRole('link', {
       name: /about/i,
     });
@@ -58,6 +64,7 @@ describe('Testa Home, About, Favorite', () => {
     });
     expect(aboutRedirect).toBeInTheDocument();
 
+    // Testa o redirecionamento para Favorite Pokémon
     const favLink = screen.getByRole('link', {
       name: /favorite pokémons/i,
     });
@@ -72,15 +79,9 @@ describe('Testa Home, About, Favorite', () => {
 });
 
 test(('Testa redirecionamento da Page Not Found'), () => {
-  const historyMock = createMemoryHistory();
+  const { history } = renderWithRouter(<App />);
 
-  render(
-    <Router history={ historyMock }>
-      <App />
-    </Router>,
-  );
-
-  historyMock.push('/rota-inexistente');
+  history.push('/rota-inexistente');
 
   const pageNFound = screen.getByRole('heading', {
     name: /page requested not found/i,
