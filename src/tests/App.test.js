360e640f40a86993db42634680f2aa213659695b
Daniel <daniel.melo.oliveira27@gmail.com>
2021-08-20 02:55:15 -0300

    requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,48 @@
-test('a', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes para App.js', () => {
+  it('Verifica se a header possui os links', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritePokemonLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favoritePokemonLink).toBeInTheDocument();
+  });
+
+  it('Verifica se redireciona para "/" quando clica em "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se redireciona para "/about" quando clica em "About"', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica se redireciona para "/favorites" ao clicar em "Favorite Pokémons"', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
