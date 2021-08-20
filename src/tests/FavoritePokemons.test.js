4c45f8509eb30630124b0ba7f6b8263b001e3ba0
FelipeNevess <felipe012neves@gmail.com>
2021-08-20 11:46:26 -0300

    FIZ O PUSH

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <FavoritePokemons />
+      </Router>,
+    );
+  });
+});
