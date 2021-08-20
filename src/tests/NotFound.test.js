6e584eaca19c5334e2f39c74dcdc75b7b4603e79
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-20 16:34:32 -0300

    Requisito 4 - NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Requisito 4 - NotFound.js', () => {
+  test('Verfica se a página contém um heading com texto especifico', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/rota-que-nao-existe');
+    expect(screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+      exact: false,
+    })).toBeInTheDocument();
+  });
+
+  test('Verifica se renderiza imagem do Pikachu chorando', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/rota-que-nao-existe');
+    const pikachuCryImg = screen.getByRole('img', {
+      name: /pikachu crying/i,
+      exact: false,
+    });
+    const pathImagePikachuCryng = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(pikachuCryImg).toHaveAttribute('src', pathImagePikachuCryng);
+  });
+});
