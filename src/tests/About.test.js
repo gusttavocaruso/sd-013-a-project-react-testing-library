c661a9618de826812f12787ad4865460f7cd4389
Gabriel Priss <gabrielpriss@hotmail.com>
2021-08-20 15:10:35 -0300

    primeiros requisitos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testa componente "About"', () => {
+  it('faz o teste da bagaça', () => {
+    render(<About />);
+    const heading = screen.getByRole('heading');
+    expect(heading.textContent).toBe('About Pokédex');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,35 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import App from '../App';
 
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
+
+const home = screen.getByText('Home');
+const about = screen.getByText('About');
+const favorite = screen.getAllByText('Favorite Pokémons');
+
+describe('Testa Home', () => {
+  it('testa o link "Home" se possui o texto "Home"', () => {
+    expect(home.textContent).toBe('Home');
+    const heading = screen.getByRole('heading');
+    expect(heading.textContent).toBe('About Pokédex');
+  });
+  it('testa o link "Home" se direciona para "/"', () => {
+    expect(home.getAttribute('href')).toBe('/');
+  });
+});
+describe('Testa About', () => {
+  it('testa o link "About" se possui o texto "About"', () => {
+    expect(about.textContent).toBe('About');
+    const heading = screen.getByRole('heading');
+    expect(heading.textContent).toBe('About Pokédex');
+  });
+  it('testa o link "Home" se direciona para "/"', () => {
+    expect(home.getAttribute('href')).toBe('/');
+  });
+});
