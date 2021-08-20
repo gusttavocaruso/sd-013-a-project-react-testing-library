3968b0f3da20f57a895936b8a9dd1d2fe8162a29
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-20 01:25:54 -0300

    Requisito 3 Ok

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { FavoritePokemons } from '../components';
+
+const mockFavoritePoke = [{
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Kanto Viridian Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    },
+    {
+      location: 'Kanto Power Plant',
+      map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    },
+  ],
+  summary: 'This intelligent Pokémon roasts hard berries with electricity to make'
+  + 'them tender enough to eat.',
+}];
+
+describe('Teste o componente "<FavoritePokemons.js />"', () => {
+  it('Teste se é exibido na tela a mensagem "No favorite pokemon found", se a'
+  + ' pessoa não tiver pokémons favoritos.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <FavoritePokemons pokemons={ [] } />
+      </Router>,
+    );
+    const noFavoritePokemon = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <FavoritePokemons pokemons={ mockFavoritePoke } />
+      </Router>,
+    );
+    const favoritePokemon = screen.getByText(/Pikachu/i);
+    expect(favoritePokemon).toBeInTheDocument();
+  });
+});
