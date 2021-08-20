7b6ddf967e34a5f6a4c6da3e436e29fbe8528c7a
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-08-19 22:57:47 -0300

    Atende ao Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica o componente FavoritePokemons', () => {
+  test('Verifica mensagem exibida na tela', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const msgNotFavorite = screen.getByText('No favorite pokemon found');
+    expect(msgNotFavorite).toBeInTheDocument();
+  });
+
+  test('Verifica se renderiza os Pokemons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', {
+      name: 'More details' });
+    userEvent.click(moreDetails);
+
+    const pokemonFavorite = screen.getByRole('checkbox');
+    userEvent.click(pokemonFavorite);
+
+    const pageFavorite = screen.getByRole('link', {
+      name: 'Favorite Pokémons' });
+    userEvent.click(pageFavorite);
+
+    expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
+  });
+});
+
+// Apos finalizar os testes percebi que poderia ter encapsulado tudo dentro do userEvent, porem apos concluir pelo menos os 80%, tentarei refatorar. (Claro se passar no avaliador).
