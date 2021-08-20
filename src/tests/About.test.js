07cddcab7f75ca341b66817f0a214fb42fe770de
Murilo Ribeiro <murilorsv14@gmail.com>
2021-08-20 11:56:27 -0300

    fix lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import renderWithRouter from '../helper/renderWithRouter';
 import About from '../components/About';
 
