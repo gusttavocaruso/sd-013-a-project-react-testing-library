7cf990b14675e710d287e6f6ea04d0e26034eff5
Daniel <daniel.melo.oliveira27@gmail.com>
2021-08-20 14:38:58 -0300

    requisitos 05 e 06

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,69 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes para Pokedex', () => {
+  it('Verifica se renderiza um "h2" com a mensagem', () => {
+    renderWithRouter(<App />);
+    const h2 = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Verifica se exibe o proximo pokemon ao clicar no botão', () => {
+    renderWithRouter(<App />);
+    const nextBtn = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    const pokemon = screen.getByTestId('pokemon-name');
+
+    expect(pokemon.textContent).toBe(pokemons[0].name);
+
+    userEvent.click(nextBtn);
+    expect(pokemon.textContent).toBe(pokemons[1].name);
+    userEvent.click(nextBtn);
+    expect(pokemon.textContent).toBe(pokemons[2].name);
+  });
+
+  it('Verifica se apenas um pokemon é exibido na tela', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getAllByTestId(/pokemon-name/i);
+    expect(pokemonName).toHaveLength(1);
+  });
+
+  it('Verfica se existem filtros ', () => {
+    renderWithRouter(<App />);
+    const filters = [
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+    const btn = screen.getAllByTestId('pokemon-type-button');
+    const allBtn = screen.getByText(/All/);
+    btn.forEach((button, index) => expect(button).toHaveTextContent(filters[index]));
+    expect(allBtn).toBeInTheDocument();
+  });
+
+  it('Testa se tem um botao que reseta o filtro', () => {
+    renderWithRouter(<App />);
+    const allBtn = screen.getByText(/All/);
+    expect(allBtn).toBeInTheDocument();
+    const nextBtn = screen.getByText(/Próximo pokémon/);
+    expect(nextBtn).toBeInTheDocument();
+    const pikachu = screen.getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+    userEvent.click(nextBtn);
+    const charmander = screen.getByText(/Charmander/i);
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(allBtn);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes para PokemonDetails', () => {
+  it('Verifica se renderiza um card com as informações do pokemon', () => {
+    renderWithRouter(<App />);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeName.innerHTML).toBe('Pikachu');
+
+    const pokeType = screen.getByTestId('pokemon-type');
+    expect(pokeType).toBeInTheDocument();
+    expect(pokeType.innerHTML).toBe('Electric');
+
+    const weight = screen.getByTestId('pokemon-weight');
+    expect(weight).toBeInTheDocument();
+    expect(weight.innerHTML).toBe('Average weight: 6.0 kg');
+
+    const image = screen.getByAltText('Pikachu sprite');
+    expect(image).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    );
+    expect(image).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+
+  it('Verifica se redireciona para "/pokemons/id" ao clicar ', () => {
+    const { history } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('ícone de estrela no poḱemon favoritado', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByText('More details');
+    userEvent.click(moreDetails);
+    const fav = screen.getByRole('checkbox');
+    userEvent.click(fav);
+    const img = screen.getByAltText('Pikachu is marked as favorite');
+    expect(img)
+      .toHaveProperty('src', 'http://localhost/star-icon.svg');
+  });
+});
