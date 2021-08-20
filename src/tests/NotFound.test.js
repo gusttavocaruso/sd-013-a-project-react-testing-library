8d271d55b908009a5d328f63583f028f26aa167c
Jéssica Queiroz <gnr0034@gmail.com>
2021-08-20 15:53:54 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('NotFound.js Tests', () => {
+  test('test if the page contains a heading h2 with the text Page requested...', () => {
+    // Acess screen elements
+    render(<NotFound />);
+    const titleH2 = screen.getByRole('heading', { name: /Page requested not found/ });
+    expect(titleH2).toBeInTheDocument();
+  });
+
+  test('test if the page contains an image with the adress urlImg', () => {
+    render(<NotFound />);
+    const urlImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const altText = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByAltText(altText);
+    expect(image.src).toStrictEqual(urlImg);
+  });
+});
