41ae58542a248cc1fbfba5f2b1b532df3c881267
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-08-20 10:50:38 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('FavoritePokemons.js tests', () => {
+  test('É exibido na tela "no favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const message = screen.getByText(/No favorite pokemon found/i);
+
+    expect(message).toBeInTheDocument();
+  });
+
+  test('É exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const pokemon1 = screen.queryAllByTestId('pokemon-name');
+
+    expect(pokemon1).toBeDefined();
+  });
+});
