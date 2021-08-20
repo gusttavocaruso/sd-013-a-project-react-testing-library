039fc5eed13f52370b15c055f71b2c2eec76b6ed
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-20 02:58:40 -0300

    desafio3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('testa componente Favorite Pokemons.js', () => {
+  it('testa se é exibida a msg "No Favorite Pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+  it('teste se é exibido todos os cards de pokemon favoritados', () => {
+    renderWithRouter(<App />);
+    // (?)não entendi pq tem que renderizar o App e não o FavoritePokemons(?)
+    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+  });
+});
