0e7e48d4eaf58144f653cdf91663f6b7046e93ff
Luiz Casimiro <luizcasimi@gmail.com>
2021-08-21 18:48:42 -0300

    solve 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helper/RenderWithRouter';
+import About from '../components/About';
+
+describe('Testando o componente About.js', () => {
+  test('se a página contém um heading h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const heading2 = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(heading2).toBeInTheDocument();
+  });
+
+  test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraphs = screen.getAllByText(/pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  test('se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutImg = screen.getByRole('img');
+    const expectedSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(aboutImg.src).toEqual(expectedSrc);
+  });
+});
