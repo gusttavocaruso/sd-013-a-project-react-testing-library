98c8706f3b39b50d39eaf0f2e2b69605f27d5b34
DouglasDrozda <lowdouglas9712@gmail.com>
2021-08-20 21:27:25 -0300

    Feito requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { render, screen } from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import { NotFound } from '../components';
+import renderWithRouter from '../components/helper/renderWithRouter';
 
 describe('Teste o componente NotFound', () => {
   it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
     render(<NotFound />);
-    const h2 = screen.getByRole('heading', { level: 2 });
-    const msg = /requested not found/i;
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /requested not found/i,
+    });
     expect(h2).toBeInTheDocument();
-    expect(msg).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const { history } = renderWithRouter(<NotFound />);
+    history.push('/pagina/que-nao-existe/');
+
+    const urlImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagemNotFound = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(imagemNotFound.src).toBe(urlImg);
   });
 });
