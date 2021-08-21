d8d47c0188133b1098eda4063c23d741a57bbc13
FelipeNevess <felipe012neves@gmail.com>
2021-08-20 21:55:39 -0300

    Desafio 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('se página contém um heading h2 com o texto Page requested not found 😭;', () => {
+    render(
+      <NotFound />,
+    );
+
+    const h2Text = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <NotFound />
+      </Router>,
+    );
+
+    history.push('/pagina/nops');
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagem = screen.getByRole('img', {
+      name: /Pikachu crying/i,
+    });
+    expect(imagem.src).toBe(url);
+  });
+});
