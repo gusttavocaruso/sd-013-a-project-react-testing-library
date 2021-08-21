15222cea591bf8f2bfde10cb6e3af360a74ba94b
FelipeNevess <felipe012neves@gmail.com>
2021-08-20 21:01:54 -0300

    Desafio 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
@@ -12,5 +14,32 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
         <FavoritePokemons />
       </Router>,
     );
+
+    const paragrafoText = screen.getByText(/no favorite pokemon found/i);
+
+    expect(paragrafoText).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const detalhes = screen.getByText(/more details/i);
+    userEvent.click(detalhes);
+
+    const favoritado = screen.getByText(/favoritado?/i);
+    userEvent.click(favoritado);
+
+    const favorito = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    userEvent.click(favorito);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
   });
 });
