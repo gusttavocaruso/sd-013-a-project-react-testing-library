b1929cad7bd395b63a8a12ae1014a558d3c535f8
Tatiane Chiarello <tatichiarello@gmail.com>
2021-08-22 01:00:12 +0200

    finaliza requisito três

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('se é exibido "No favorite pokemon found" se não tiver pokémons favoritos', () => {
+  render(<BrowserRouter><FavoritePokemons /></BrowserRouter>);
+
+  const notFound = screen.getByText(/No favorite pokemon found/i);
+  expect(notFound).toBeVisible();
+});
+
+test('se é exibido todos os cards de pokémons favoritados', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+
+  userEvent.click(screen.getByText(/more details/i));
+  userEvent.click(screen.getByRole('checkbox', { name: /pokémon favoritado/i }));
+  userEvent.click(screen.getByText(/favorite pokémons/i));
+
+  const favoritePokemons = screen.getByAltText(/is marked as favorite/i);
+  expect(favoritePokemons).toHaveAttribute('src', '/star-icon.svg');
+});
