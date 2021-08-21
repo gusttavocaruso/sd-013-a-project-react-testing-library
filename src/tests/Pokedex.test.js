c92b665f4241d95c35b31d16fd9762218eea0dc0
andzoni <lorenzoniandre@outlook.com>
2021-08-21 18:09:21 +0100

    Start requirement 05 && finish requirement 06

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Pokedex from '../components/Pokedex';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './rederWithRouter';
+import App from '../App';
+import pokemons from '../data';
 
 describe('Testing Component Pokedex:', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-    render(<Pokedex />);
+    renderWithRouter(<App />);
 
     const pokedexText = screen.getByRole('heading', {
       name: /Encountered pokémons/i,
@@ -13,33 +16,103 @@ describe('Testing Component Pokedex:', () => {
     expect(pokedexText).toBeInTheDocument();
   });
 
-  test('Teste se é exibido o próximo Pokémon da lista quando o botão é clicado', () => {
-    render(<Pokedex />);
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão é clicado.', () => {
+    renderWithRouter(<App />);
 
     const pokeButtonNext = screen.getByRole('button', {
       name: /Próximo pokémon/i,
     });
+
     expect(pokeButtonNext).toBeInTheDocument();
 
-    // Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão;
+    const pokeId = screen.getByTestId('pokemon-name');
 
-  // O primeiro Pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último Pokémon da lista;
+    expect(pokeId).toBeInTheDocument();
+
+    pokemons.map((pokemon, index) => {
+      expect(pokeId).toHaveTextContent(pokemon.name);
+      userEvent.click(pokeButtonNext);
+      return expect(pokeId).not.toHaveTextContent(pokemons[index].name);
+    });
+
+    pokemons.forEach((pokemon, index) => {
+      if (index < pokemons.length - 1) userEvent.click(pokeButtonNext);
+    });
+
+    const lastPoke = pokemons[8].name;
+
+    expect(pokeId).toHaveTextContent(lastPoke);
+
+    const firstPoke = pokemons[0].name;
+    userEvent.click(pokeButtonNext);
+
+    expect(pokeId).toHaveTextContent(firstPoke);
   });
 
   test('Teste se é mostrado apenas um Pokémon por vez.', () => {
-    render(<Pokedex />);
+    renderWithRouter(<App />);
+
+    const pokeNext = screen.getAllByTestId('pokemon-name');
+
+    expect(pokeNext.length).toBe(1);
   });
 
   test('Teste se a Pokédex tem os botões de filtro.', () => {
-    render(<Pokedex />);
-  });
+    renderWithRouter(<App />);
 
-  test('Teste se a Pokédex contém um botão para resetar o filtro.', () => {
-    render(<Pokedex />);
+    const types = [
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+
+    // Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição.
+
+    const typeButton = screen.getAllByTestId('pokemon-type-button');
+
+    expect(typeButton.length).toBe(types.length);
+
+    // A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo;
+    // O texto do botão deve corresponder ao nome do tipo, ex. Psychic;
+
+    typeButton.map((button, index) => {
+      expect(button).toBeInTheDocument();
+      return expect(typeButton[index]).toHaveTextContent(types[index]);
+    });
+
+    // O botão All precisa estar sempre visível.
 
     const pokeButtonAll = screen.getByRole('button', {
       name: /All/i,
     });
+    expect(pokeButtonAll).toBeInTheDocument();
+  });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro.', () => {
+    renderWithRouter(<App />);
+    const theNextPoke = screen.getByTestId('pokemon-name');
+
+    // O texto do botão deve ser All;
+
+    const pokeButtonAll = screen.getByRole('button', {
+      name: /All/i,
+    });
+    expect(pokeButtonAll).toBeInTheDocument();
+
+    // A Pokedéx deverá mostrar os Pokémons normalmente (sem filtros) quando o botão All for clicado;
+
+    userEvent.click(pokeButtonAll);
+    const pokeButtonNext = screen.getByRole('button', {
+      name: /Próximo pokémon/i,
+    });
+    pokemons.forEach((pokemon, index) => {
+      expect(theNextPoke).toHaveTextContent(pokemons[index].name);
+    });
+    userEvent.click(pokeButtonNext);
     expect(pokeButtonAll).toBeInTheDocument();
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import Pokemon from '../components/Pokemon';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './rederWithRouter';
+import App from '../App';
+import pokemons from '../data';
 
 describe('Testing Component Pokemon:', () => {
+  const {
+    id,
+    name,
+    type,
+    averageWeight: {
+      value,
+      measurementUnit,
+    },
+    image,
+  } = pokemons[0];
+
   test('Teste se é renderizado um card com as informações do pokémon.', () => {
-    render(<Pokemon />);
+    renderWithRouter(<App />);
+
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent(name);
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent(type);
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+    expect(screen.getByAltText(`${name} sprite`)).toHaveAttribute('src', image);
   });
 
   test('Teste se o card do Pokémon contém um link de navegação para os detalhes.', () => {
-    render(<Pokemon />);
-  });
+    const { history } = renderWithRouter(<App />);
 
-  test('Teste o link de nav é redirecionado para detalhes de Pokémon.', () => {
-    render(<Pokemon />);
-  });
-
-  test('Teste também se a URL exibida no navegador muda para /pokemon/<id>.', () => {
-    render(<Pokemon />);
+    const pokeDetails = screen.getByRole('link', {
+      name: /More details/i,
+    });
+    expect(pokeDetails).toBeDefined();
+    userEvent.click(pokeDetails);
+    expect(history.location.pathname).toBe(`/pokemons/${id}`);
   });
 
   test('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
-    render(<Pokemon />);
+    renderWithRouter(<App />);
+    const pokeDetails = screen.getByRole('link', {
+      name: /More details/i,
+    });
+    userEvent.click(pokeDetails);
+
+    const favCheckBox = screen.getByRole('checkbox', {
+      name: 'Pokémon favoritado?',
+    });
+    userEvent.click(favCheckBox);
+
+    const starIcon = screen.getByRole('img', { name: `${name} is marked as favorite` });
+    expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
   });
 });
