5a44667cbfb1ba6443268786061a506583053b0b
junglejf <jfcchagas@id.uff.br>
2021-08-21 00:27:29 -0300

    Testes do ícone de estrela Adiconado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
 // import { favoritePokemons, pokemons, pokemonsType } from './mocks/dataFavorite';
 import renderWithRouter from './util/renderWithRouter';
 import dataPokemons from '../data';
 
+const iconSVG = '/star-icon.svg';
+afterEach(cleanup);
+
 describe('1-Teste se é renderizado um card com as informações de determinado pokémon',
   () => {
-    it('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    it('1.1 - O nome correto do Pokémon deve ser mostrado na tela', () => {
       renderWithRouter(<App />);
       const firstPokemon = screen.getByTestId('pokemon-name');
       const { name } = dataPokemons[0];
       expect(firstPokemon).toBeInTheDocument();
       expect(firstPokemon).toHaveTextContent(name);
     });
-    it('O tipo correto do pokémon deve ser mostrado na tela.', () => {
+    it('1.2 - O tipo correto do pokémon deve ser mostrado na tela.', () => {
       renderWithRouter(<App />);
       const pokemonsType = [...new Set(dataPokemons.map(({ type }) => type))];
       pokemonsType.forEach((type) => {
@@ -24,7 +27,7 @@ describe('1-Teste se é renderizado um card com as informações de determinado 
         expect(pokemonType).toHaveTextContent(type);
       });
     });
-    it('Verifica se o peso médio é exibido corretamente', () => {
+    it('1.3 - Verifica se o peso médio é exibido corretamente', () => {
       renderWithRouter(<App />);
       const pokemonWeigth = screen.getByTestId('pokemon-weight');
       expect(pokemonWeigth).toBeInTheDocument();
@@ -34,7 +37,7 @@ describe('1-Teste se é renderizado um card com as informações de determinado 
         `Average weight: ${value} ${measurementUnit}`,
       );
     });
-    it('A imagem do Pokémon deve ser exibida.', () => {
+    it('1.4 - A imagem do Pokémon deve ser exibida.', () => {
       renderWithRouter(<App />);
       const pokemon = dataPokemons[0];
       const { name, image } = pokemon;
@@ -44,7 +47,7 @@ describe('1-Teste se é renderizado um card com as informações de determinado 
     });
   });
 describe('2 - Teste se possui um link para página de detalhes', () => {
-  it('O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido', () => {
+  it('2.1 - O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido', () => {
     renderWithRouter(<App />);
     const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
     const pokemon = dataPokemons[0];
@@ -54,9 +57,9 @@ describe('2 - Teste se possui um link para página de detalhes', () => {
     expect(moreDetailsLink).toHaveAttribute('href', `/pokemons/${id}`);
   });
 
-  it('Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento'
+  it('2.2 - Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento'
   + 'da aplicação para a página de detalhes de Pokémon.', () => {
-    const { history } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     fireEvent.click(screen.getByText(/More Details/i));
     const pokemon = dataPokemons[0];
     const { name, type, averageWeight: { value, measurementUnit }, summary, foundAt } = pokemon;
@@ -80,7 +83,7 @@ describe('2 - Teste se possui um link para página de detalhes', () => {
     });
   });
 
-  it('Teste também se a URL exibida no navegador muda para /pokemon/<id>',
+  it('2.3 - Teste também se a URL exibida no navegador muda para /pokemon/<id>',
     () => {
       const { history } = renderWithRouter(<App />);
       fireEvent.click(screen.getByText(/More Details/i));
@@ -91,4 +94,27 @@ describe('2 - Teste se possui um link para página de detalhes', () => {
     });
 });
 
-
+describe('3 - Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+  it('3.1 - O ícone deve ser uma imagem com o atributo src contendo o caminho /star-icon.svg', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/More Details/i));
+    fireEvent.click(screen.getByRole('checkbox'));
+    const { name } = dataPokemons[0];
+    let starIcon = screen.getByAltText(`${name} is marked as favorite`);
+    expect(starIcon).toBeInTheDocument();
+    expect(starIcon).toHaveAttribute('src', iconSVG);
+    fireEvent.click(screen.getByText(/Home/i));
+    starIcon = screen.getByAltText(`${name} is marked as favorite`);
+    expect(starIcon).toHaveAttribute('src', iconSVG);
+  });
+  it('3.2 - A imagem deve ter o atributo alt igual a <pokemon> is marked as favorite,'
+  + 'onde <pokemon> é o nome do Pokémon exibido', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Próximo pokémon/i));
+    fireEvent.click(screen.getByText(/More Details/i));
+    fireEvent.click(screen.getByRole('checkbox'));
+    const { name } = dataPokemons[1];
+    const pokemonIMG = screen.getByAltText(`${name} is marked as favorite`);
+    expect(pokemonIMG).toBeInTheDocument();
+  });
+});
