b34302f45ae79abf7a0e87afe59359aeef05dc8c
LuizFJP <luizfjportela@gmail.com>
2021-08-21 15:56:47 -0300

    requirement 03

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test the component About', () => {
+  it('If the page have a h2 heading with the text "About Pokédex"', () => {
+    renderWithRouter(<About />);
+
+    const headingText = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    const paragraph01 = screen
+      .getByText((content) => content
+        .startsWith('This application simulates a Pokédex, a'));
+    const paragraph02 = screen.getByText(
+      'One can filter Pokémons by type, and see more details for each one of them',
+    );
+    const img = screen.getByAltText(/Pokédex/i);
+
+    expect(headingText).toBeInTheDocument();
+    expect(paragraph01).toBeInTheDocument();
+    expect(paragraph02).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test the component FavoritePokemons', () => {
+  it(`Test if it is showed in the screen the message No favorite pokemon found,
+   if the user do not have favorite pokemons`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonsNotFound = screen.getByText('No favorite pokemon found');
+    const existPokemons = screen.queryByTestId('pokemon-name');
+    expect(pokemonsNotFound).toBeInTheDocument();
+    expect(existPokemons).toBeNull();
+  });
+  it('Test if it is showed all of the favorite pokemons cards', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const favPokemon = screen.getByLabelText('Pokémon favoritado?');
+
+    userEvent.click(favPokemon);
+    expect(favPokemon).toBeChecked();
+
+    const { history: path } = renderWithRouter(<FavoritePokemons />);
+    path.push('/favorites');
+
+    const pokemon = screen.getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
+  });
+});
