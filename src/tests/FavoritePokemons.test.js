097752857c163a4c4d1e7b9095a4aa6f579d5958
Luiz Casimiro <luizcasimi@gmail.com>
2021-08-21 19:56:19 -0300

    solve 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../helper/RenderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Testando o componente FavoritePokemons.js', () => {
+  test('se é exibido "No favorite pokemon found", se não houver favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorites = screen.getByText('No favorite pokemon found');
+    expect(noFavorites).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('More details'));
+    fireEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    fireEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+  });
+});
