8d7c70a0943619fd5b04d20b670c11f657af1a31
Adriana Barreto <adri92barreto123@gmail.com>
2021-08-21 16:30:53 -0300

    Executa segundo requesito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('About.js tests', () => {
+  test('Exibe um heading com um texto', () => {
+    render(<About />);
+
+    const heading = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Exibe dois paragráfos com textos', () => {
+    render(<About />);
+    const texto1 = /This application simulates a Pokédex, a digital /i;
+    const texto2 = /One can filter Pokémons by type, and see more details/i;
+    const primeiroP = screen.getByText(texto1);
+    const segundoP = screen.getByText(texto2);
+
+    expect(primeiroP).toBeInTheDocument();
+    expect(segundoP).toBeInTheDocument();
+  });
+
+  test('Contém a imagem de uma Pokédex', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+    const href = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(image).toHaveAttribute('src', href);
+    expect(image).toBeInTheDocument();
+  });
+});
