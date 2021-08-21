4ed12021e593697dc9e79c8f7e9e2e99a92594bd
Ramon Vitor <vitorramon@hotmail.com>
2021-08-21 17:24:41 -0300

    Requisito 4 - finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "NotFound".', () => {
+  test('Testa se é exibida a mensagem "Page requested no found".', () => {
+    render(<NotFound />);
+
+    const pageNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(pageNotFound).toBeInTheDocument();
+  });
+
+  test('Testa se a imagem é exibida.', () => {
+    render(<NotFound />);
+
+    const image = screen.getByAltText(/pikachu crying/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
