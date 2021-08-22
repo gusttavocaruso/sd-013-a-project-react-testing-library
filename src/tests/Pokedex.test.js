add4aea1b45b9de2ce37dcc164e3c12141141395
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-22 16:27:20 -0300

    Começando o test do requisito 5 - 5.2.1 5.2.2 5.2.3 ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { Router as Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
 
+// Função aulixiar para os test, Agradecimento ao Josue pela Dica.
 const withRenderJosueAproves = (component) => {
   const history = createMemoryHistory();
   return { ...render(
@@ -15,6 +17,7 @@ const withRenderJosueAproves = (component) => {
   history };
 };
 
+// Props para o component de funcionar corretamente.
 const isPokemonFavoriteById = {
   4: false,
   10: false,
@@ -38,56 +41,74 @@ describe('Teste o componente "<Pokedex.js />"', () => {
 
 describe('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon'
 + ' é clicado.', () => {
-  it('O botão deve conter o texto "Próximo pokémon".', () => {
+  beforeEach(() => {
+    withRenderJosueAproves(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
   });
+
+  const bntNextPokemon = () => screen.getByTestId('next-pokemon');
+
+  it('O botão deve conter o texto "Próximo pokémon".', () => {
+    expect(bntNextPokemon()).toHaveTextContent('Próximo pokémon');
+  });
+
   it('Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar'
   + 'sucessivamente no botão.', () => {
+    let inicial = 1;
+    const stop = 3;
+    const currentPokemon = screen.getByTestId('pokemon-name');
 
+    pokemons.forEach((pokemon) => {
+      expect(currentPokemon).toHaveTextContent(pokemon.name);
+      userEvent.click(bntNextPokemon());
+      if (inicial !== stop) { inicial += 1; }
+    });
   });
 
   it('O primeiro Pokémon da lista deve ser mostrado ao clicar no botão, se estiver no'
-  + 'último Pokémon da Lista', () => {
+  + ' último Pokémon da Lista', () => {
+    const currentPokemon = screen.getByTestId('pokemon-name');
 
+    expect(currentPokemon).toHaveTextContent('Pikachu');
+
+    pokemons.forEach(() => {
+      userEvent.click(bntNextPokemon());
+    });
+
+    expect(currentPokemon).toHaveTextContent('Pikachu');
   });
 });
 
 describe('Teste se é mostrado apenas um Pokémon por vez.', () => {
   it('Testa se mostra um pokémon.', () => {
-
   });
 });
 
 describe('Teste se a Pokédex tem os botões de filtro', () => {
   it('Deve existir um botão de filtragem para cada tipo de Pokémon, sem'
   + 'repetição.', () => {
-
   });
 
   it('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos'
   + 'pokémons daquele tipo;', () => {
-
   });
 
   it('O texto do botão deve corresponder ao" nome do tipo", ex. "Psychic";', () => {
-
   });
 
   it('O botão "All" precisa estar sempre visível.', () => {
-
   });
 });
 
 describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
   it('O texto do botão deve ser All.', () => {
-
   });
 
   it('A Pokedéx deverá mostrar os Pokémons normalmente (sem filtros) quando o botão All'
   + 'for clicado.', () => {
-
   });
 
   it('Ao carregar a página, o filtro selecionado deverá ser All.', () => {
-
   });
 });
