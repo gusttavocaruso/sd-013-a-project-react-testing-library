846901cc8ba7833bc5e2e8b23368a8266a4df02f
EmmanuelBMS <acquabriall2020@gmail.com>
2021-08-22 16:14:27 -0300

    refatora requisito 1 para melhorar completamente o test, após uma revisão de coteúdo no course

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,66 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter, Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('Testa se o topo da aplicação contém um conjunto fixo de links de navegação.',
   () => {
     test('Testa se os links existem e funcionam', () => {
-      render(
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>,
-      );
+      renderWithRouter(<App />);
 
-      const home = screen.getByRole('link', {
-        name: /home/i,
-      });
-      const about = screen.getByRole('link', {
-        name: /about/i,
-      });
-      const favoritePokemons = screen.getByRole('link', {
-        name: /favorite pokémons/i,
-      });
+      const home = screen.getByRole('link', { name: 'Home' });
+
       expect(home).toBeInTheDocument();
+
+      const about = screen.getByRole('link', { name: 'About' });
+
       expect(about).toBeInTheDocument();
+
+      const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
       expect(favoritePokemons).toBeInTheDocument();
-
-      userEvent.click(home);
-      const homeText = screen.getByRole('heading', {
-        name: /encountered pokémons/i,
-        level: 2,
-      });
-      expect(homeText).toBeInTheDocument();
-
-      userEvent.click(about);
-      const aboutText = screen.getByRole('heading', {
-        name: /about pokédex/i,
-        level: 2,
-      });
-      expect(aboutText).toBeInTheDocument();
-
-      userEvent.click(favoritePokemons);
-      const favoriteText = screen.getByRole('heading', {
-        name: /favorite pokémons/i,
-        level: 2,
-      });
-      expect(favoriteText).toBeInTheDocument();
     });
-    test('Testa se ao acessar URL que não existe rendiza página não encontrada.', () => {
-      const historyMock = createMemoryHistory();
-      render(
-        <Router history={ historyMock }>
-          <App />
-        </Router>,
-      );
-      historyMock.push('/erro-404');
 
-      const notFoundText = screen.getByRole('heading', {
-        name: /page requested not found/i,
-        level: 2,
-      });
-      expect(notFoundText).toBeInTheDocument();
+    test('Testa se ao clicar em Home renderiza a Página inicial', () => {
+      const { history } = renderWithRouter(<App />);
+      userEvent.click(screen.getByRole('link', { name: 'Home' }));
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+
+    test('Testa se ao clicar em About renderiza a página About', () => {
+      const { history } = renderWithRouter(<App />);
+      userEvent.click(screen.getByRole('link', { name: 'About' }));
+      const { pathname } = history.location;
+      expect(pathname).toBe('/about');
+    });
+
+    test('Testa se ao clicar em Favorite Pokémons renderiza a página Favorites', () => {
+      const { history } = renderWithRouter(<App />);
+      userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+
+    test('Testa se ao acessar uma URL que não existe, renderiza PageNotFound', () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/erro-404');
+      const { pathname } = history.location;
+      expect(pathname).toBe('/erro-404');
+      const notFound = screen.getByAltText(
+        'Pikachu crying because the page requested was not found',
+      );
+      expect(notFound).toBeInTheDocument();
     });
   });
