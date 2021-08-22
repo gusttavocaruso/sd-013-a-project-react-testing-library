ae4db69221bd4a4ba2ede29258bd277a67b3f58d
Erick Santos <erickosantos.dev@gmail.com>
2021-08-22 18:17:11 -0300

    Inicializando projeto

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -10,7 +10,7 @@ const NotFound = () => (
     <img
       className="not-found-image"
       src={`https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`}
-      alt="Pikachu crying because the page requested was not found"
+      alt="Pikachu crying because the page requested was not found!"
     />
   </div>
 );
