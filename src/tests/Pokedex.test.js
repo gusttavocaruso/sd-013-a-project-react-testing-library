7658ed59cbfd934f712ac52b000019dbb8284ed1
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-08-22 19:39:08 -0300

    Requisito 5 feito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import apiPokemons from '../data';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    renderWithRouter(<App />);
+
+    const headingText = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+
+  test('Se é exibido o próximo Pokémon quando o botão Próximo pokémon é clicado.', () => {
+    renderWithRouter(<App />);
+
+    const nextButton = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(nextButton).toBeInTheDocument();
+
+    const pokeNames = apiPokemons.map((pokemon) => pokemon.name);
+    const pokeTestID = screen.getByTestId('pokemon-name');
+
+    for (let index = 0; index <= pokeNames.length; index += 1) {
+      const nove = 9;
+      if (index === nove) {
+        expect(pokeTestID).toHaveTextContent(pokeNames[0]);
+      } else {
+        expect(pokeTestID).toHaveTextContent(pokeNames[index]);
+        userEvent.click(nextButton);
+      }
+    }
+  });
+
+  test('Se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(<App />);
+    const pokeTestID = screen.getAllByTestId('pokemon-name');
+    expect(pokeTestID).toHaveLength(1);
+  });
+
+  test('Se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+
+    const pokeTypes = apiPokemons.map((pokemon) => pokemon.type);
+    const reduced = [...new Set(pokeTypes.reduce((acc, cur) => [...acc, cur], []))];
+
+    const allButtons = screen.getAllByTestId('pokemon-type-button');
+
+    reduced.forEach((redu, index) => {
+      expect(allButtons[index]).toHaveTextContent(redu);
+
+      const allButton = screen.getByRole('button', {
+        name: 'All',
+      });
+      expect(allButton).toBeInTheDocument();
+    });
+  });
+});
