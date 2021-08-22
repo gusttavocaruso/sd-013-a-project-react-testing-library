734428489298196570ad4a7840733ced8ef4ca89
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-22 12:24:46 -0300

    desafio5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,12 +3,10 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
-// import Pokemons from '../data';
 
 describe('teste o component Pokedex.js', () => {
   it('teste se há um h2 com o texto "Encountered pokémons"', () => {
-    // so passa se renderizar o <App /> (?)
-    renderWithRouter(<App />);
+    renderWithRouter(<App />); // so passa se renderizar o <App /> (?)
     const h2 = screen.getByRole('heading', {
       name: 'Encountered pokémons',
       classe: 2,
@@ -24,8 +22,7 @@ describe('teste o component Pokedex.js', () => {
     expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
   });
   it('o 1º pokémon da lista deve ser mostrado ao clicar no ultimo da lista', () => {
-    renderWithRouter(<App />);
-    // a partir do codigo da Aline Hoshino.
+    renderWithRouter(<App />); // a partir do codigo da Aline Hoshino.
     const lestButton = screen.getByRole('button', { name: 'Próximo pokémon' });
     expect(lestButton).toBeInTheDocument();
     const pikachu = screen.getByText(/pikachu/i);
@@ -57,48 +54,56 @@ describe('teste o component Pokedex.js', () => {
     userEvent.click(lestButton);
     const PikachuReestart = screen.getByText(/Pikachu/i);
     expect(PikachuReestart).toBeInTheDocument();
-    // const dragonair = screen.getByText('Dragonair');
-    // expect(dragonair).toBeInTheDocument();
-    // const pikachu = screen.getByText(/pikachu/i);
-    // userEvent.click(lestButton);
-    // expect(pikachu).toBeInTheDocument();
-    // expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
   });
   it('testa se é mostrado um pokémon por vez', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(<App />); // a partir do cod. de Aline Hoshino
     const allThePokemons = screen.getAllByTestId(/pokemon-name/i);
     expect(allThePokemons).toHaveLength(1);
   });
   it('Os botão, devem existir e corresponder ao nome do tipo do pokémon', () => {
     renderWithRouter(<App />);
+    const buttonTypeElectric = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonElectric = screen.getByRole('button', { name: 'Electric' });
+    expect(buttonElectric).toBeInTheDocument();
     userEvent.click(buttonElectric);
-    const type = screen.getAllByText(/Electric/i);
-    expect(type.innerText).toBe(buttonElectric.innerText);
+    const typePokemonElectric = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonElectric.innerText).toBe(buttonTypeElectric.innerText);
+    const buttonTypeFire = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonFire = screen.getByRole('button', { name: 'Fire' });
+    expect(buttonFire).toBeInTheDocument();
     userEvent.click(buttonFire);
-    const typeFire = screen.getAllByText(/Fire/i);
-    expect(typeFire.innerText).toBe(buttonFire.innerText);
+    const typePokemonFire = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonFire.innerText).toBe(buttonTypeFire.innerText);
+    const buttonTypeBug = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonBug = screen.getByRole('button', { name: 'Bug' });
+    expect(buttonBug).toBeInTheDocument();
     userEvent.click(buttonBug);
-    const typeBug = screen.getAllByText(/Bug/i);
-    expect(typeBug.innerText).toBe(buttonBug.innerText);
+    const typePokemonBug = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonBug.innerText).toBe(buttonTypeBug.innerText);
+    const buttonTypePoison = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonPoison = screen.getByRole('button', { name: 'Poison' });
+    expect(buttonPoison).toBeInTheDocument();
     userEvent.click(buttonPoison);
-    const typePoison = screen.getAllByText(/Poison/i);
-    expect(typePoison.innerText).toBe(buttonPoison.innerText);
+    const typePokemonPoison = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonPoison.innerText).toBe(buttonTypePoison.innerText);
+    const buttonTypePsychic = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonPsychic = screen.getByRole('button', { name: 'Psychic' });
+    expect(buttonPsychic).toBeInTheDocument();
     userEvent.click(buttonPsychic);
-    const typePsychic = screen.getAllByText(/Psychic/i);
-    expect(typePsychic.innerText).toBe(buttonPsychic.innerText);
+    const typePokemonPsychic = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonPsychic.innerText).toBe(buttonTypePsychic.innerText);
+    const buttonTypeNormal = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonNormal = screen.getByRole('button', { name: 'Normal' });
+    expect(buttonNormal).toBeInTheDocument();
     userEvent.click(buttonNormal);
-    const typeNormal = screen.getAllByText(/Normal/i);
-    expect(typeNormal.innerText).toBe(buttonNormal.innerText);
+    const typePokemonNormal = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonNormal.innerText).toBe(buttonTypeNormal.innerText);
+    const buttonTypeDragon = screen.getAllByTestId(/pokemon-type-button/i);
     const buttonDragon = screen.getByRole('button', { name: 'Dragon' });
+    expect(buttonDragon).toBeInTheDocument();
     userEvent.click(buttonDragon);
-    const typeDragon = screen.getAllByText(/Normal/i);
-    expect(typeDragon.innerText).toBe(buttonDragon.innerText);
+    const typePokemonDragon = screen.getAllByTestId(/pokemon-type/i);
+    expect(typePokemonDragon.innerText).toBe(buttonTypeDragon.innerText);
   });
   it('botão All sempre visivel', () => {
     renderWithRouter(<App />);
@@ -109,13 +114,6 @@ describe('teste o component Pokedex.js', () => {
     renderWithRouter(<App />);
     const buttonProximoPokemon = screen.getByRole('button', { name: /Próximo Pokémon/i });
     expect(buttonProximoPokemon).toBeInTheDocument();
-    // const allPokemons = Pokemons.map((pokemon) => pokemon.index);
-    // allPokemons.forEach((pokemon, index) => {
-    //   // pokemonIndex = pokemon;
-    //   userEvent.click(buttonProximoPokemon);
-    //   expect(allPokemons).tobe(pokemon.index[8]);
-    // });
-    // userEvent.click(buttonProximoPokemon);
     const pikachu = screen.getByText(/pikachu/i);
     expect(pikachu).toBeInTheDocument();
     userEvent.click(buttonProximoPokemon);
@@ -145,5 +143,13 @@ describe('teste o component Pokedex.js', () => {
     userEvent.click(buttonProximoPokemon);
     const PikachuReestart = screen.getByText(/Pikachu/i);
     expect(PikachuReestart).toBeInTheDocument();
+  });
+  it('Ao carregar a página, o filtro selecionado deverá ser botão All', () => {
+    renderWithRouter(<App />);
+    const All = screen.getByRole('button', { name: /All/i });
+    expect(All).toBeInTheDocument();
+    userEvent.click(All);
+    const pikachu = screen.getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
   });
 });
