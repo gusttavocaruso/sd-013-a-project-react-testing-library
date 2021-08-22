b08dc89356b1c8ec993c7e2a37e67d52d27df5f8
EmmanuelBMS <acquabriall2020@gmail.com>
2021-08-22 16:42:30 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente About.js', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'About' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const aboutText = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+});
