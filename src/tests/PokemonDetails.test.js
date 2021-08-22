a2168662b8af127ca0ce5061f5f254056b96127c
Ramon Vitor <vitorramon@hotmail.com>
2021-08-22 20:36:12 -0300

    Requisito 7 - incompleto

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { PokemonDetails } from '../components';
+
+describe('Testa o componente PokemonDetails.', () => {
+  test('Testa se é exibido o texto "<name> Details", <name> = nome do pokémon', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <PokemonDetails />
+      </Router>,
+    );
+
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(moreDetails);
+    expect(moreDetails).toBeInTheDocument();
+  });
+});
