712fae975c1cab9632ad21fe7e72c4fbdd080f6c
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-08-22 23:24:05 -0300

    desafio 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// Aqui importamos o componente que iremos testar
+import App from '../App';
+// Aqui importamos a helper
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 1 - Verificando Existência de Links em <App.js />', () => {
+  test('testando o component App', () => {
+    renderWithRouter(<App />);
+
+    const linkToHome = screen.getByText('Home');
+    expect(linkToHome).toBeInTheDocument();
+
+    const linkToAbout = screen.getByText('About');
+    expect(linkToAbout).toBeInTheDocument();
+
+    const linkToFavorite = screen.getByText('Favorite Pokémons');
+    expect(linkToFavorite).toBeInTheDocument();
+  });
+});
