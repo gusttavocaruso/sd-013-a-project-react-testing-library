4c55d8d65e589cf0f1d5a647d9b2bf0dfad5b698
Joao Santos <jvitor.spaula@gmail.com>
2021-08-23 18:33:01 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Favoritepomenos from '../components/FavoritePokemons';
+
+describe('Testa favoritePokemons', () => {
+  test('No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<Favoritepomenos />);
+    history.push('/favorites');
+    const renderText = screen.getByText('No favorite pokemon found');
+    expect(renderText).toBeInTheDocument();
+  });
+
+  /* test('todos os pokemons', () => {
+    const { history } = renderWithRouter(<Favoritepomenos />);
+    history.push('/favorites');
+    const renderFavorites = screen.getAllByTestId('pokemon-name');
+    userEvent.click()
+    expect(renderFavorites.length).toBeGreaterThanOrEqual(0);
+  }); */
+});
