8faa9dbd85a5df3386ee15f149ac6c080497c6d7
Joao Santos <jvitor.spaula@gmail.com>
2021-08-23 16:12:01 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa componente app', () => {
+  test('Testa link Home', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByText(/Home/);
+    expect(elementText).toBeInTheDocument();
+  });
+
+  test('Testa link About', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByText(/About/);
+    expect(elementText).toBeInTheDocument();
+  });
+
+  test('Testa link favoritePokemon', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByText(/Favorite Pokémon/);
+    expect(elementText).toBeInTheDocument();
+  });
+});
