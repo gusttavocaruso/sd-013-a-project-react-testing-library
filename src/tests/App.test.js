dcb777b6ce70f373324390f780f2451ef2dbf6df
Lucas Lima <lucassauro.lt@gmail.com>
2021-08-23 10:12:19 -0300

    Requisito 1 completo.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test component app.js to verify if it has valid links', () => {
+  test('if "home" button links to route "/"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('if "about" button links to route "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('if "Favorite Pokémons" button links to route "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
