cc944d5c16fe10c6ac4f16e6c3d21280a0a8a030
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-22 21:56:23 -0300

    FavoritePokemons mutation test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa a pagina de pokemonsfavoritos', () => {
+  test('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const getFavorite = screen.getByText(/No Favorite pokemon found/i);
+    expect(getFavorite).toBeInTheDocument();
+  });
+});
