b9d55498df12c23cb2d75770743029a5f4fbb140
Ramon Vitor <vitorramon@hotmail.com>
2021-08-22 21:03:32 -0300

    Requisito 5 - incompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testa o componente "Pokedex".', () => {
+  test('Testa se é exibido um h2 com o texto "Encountered pokémons".', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const encounteredPokemons = screen.getByRole('heading', {
+      level: 2,
+      name: /encountered pokémons/i,
+    });
+    expect(encounteredPokemons).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido o próximo Pokémon ao clicar no botão.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const proxPoke = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(proxPoke).toBeInTheDocument();
+    userEvent.click(proxPoke);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    expect(pokeName).toHaveTextContent(/charmander/i);
+  });
+});
