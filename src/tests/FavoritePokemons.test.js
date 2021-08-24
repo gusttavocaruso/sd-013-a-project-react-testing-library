0ebf94f1666e0b09737a28cedb22ad976a3e88e4
ivandosss <ivanndosssilva9389@gmail.com>
2021-08-23 23:43:28 -0300

    Test FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,18 +1,85 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { FavoritePokemons } from '../components';
+
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame . If it is weak, the flame also burns weakly.',
+  },
+];
 
 describe('favoritePokemons.js', () => {
   test('Teste se é exibido na tela a mensagem `No favorite pokemon found`', () => {
-    render(<FavoritePokemons />);
+    render(
+      <BrowserRouter>
+        <FavoritePokemons pokemons={ [] } />
+      </BrowserRouter>,
+    );
 
-    const favoritePokemons = screen.getByRole('heading', {
-      name: /Favorite pokémons/i,
-    });
+    const favoritePokemons = screen.getByText(/No favorite pokemon found/i);
     expect(favoritePokemons).toBeInTheDocument();
   });
 
-  // test('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+  test('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </BrowserRouter>,
+    );
 
-  // });
+    const favorite = screen.getAllByRole('link', {
+      name: /more details/i,
+    });
+    expect(favorite).toHaveLength(pokemons.length);
+  });
 });
