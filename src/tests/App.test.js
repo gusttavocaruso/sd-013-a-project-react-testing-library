34a0b2a9c184895ec3054a3f90770e26c64babe5
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-08-24 03:15:11 -0300

    Adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('App', () => {
+  test('Renderiza os links com o texto "Home", "About", "Favorite Pokémons" ', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    expect(screen.getByText('Home')).toBeDefined();
+    expect(screen.getByText('About')).toBeDefined();
+    expect(screen.getByText('Favorite Pokémons')).toBeDefined();
+  });
+});
