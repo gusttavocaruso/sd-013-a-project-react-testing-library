fb06b3708c9ea3422572c4c96ecf93a2799e5d74
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-24 20:56:32 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,48 +1,94 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 // import Pokedex from '../components/Pokedex';
-// import App from '../App';
-// import pokemons from '../data';
+import App from '../App';
+import pokemons from '../data';
 
-// describe('Pokedex.js tests', () => {
-//   test('Se página contém um heading h2 com o texto Encountered pokémons', () => {
-//     render(
-//       <BrowserRouter>
-//         <Pokedex />
-//       </BrowserRouter>,
-//     );
+describe('Pokedex.js tests', () => {
+  test('Se página contém um heading h2 com o texto Encountered pokémons', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
 
-//     const h2 = screen.getByRole('heading', {
-//       level: 2,
-//       name: /Encountered pokémons/i,
-//     });
-//     expect(h2).toBeInTheDocument();
-//   });
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
 
-//   test('Primeiro Bloco - 6 testes', () => {
-//     render(
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>,
-//     );
+  test('Primeiro Bloco - 6 testes', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
 
-//     const buttonNext = screen.getByTestId('next-pokemon');
-//     expect(buttonNext).toBeInTheDocument();
+    const buttonNext = screen.getByTestId('next-pokemon');
+    expect(buttonNext).toBeInTheDocument();
 
-//     userEvent.click(screen.getByText(nextPokemon));
-//     const next = screen.getByText('Charmander');
-//     expect(next).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Próximo pokémon/i));
+    const next = screen.getByText('Charmander');
+    expect(next).toBeInTheDocument();
 
-//     userEvent.click(screen.getByText(nextPokemon));
-//     const next2 = screen.getByText('Caterpie');
-//     expect(next2).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Próximo pokémon/i));
+    const next2 = screen.getByText('Caterpie');
+    expect(next2).toBeInTheDocument();
+  });
 
-//     pokemons.forEach((pokemon, index) => {
-//       if (index < pokemons.length) userEvent.click(buttonNext);
-//       const first = screen.getByText('Pikachu');
-//       expect(first).toBeInTheDocument();
-//     });
-//   });
-// });
+  test('primeiro Pokémon da lista deve ser mostrado quando na última posição;', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const buttonNext = screen.getByTestId('next-pokemon');
+    const LENGHT = 9;
+    pokemons.forEach((pokemon, index) => {
+      if (index <= LENGHT) userEvent.click(buttonNext);
+    });
+    const first = screen.getByText(/Pikachu/i);
+    expect(first).toBeInTheDocument();
+  });
+
+  test('se a Pokédex tem os botões de filtro', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const NUMBER_OF_BUTTONS = 7;
+    const allButtonType = screen.getAllByTestId('pokemon-type-button');
+    allButtonType.forEach((item) => expect(item).toBeInTheDocument());
+    expect(allButtonType).toHaveLength(NUMBER_OF_BUTTONS);
+  });
+  test('se  apartir da seleção de um botão de tipo, a Pokédex circula limitada', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const allButtonType = screen.getAllByTestId('pokemon-type-button');
+    const psychicButton = allButtonType.find((button) => button.innerHTML === 'Psychic');
+    userEvent.click(psychicButton);
+    const teste = screen.getByTestId('pokemon-type').innerHTML;
+    expect(teste).toBe('Psychic');
+  });
+
+  test('se a Pokédex tem o botão All', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const buttonAll = screen.getByRole('button', { name: 'All' });
+    userEvent.click(buttonAll);
+    const firstPoke = screen.getByText('Pikachu');
+    expect(firstPoke).toBeInTheDocument();
+  });
+});
