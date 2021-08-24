b7307894e00477593256255dc41740d2912eae19
ivandosss <ivanndosssilva9389@gmail.com>
2021-08-24 00:32:15 -0300

    Test notFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-// test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('NotFound.js', () => {
+  test('Verifica se contém um heading `h2` `Page requested not found 😭`', () => {
+    render(<NotFound />);
+
+    const notFond = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFond).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`.', () => {
+    render(<NotFound />);
+
+    const error404Gif = screen.getByRole('img');
+    expect(error404Gif.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
