2baed9e5842f34c59cd55b475323db9f38bfb5ec
Seu nomecd <canaldogildosantos@gmail.com>
2021-08-23 23:18:51 -0300

    desafio 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 // import data from '../data';
 
@@ -30,28 +30,27 @@ describe('testar o componente pokémon', () => {
   });
   it('se o card contém lik de navegação para exibir detalhes "link=Id"', () => {
     const { history } = renderWithRouter(<App />);
-    const moreDetails = screen.getByRole('link', { name: 'More details' } );
-    expect(moreDetails).toBeInTheDocument();
-    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    const moreDetails = 'More details';
+    const Details = screen.getByRole('link', { name: moreDetails });
+    expect(Details).toBeInTheDocument();
+    userEvent.click(screen.getByRole('link', { name: moreDetails }));
     const linkEidIguais = history.location.pathname;
     expect(linkEidIguais).toBe('/pokemons/25');
     const pagDetails = screen.getByText(/Pikachu Details/i);
     expect(pagDetails).toBeInTheDocument();
-  })
+  });
   it('icone de estrela nos pokemons favoritados', () => {
-   renderWithRouter(<App />);
-   const more = screen.getByRole('link', { name: 'More details' } );
-   userEvent.click(more);
-   const favorite = screen.getByLabelText('Pokémon favoritado?');
-   userEvent.click(favorite);
-   const linkFavorite = screen.getByRole('link', { name:/Favorite pokémons/i });
-   expect(linkFavorite).toBeInTheDocument();
-   userEvent.click(linkFavorite);
-   const imgIconeStar = screen.getByAltText(/Pikachu is marked as favorite/i);
-   expect(imgIconeStar).toBeInTheDocument;
-  //  const imgStar = screen.getByAltText(/Pikachu sprite/i)
-  //  expect(imgStar).toBeInTheDocument();
-    expect(imgIconeStar).toHaveAttribute('src', '/star-icon.svg', 'alt', 'Pikachu is marked as favorite');
-  //  expect(imgStar).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+    renderWithRouter(<App />);
+    const more = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(more);
+    const favorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favorite);
+    // const linkFavorite = screen.getByRole('link', { name: /Favorite pokémons/i });
+    // expect(linkFavorite).toBeInTheDocument();
+    // userEvent.click(linkFavorite);
+    const imgIconeStar = screen.getByAltText(/Pikachu is marked as favorite/i);
+    // expect(imgIconeStar).toBeInTheDocument;
+    expect(imgIconeStar).toHaveAttribute('src', '/star-icon.svg');
+    expect(imgIconeStar).toHaveAttribute('alt', 'Pikachu is marked as favorite');
   });
 });
