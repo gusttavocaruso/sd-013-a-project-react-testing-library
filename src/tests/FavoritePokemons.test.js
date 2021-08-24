04c4bf33c671877c24ee99ec9293bd68b851f07c
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-24 16:06:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('FavoritePokemons.js tests', () => {
+  test('Renderiza a mensagem No favorite pokemon found', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+
+    const msg = screen.getByText('No favorite pokemon found');
+    expect(msg).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    userEvent.click(screen.getByRole('link', {
+      name: 'More details',
+    }));
+
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    const pokeName = screen.getByTestId('pokemon-name');
+
+    expect(pokeName).toBeInTheDocument();
+  });
+});
