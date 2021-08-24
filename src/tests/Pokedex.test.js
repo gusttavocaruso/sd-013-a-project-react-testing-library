505d24cef89bde9dd724eb8946a2ab3d605fbebc
Ramon Vitor <vitorramon@hotmail.com>
2021-08-24 14:22:44 -0300

    Adicionado arquivo renderWithRouter.js para usar no pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 import App from '../App';
 
 describe('Testa o componente "Pokedex".', () => {
+  const pokemonName = 'pokemon-name';
+
   test('Testa se é exibido um h2 com o texto "Encountered pokémons".', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    renderWithRouter(<App />);
 
     const encounteredPokemons = screen.getByRole('heading', {
       level: 2,
@@ -22,12 +19,7 @@ describe('Testa o componente "Pokedex".', () => {
   });
 
   test('Testa se é exibido o próximo Pokémon ao clicar no botão.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <App />
-      </Router>,
-    );
+    renderWithRouter(<App />);
 
     const proxPoke = screen.getByRole('button', {
       name: /próximo pokémon/i,
@@ -35,7 +27,31 @@ describe('Testa o componente "Pokedex".', () => {
     expect(proxPoke).toBeInTheDocument();
     userEvent.click(proxPoke);
 
-    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeName = screen.getByTestId(pokemonName);
     expect(pokeName).toHaveTextContent(/charmander/i);
+  });
+
+  test('Testa se é exibido o primeiro Pokémon ao passar do final da lista.', () => {
+    renderWithRouter(<App />);
+
+    const proxPoke = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(proxPoke).toBeInTheDocument();
+
+    // Código abaixo feito com a ajuda do código do Pedro Trasfereti
+    pokemons.forEach(() => userEvent.click(proxPoke));
+
+    const pokeName = screen.getByTestId(pokemonName);
+    expect(pokeName).toHaveTextContent(pokemons[0].name);
+  });
+
+  test('Testa se é exibido apenas um Pokémon por vez.', () => {
+    renderWithRouter(<App />);
+
+    const numero = screen.getAllByTestId(pokemonName);
+    expect(numero.length).toBe(1);
+    const firstPokemon = screen.getByText(/Pikachu/i);
+    expect(firstPokemon).toBeInTheDocument();
   });
 });
