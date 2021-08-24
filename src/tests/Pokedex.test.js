7fd8811f2a88276573c929e34098f3be4d54cd13
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-08-24 19:03:31 -0300

    Requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import userEvent from '@testing-library/user-event';
+// import Pokedex from '../components/Pokedex';
+// import App from '../App';
+// import pokemons from '../data';
+
+// describe('Pokedex.js tests', () => {
+//   test('Se página contém um heading h2 com o texto Encountered pokémons', () => {
+//     render(
+//       <BrowserRouter>
+//         <Pokedex />
+//       </BrowserRouter>,
+//     );
+
+//     const h2 = screen.getByRole('heading', {
+//       level: 2,
+//       name: /Encountered pokémons/i,
+//     });
+//     expect(h2).toBeInTheDocument();
+//   });
+
+//   test('Primeiro Bloco - 6 testes', () => {
+//     render(
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>,
+//     );
+
+//     const buttonNext = screen.getByTestId('next-pokemon');
+//     expect(buttonNext).toBeInTheDocument();
+
+//     userEvent.click(screen.getByText(nextPokemon));
+//     const next = screen.getByText('Charmander');
+//     expect(next).toBeInTheDocument();
+
+//     userEvent.click(screen.getByText(nextPokemon));
+//     const next2 = screen.getByText('Caterpie');
+//     expect(next2).toBeInTheDocument();
+
+//     pokemons.forEach((pokemon, index) => {
+//       if (index < pokemons.length) userEvent.click(buttonNext);
+//       const first = screen.getByText('Pikachu');
+//       expect(first).toBeInTheDocument();
+//     });
+//   });
+// });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import pokemons from '../data';
+import Pokemon from '../components/Pokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Pokemon.js tests', () => {
+  test('se é renderizado um card com as informações de determinado pokémon', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const { type } = pokemons[0];
+    const name = screen.getByTestId(/pokemon-name/i);
+    expect(name).toBeInTheDocument();
+    const typePoke = screen.getByTestId('pokemon-type');
+    expect(typePoke.innerHTML).toBe(type);
+    // const typePoke2 = screen.getByTestId('pokemon-type');
+    expect(typePoke).toBeInTheDocument();
+    const average = screen.getByTestId('pokemon-weight');
+    expect(average.innerHTML).toBe('Average weight: 6.0 kg');
+    const img = screen.getByRole('img');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(img.alt).toBe('Pikachu sprite');
+  });
+
+  test('se o card contém um link de navegação para exibir detalhes deste Pokémon', () => {
+    const { history } = renderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } />,
+    );
+
+    const { id } = pokemons[0];
+    const linkDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkDetails.href).toBe(`http://localhost/pokemons/${id}`);
+    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    const pathName = history.location.pathname;
+    expect(pathName).toBe(`/pokemons/${id}`);
+  });
+  test('se existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+
+    const alt = screen.getByAltText(/Pikachu is marked as favorite/i);
+    expect(alt).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
