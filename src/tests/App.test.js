31fee398bacc1e485997a2e6adeb2507b563c3b1
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-24 21:00:12 -0300

    Atende Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('First test - App.js', () => {
+  test('Testing the navegation links on the application', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const lHome = screen.getByRole('link', { name: /home/i });
+    const lAbout = screen.getByRole('link', { name: /about/i });
+    const lFavoritePokemon = screen.getByRole('link', { name: /favorite/i });
+    expect(lHome).toBeInTheDocument();
+    expect(lAbout).toBeInTheDocument();
+    expect(lFavoritePokemon).toBeInTheDocument();
+  });
+});
