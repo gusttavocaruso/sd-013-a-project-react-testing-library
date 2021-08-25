fd3e1777b0730435fbbc64ad6c553fdae6aa66be
suco <francalucas937@gmail.com>
2021-08-25 10:59:18 -0300

    quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente About', () => {
+  it('Teste se página contém o texto "Page requested not found 😭" ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina-nao-encontrada');
+
+    const notFoundText = screen.getByText(/Page requested not found/i);
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen
+      .getByRole('img',
+        { name: 'Pikachu crying because the page requested was not found' });
+    expect(imgNotFound.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
