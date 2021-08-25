33ff50313567616a01ad4fed38f807684b2b6609
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-08-25 00:08:15 -0300

    test NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+describe('teste NotFound.js', () => {
+  test('Teste se página contém um h2 com o texto Page requested not found 😭', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Verifica se a página mostra a imagem:', () => {
+    render(<NotFound />);
+
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Teste 5 - Pokedex.js', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    expect(screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+    })).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido o próximo Pokémon quando o botão é clicado.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const buttonPokemonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    fireEvent.click(buttonPokemonNext);
+    expect(screen.getByText(pokemons[1].name)).toBeInTheDocument();
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const buttonPokemonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    fireEvent.click(buttonPokemonNext);
+    const favorites = screen.getAllByRole('link', { name: /more details/i });
+    expect(favorites).toHaveLength(1);
+  });
+});
