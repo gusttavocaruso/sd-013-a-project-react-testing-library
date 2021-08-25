d50309a694167ab1328093365beefa41a9fc941f
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-24 21:33:35 -0300

    NotFound mutation test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('Teste da aplicação', () => {
+  test('Teste se a página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+
+    const headingNot = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(headingNot).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const getImg = screen.getAllByRole('img');
+    const img = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(getImg[1].src).toBe(img);
+  });
+});
