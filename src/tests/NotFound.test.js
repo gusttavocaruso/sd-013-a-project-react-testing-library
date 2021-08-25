90b9110bd79486027a09b08a710330957577c322
Ygor <ygormaiaa1@gmail.com>
2021-08-25 00:59:19 -0300

    Requisito 04 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Testando NotFound.js', () => {
+  test('Teste se a página tem um h2 com o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+
+    const textNotFound = screen.getByRole('heading', {
+      name: 'Page requested not found Crying emoji', level: 2,
+    });
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
