4cf653cc0a1cc0bc517181cdd7df9235a1b32eea
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-08-25 18:36:04 -0300

    ue

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+beforeEach(() => {
+  render(<About />);
+});
+
+describe('About component:', () => {
+  it('should have an h2 \'About Pokédex\' ', () => {
+    const h2 = screen.queryByRole('heading', { name: 'About Pokédex' });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('should have 2 paragraphs', () => {
+    const firstParagraph = /This application simulates a Pokédex/i;
+    const elFirstParagraph = screen.queryByText(firstParagraph);
+    expect(elFirstParagraph).toBeInTheDocument();
+
+    const secondParagraph = /One can filter Pokémons by type/i;
+    const elSecondParagraph = screen.queryByText(secondParagraph);
+    expect(elSecondParagraph).toBeInTheDocument();
+  });
+
+  it('should have an image of a pokedex', () => {
+    const img = screen.queryByAltText('Pokédex');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('FavoritePokemons component:', () => {
+  it('should show \'No favorite pokemon found\' if there\'s no favorite', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.queryByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('should show all favorited pokemons', () => {
+    const POKEMON_NAME = 'pokemon-name';
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.queryByRole('link', { name: 'More details' }));
+    userEvent.click(screen.queryByRole('checkbox', { checked: false }));
+    history.push('/favorites');
+    const pokemon = screen.queryByTestId(POKEMON_NAME);
+    expect(pokemon.textContent).toBe('Pikachu');
+
+    history.push('/');
+    userEvent.click(screen.queryByTestId('next-pokemon'));
+    const charmander = screen.queryByTestId(POKEMON_NAME);
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(screen.queryByRole('link', { name: 'More details' }));
+    userEvent.click(screen.queryByRole('checkbox', { checked: false }));
+
+    const favorite = screen.queryByTestId('pokemon-name');
+    history.push('/favorites');
+    expect(favorite.textContent).toBe('Charmander');
+  });
+});
