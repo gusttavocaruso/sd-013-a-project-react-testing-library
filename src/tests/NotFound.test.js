3d7284d963246b418d8a689f29c87f4fb4de5e5a
Gabriel Lenz <lenzordmusic@gmail.com>
2021-08-24 21:09:52 -0400

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Quarto requisito: Not Found', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Verifica se a página contém uma h2', () => {
+    const heading = screen.getByRole('heading', { name: /Page requested/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Verifica se a img tem a src especificadas', () => {
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(img.src).toStrictEqual(src);
+  });
+});
