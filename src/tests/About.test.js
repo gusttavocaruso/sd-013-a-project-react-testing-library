9a9c36b3aed3704dff7042ab3c45d0df1a2ac944
suco <francalucas937@gmail.com>
2021-08-25 10:55:28 -0300

    segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import RenderWithRouter from '../renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
-it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-  RenderWithRouter(<About />);
+describe('Testando o componente About', () => {
+  it('Teste se a página contém um heading `h2` com o texto "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const headingH2 = screen.getByRole('heading', { level: 2 });
+    expect(headingH2).toHaveTextContent(/About Pokédex/i);
+  });
 
-  const text = `This application simulates a Pokédex,
-   a digital encyclopedia containing all Pokémons`;
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
 
-  const text2 = `One can filter Pokémons by type, and
-   see more details for each one of them`;
-  const paragraph = screen.getByText((content) => content.startsWith(text));
-  const paragraph2 = screen.getByText(text2);
+    const paragraph = screen.getByText((content) => content
+      .startsWith('This application'));
+    const paragraph2 = screen.getByText((content) => content
+      .startsWith('One can'));
 
-  expect(paragraph).toBeInTheDocument();
-  expect(paragraph2).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const imagePokedex = screen.getByRole('img');
+    expect(imagePokedex.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
