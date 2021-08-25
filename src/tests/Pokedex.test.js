b1074dda952ffa9f7640fb1a1c934167892653ee
Ramon Vitor <vitorramon@hotmail.com>
2021-08-24 22:06:36 -0300

    Requisito 5 - finalizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -49,9 +49,62 @@ describe('Testa o componente "Pokedex".', () => {
   test('Testa se é exibido apenas um Pokémon por vez.', () => {
     renderWithRouter(<App />);
 
-    const numero = screen.getAllByTestId(pokemonName);
-    expect(numero.length).toBe(1);
+    const quantidadePorVez = screen.getAllByTestId(pokemonName);
+    expect(quantidadePorVez.length).toBe(1);
+
     const firstPokemon = screen.getByText(/Pikachu/i);
     expect(firstPokemon).toBeInTheDocument();
+  });
+
+  test('Testa se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+
+    const botaoAll = screen.getByRole('button', {
+      name: /all/i,
+    });
+    expect(botaoAll).toBeInTheDocument();
+
+    // Código feito com a ajuda de Felipe Neves
+    pokemons.forEach((pokemon) => {
+      const botao = screen.getByRole('button', {
+        name: pokemon.type,
+      });
+      return expect(botao).toBeInTheDocument();
+    });
+  });
+
+  test('Testa se a seleção de um botão de tipo, circula pokémons daquele tipo.', () => {
+    renderWithRouter(<App />);
+
+    // Código feito com a ajuda de Felipe Neves
+    pokemons.forEach((pokemon) => {
+      const botao = screen.getByRole('button', {
+        name: pokemon.type,
+      });
+      userEvent.click(botao);
+
+      const pokeType = screen.getByTestId('pokemon-type');
+      expect(pokeType).toHaveTextContent(pokemon.type);
+    });
+  });
+
+  test('Testa se a Pokédex contém um botão para resetar o filtro.', () => {
+    renderWithRouter(<App />);
+
+    // Código feito com a ajuda de Felipe Neves
+    const botaoAll = screen.getByTestId('');
+    expect(botaoAll).toBeInTheDocument();
+
+    userEvent.click(botaoAll);
+
+    const botaoProximo = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(botaoProximo).toBeInTheDocument();
+
+    userEvent.click(botaoProximo);
+
+    const proxPoke = screen.getByTestId('pokemon-type');
+    expect(proxPoke).toHaveTextContent(/fire/i);
   });
 });
