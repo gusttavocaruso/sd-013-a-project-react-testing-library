2e2302f84224c3c8ca5b476262912b81b45a5d62
Micael Santos <micaelmaicon@hotmail.com>
2021-08-25 16:16:27 -0300

    Finish requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Requisito-3 - Testa o componente <FavoritePokemons.js />', () => {
+  RenderWithRouter(<FavoritePokemons />);
+
+  test('Testa se é exibido na tela a mensagem No favorite pokemon.', () => {
+    const textFavorite = screen.getByText('No favorite pokemon found');
+    expect(textFavorite).toBeInTheDocument();
+  });
+});
