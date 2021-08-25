2fdba6db95d66467d2f28eddba334706095b3daf
Leonardo Santos <lcds90@gmail.com>
2021-08-24 21:53:35 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from './mocks/pokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('<FavoritePokemons /> Tests Section', () => {
+  it('should return `no favorite pokemon found` if doesn\'t have any', () => {
+    render(
+      <FavoritePokemons />,
+    );
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should return all favorite pokemons', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const message = screen.getByText(/pikachu/i);
+    expect(message).toBeInTheDocument();
+  });
+});
