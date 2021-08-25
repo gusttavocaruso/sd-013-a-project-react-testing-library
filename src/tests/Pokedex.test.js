2d3583cae3ce2e74f9b1966571be4f4d54129fc2
Caê Calçolari <eduardokae01@hotmail.com>
2021-08-25 08:58:37 -0300

    requisito 5 - pokedex tests

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,101 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa componente "Pokedex.js"', () => {
+  it('verifica que existe um heading h2 com texto "Encoutered pokémons"', () => {
+    renderWithRouter(<App />);
+    const h2Text = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  it('Teste se os botões estão funcionando da maneira esperada', () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(button).toBeInTheDocument();
+
+    const pokemon = screen.getByText(/pikachu/i);
+    userEvent.click(button);
+    expect(pokemon).not.toBe(/pikachu/i);
+    expect(pokemon).toBeDefined();
+  });
+
+  it('verifica se existem todos os botões de filtro', () => {
+    renderWithRouter(<App />);
+
+    const allButtons = screen.getByRole('button', {
+      name: /all/i,
+    });
+    expect(allButtons).toBeInTheDocument();
+    userEvent.click(allButtons);
+
+    const electricButtons = screen.getByRole('button', {
+      name: /electric/i,
+    });
+    expect(electricButtons).toBeInTheDocument();
+
+    const fireButtons = screen.getByRole('button', {
+      name: /fire/i,
+    });
+    expect(fireButtons).toBeInTheDocument();
+
+    const bugButtons = screen.getByRole('button', {
+      name: /bug/i,
+    });
+    expect(bugButtons).toBeInTheDocument();
+
+    const poisonButtons = screen.getByRole('button', {
+      name: /poison/i,
+    });
+    expect(poisonButtons).toBeInTheDocument();
+
+    const psychicButtons = screen.getByRole('button', {
+      name: /psychic/i,
+    });
+    expect(psychicButtons).toBeInTheDocument();
+
+    const normalButtons = screen.getByRole('button', {
+      name: /normal/i,
+    });
+    expect(normalButtons).toBeInTheDocument();
+
+    const dragonButtons = screen.getByRole('button', {
+      name: /dragon/i,
+    });
+    expect(dragonButtons).toBeInTheDocument();
+  });
+
+  it('verifica se após escolher o filtro, mostra apenas pokémons daquele tipo', () => {
+    renderWithRouter(<App />);
+
+    const dragonButtons = screen.getByRole('button', {
+      name: /dragon/i,
+    });
+    userEvent.click(dragonButtons);
+
+    const pokemon = screen.getByText(/dragonair/i);
+
+    const nextButton = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+
+    userEvent.click(nextButton);
+
+    expect(pokemon).toBeInTheDocument();
+  });
+
+  it('verifica que existe um pokemon-type-button', () => {
+    renderWithRouter(<App />);
+
+    const getDataTestId = screen.getAllByTestId('pokemon-type-button');
+    expect(getDataTestId).toBeDefined();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,86 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+// import Pokemon from '../components/Pokemon';
+
+describe('Pokemon.js tests', () => {
+  it('verifica que o nome e tipo correto do pokemon renderiza', () => {
+    renderWithRouter(<App />);
+
+    const getName = screen.getByTestId('pokemon-name');
+    expect(getName).toBeDefined();
+
+    const getType = screen.getByTestId('pokemon-type');
+    expect(getType).toBeDefined();
+
+    const getNameText = screen.getByText(/pikachu/i);
+    expect(getNameText).toBeInTheDocument();
+
+    const getTypeText = screen.getByText(/electric/i);
+    expect(getTypeText).toBeInTheDocument();
+  });
+
+  it('Verifica se o peso médio renderiza corretamente', () => {
+    renderWithRouter(<App />);
+
+    const getWeight = screen.getByTestId('pokemon-weight');
+    expect(getWeight).toBeInTheDocument();
+  });
+
+  it('verifica se a imagem renderiza e se existe um alt', () => {
+    renderWithRouter(<App />);
+
+    const getImage = screen.getByRole('img', {
+      name: /pikachu sprite/i,
+    });
+    expect(getImage).toBeInTheDocument();
+
+    const getImageByAlt = screen.getByAltText(/pikachu sprite/i);
+    expect(getImageByAlt).toBeInTheDocument();
+  });
+
+  it('Verifica se existe um"Link" com o texto "More details" e se funciona', () => {
+    renderWithRouter(<App />);
+
+    const getLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    expect(getLink).toBeInTheDocument();
+    userEvent.click(getLink);
+    const details = screen.getByText('Pikachu Details');
+    expect(details).toBeInTheDocument();
+  });
+
+  it('Verifica se o id da URL combina com o do pokemon', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const getLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+
+    userEvent.click(getLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('Verifica se existe uma estrela nos favoritados', () => {
+    renderWithRouter(<App />);
+
+    const getLink = screen.getByRole('link', {
+      name: /more details/i,
+    });
+
+    userEvent.click(getLink);
+
+    const getCheckbox = screen.getByText(/pokémon favoritado?/i);
+    expect(getCheckbox).toBeInTheDocument();
+    userEvent.click(getCheckbox);
+
+    const getStarImage = screen.getByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    });
+    expect(getStarImage).toBeInTheDocument();
+  });
+});
