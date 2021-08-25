542f83045ee30462a0c6e88fb5459b2ebc8ec207
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-08-25 18:13:17 -0300

    Pokedex mutation test ok

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { Pokemon } from '../components';
+
+describe('Testa um card de Pokémon.', () => {
+  test('Testa o nome do Pokemon;', () => {
+    renderWithRouter(<Pokemon />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toContain('Pikachu');
+  });
+});
