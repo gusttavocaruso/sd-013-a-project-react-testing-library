6b48ff9373fd044ef547f328473122f1280d6111
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-25 10:04:53 -0300

    Resolve o requisito 1

---
+++ src/renderWithRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+// Função criada para resolução do requisito 1 onde será efetuado os teste referente aos links de navegação
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{ component }</Router>), history,
+  });
+};
+
+export default renderWithRouter;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+// Requisito 1
+
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se o componente <App.js /> contém links de navegação.', () => {
+  it('O primeiro link deve possuir o texto "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: /Home/i });
+    fireEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('O segundo link deve possuir o texto "About"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: /About/i });
+    fireEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('O segundo link deve possuir o texto "Favorite Pokémons"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    fireEvent.click(favorites);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
