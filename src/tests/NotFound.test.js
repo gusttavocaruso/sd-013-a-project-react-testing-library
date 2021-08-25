0373305665918e361b85c959fe80a37c28aa2044
Leonardo Santos <lcds90@gmail.com>
2021-08-24 22:32:20 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Quarto requisito', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it('Verificar se a página contem <h2 />', () => {
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verificar se o caminho da imagem está correto', () => {
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByAltText(/Pikachu crying/i);
+    expect(img.src).toStrictEqual(src);
+  });
+});
