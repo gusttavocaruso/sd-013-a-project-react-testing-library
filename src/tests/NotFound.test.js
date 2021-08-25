84f4f0f706e376b08b7b0b7f38c983c1ee61b703
Micael Santos <micaelmaicon@hotmail.com>
2021-08-25 15:53:59 -0300

    Finish requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from './RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Requsito-4 - Teste do componente <NotFound />', () => {
+  beforeEach(() => {
+    RenderWithRouter(<NotFound />);
+  });
+
+  test('Testa se a página contém "h2" com o texto: "Page requested not found 😭"', () => {
+    const h2 = screen.getByRole('heading', { level: 2 }); // Role da tag <h2> é heading
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('Testa se a página contém uma imagem com o link específico', () => {
+    const image = screen.getAllByRole('img')[1];
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain(src);
+  }); // Resolução baseada no projeto do Matheus Duarte e do Andre Lorenzoni.
+});
