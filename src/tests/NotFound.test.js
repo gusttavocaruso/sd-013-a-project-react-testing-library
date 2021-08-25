669124180323e8328bddaae3ef2741dd0c5875a7
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-25 11:18:52 -0300

    Resolve o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+// Requisito 4
+
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe(' Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2 = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const alt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(alt);
+
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
