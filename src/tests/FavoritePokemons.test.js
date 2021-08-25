93c99beac16ab049755300bf6cda310cdfa515d8
Sethano <vitorcard1720@gmail.com>
2021-08-25 16:34:06 -0300

    almost

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('favorite pokemon`s tests', () => {
+  it('tests if "No favorite pokemon found" is on the screen', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+  it('test if appers on favorite the pokemon cards',
+    () => {
+      renderWithRouter(<App />);
+      userEvent.click(screen.getByText('More details'));
+      userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+      userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+      expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
+    });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('not found`s tests', () => {
+  it('tests if there is a h2 with "Page requested not found" on it', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('tests if a gif is shown on the screen', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(/Pikachu crying because the page requested was not/);
+    const src = img.src === 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(src).toBeTruthy();
+  });
+});
