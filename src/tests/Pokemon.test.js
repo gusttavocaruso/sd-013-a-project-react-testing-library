93ffb3f2ab9c8fe3c7dc911576762a1a3aafcfe9
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-08-25 02:27:01 -0300

    finaliza o projeto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa a Pokemon.js e suas funcionalidades.', () => {
+  test('Testa se é renderizado o pokemon e seus detalhes', () => {
+    renderWithRouter(<App />);
+
+    const getName = screen.getByTestId('pokemon-name');
+    const getType = screen.getByTestId('pokemon-type');
+    const getWeight = screen.getByTestId('pokemon-weight');
+    const getNext = screen.getByText('Próximo pokémon');
+    const getImage = screen.getByRole('img');
+    const url1 = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    const url2 = 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png';
+
+    expect(getName.innerHTML).toBe('Pikachu');
+    expect(getType.innerHTML).toBe('Electric');
+    expect(getWeight.innerHTML).toBe('Average weight: 6.0 kg');
+    expect(getImage.src).toEqual(url1);
+
+    fireEvent.click(getNext);
+    fireEvent.click(getNext);
+    fireEvent.click(getNext);
+
+    expect(getName.innerHTML).toBe('Ekans');
+    expect(getType.innerHTML).toBe('Poison');
+    expect(getWeight.innerHTML).toBe('Average weight: 6.9 kg');
+    expect(getImage.src).toEqual(url2);
+  });
+  test('Verifica se o botão more details esta funcional', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/');
+    const getButton = screen.getByText('More details');
+    expect(getButton).toBeInTheDocument();
+
+    fireEvent.click(getButton);
+    const getHeading = screen.getByRole('heading', { name: /summary/i });
+    expect(getHeading).toBeInTheDocument();
+  });
+});
+
+describe('Testes mais específicos da pokemon.js.', () => {
+  test('Testa se, ao clicar no botão, a URL muda.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const getButton = screen.getByText('More details');
+
+    fireEvent.click(getButton);
+    const actualUrl = history.location.pathname;
+    expect(actualUrl).toBe('/pokemons/25');
+  });
+  test('Testa se, ao favoritar, aparece uma estrelinha.', () => {
+    renderWithRouter(<App />);
+
+    const getMoreDetails = screen.getByRole('link', { name: /more details/i });
+    fireEvent.click(getMoreDetails);
+    const getFavorited = screen.getByText(/pokémon favorito?/i);
+    fireEvent.click(getFavorited);
+
+    const urlStar = '/star-icon.svg';
+    const getStar = screen.getByAltText(/is marked as favorite/i);
+
+    expect(getStar.src).toContain(urlStar);
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa a pokemonDetails.js', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+  });
+  test('Testa se as infos detalhadas são mostradas', () => {
+    const getDetails = screen.getByRole('heading', { name: /details/i });
+    const getPokemon = screen.getByRole('heading', {
+      name: /summary/i,
+      level: 2,
+    });
+    const summaryP = screen.getByText(/berries with electricity/i);
+    const getDetailsLink = screen.queryByRole('link', { name: /more details/i });
+
+    expect(getDetails).toBeInTheDocument();
+    expect(getPokemon).toBeInTheDocument();
+    expect(summaryP).toBeInTheDocument();
+    expect(getDetailsLink).not.toBeInTheDocument();
+  });
+  test('Testa se existem os mapas na página', () => {
+    const getHeadingLocal = screen.getByRole('heading', {
+      name: /game locations of/i,
+    });
+    const getLocations = screen.getAllByText(/kanto/i);
+    const getImages = screen.getAllByAltText(/location/i);
+    const url = 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png';
+    const url2 = 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png';
+
+    expect(getHeadingLocal).toBeInTheDocument();
+    expect(getLocations.length).toBe(2);
+    expect(getLocations[0]).toBeInTheDocument();
+    expect(getLocations[1]).toBeInTheDocument();
+    expect(getImages[0].src).toBe(url);
+    expect(getImages[1].src).toBe(url2);
+  });
+  test('Testa se um usuário pode favoritar através dos detalhes', () => {
+    const getFavorite = screen.getByRole('checkbox', /pokémon favoritado?/i);
+    fireEvent.click(getFavorite);
+    expect(getFavorite).toBeInTheDocument();
+    expect(getFavorite.type).toBe('checkbox');
+
+    const isFavorited = screen.getByRole('img', { name: /is marked as favorite/i });
+    expect(isFavorited).toBeInTheDocument();
+  });
+});
