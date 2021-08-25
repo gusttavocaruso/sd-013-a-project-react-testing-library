4830760164553694d5f6335b88ad3c2faf6a0603
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-08-25 14:31:15 -0300

    finaliza o projeto?

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
-import { FavoritePokemons } from '../components';
+import pokemons from '../data';
 
 describe('Testa os testes do favorites.', () => {
   test('Verifica se existe uma URL favorites.', () => {
@@ -15,28 +15,35 @@ describe('Testa os testes do favorites.', () => {
     expect(headingFavorites).toBeInTheDocument();
   });
   test('Verifica se não existe nenhum pokemon favoritado', () => {
-    render(<FavoritePokemons />);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
 
-    const getFavorites = screen.getByText(/no favorite/i);
+    const getFavorites = screen.getByText(/no favorite pokemon found/i);
     expect(getFavorites).toBeInTheDocument();
   });
 });
 
 describe('Testa a favoritação.', () => {
+  const {
+    name,
+    type,
+    averageWeight: { value, measurementUnit },
+  } = pokemons[0];
   test('Verifica se os pokemons favoritados são renderizados', () => {
     renderWithRouter(<App />);
     const getMoreDetails = screen.getByRole('link', { name: /more details/i });
     fireEvent.click(getMoreDetails);
     const getFavorited = screen.getByText(/pokémon favorito?/i);
     fireEvent.click(getFavorited);
-    const favoritesButton = screen.getByRole('link', { name: /favorite pokémons/i });
+    const favoritesButton = screen.getByRole('link',
+      { name: /favorite pokémons/i });
     fireEvent.click(favoritesButton);
     const getPokemonIDs = screen.getByTestId('pokemon-name');
     const getPokemonIDs2 = screen.getByTestId('pokemon-type');
     const getPokemonIDs3 = screen.getByTestId('pokemon-weight');
 
-    expect(getPokemonIDs).toBeInTheDocument();
-    expect(getPokemonIDs2).toBeInTheDocument();
-    expect(getPokemonIDs3).toBeInTheDocument();
+    expect(getPokemonIDs).toHaveTextContent(name);
+    expect(getPokemonIDs2).toHaveTextContent(type);
+    expect(getPokemonIDs3).toHaveTextContent(value, measurementUnit);
   });
 });
