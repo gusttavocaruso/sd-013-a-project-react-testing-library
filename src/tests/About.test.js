ca72bb9dc52d1a765207f3892b98688f0602465c
suco <francalucas937@gmail.com>
2021-08-25 10:48:28 -0300

    primeiro requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  RenderWithRouter(<About />);
+
+  const text = `This application simulates a Pokédex,
+   a digital encyclopedia containing all Pokémons`;
+
+  const text2 = `One can filter Pokémons by type, and
+   see more details for each one of them`;
+  const paragraph = screen.getByText((content) => content.startsWith(text));
+  const paragraph2 = screen.getByText(text2);
+
+  expect(paragraph).toBeInTheDocument();
+  expect(paragraph2).toBeInTheDocument();
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando topo da aplicação', () => {
+  it('deve renderizar o texto Home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+  });
+
+  it('deve renderizar o texto About', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText(/About/i);
+    expect(home).toBeInTheDocument();
+  });
+
+  it('deve renderizar o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText(/Favorite Pokémons/i);
+    expect(home).toBeInTheDocument();
+  });
+});
