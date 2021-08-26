8bf89e3618cb8038b250ac968054c20811a5c5ce
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-26 00:36:12 -0300

    Atende requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('Testing the Not Found Component', () => {
+  test('Testing if the page has a h2 with specifc text', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    expect(screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 })).toBeInTheDocument();
+  });
+
+  test('', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
