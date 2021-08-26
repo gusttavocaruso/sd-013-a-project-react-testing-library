6335b87aa960f2474d2ea8eff15081bd85f2993d
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-08-26 10:03:05 -0300

    requisito 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa componente app', () => {
+  test('Testa link Home', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(elementText).toBeInTheDocument();
+  });
+
+  test('Testa link About', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(elementText).toBeInTheDocument();
+  });
+
+  test('Testa link favoritePokemon', () => {
+    renderWithRouter(<App />);
+    const elementText = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    expect(elementText).toBeInTheDocument();
+  });
+});
