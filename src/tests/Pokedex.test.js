8061e4435deb1f4e1150928e5cecb3272a6dfdac
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-26 09:33:36 -0300

    Correção de erro no Lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -8,7 +8,6 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Teste o componente <Pokedex.js />', () => {
-
   test('Testa se página contém um heading h2 com o texto Encountered pokémons', () => {
     renderWithRouter(<App />);
 
