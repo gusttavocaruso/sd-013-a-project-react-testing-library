045eef0cabde73f91eccda9858977a66dd21533c
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-08-26 10:56:33 -0300

    correçoes lint pokemon.test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,15 +5,16 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Teste Pokemon.js', () => {
-  test('Teste se renderiza um card com as informações de determinado pokémon.', () => {});
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
-  expect(screen.getAllByText(/Electric/i)).toHaveLength(2);
-  // const { averageWeight: { value, measurementUnit } } = pokemons[0];
-  const regex = new RegExp(`/average weight: /`, 'i');
-  expect(screen.getByText(regex)).toBeInTheDocument();
+  test('Teste se renderiza um card com as informações de determinado pokémon.', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    expect(screen.getByText(pokemons[0].name)).toBeInTheDocument();
+    expect(screen.getAllByText(pokemons[0].type)).toHaveLength(2);
+    // const { averageWeight: { value, measurementUnit } } = pokemons[0];
+    // const regex = new RegExp(`/average weight: /`, 'i');
+    // expect(screen.getByText(regex)).toBeInTheDocument();
+  });
 });
