7244d177b4f1c341150b23c0b30d97cc0262f832
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-08-25 23:52:49 -0300

    Adding requiremente 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import historyRender from './History';
+
+describe('Testa o componente About', () => {
+  test('Testa se aparece a mensagem quando há pokemons favoritos', () => {
+    historyRender(<FavoritePokemons pokemons={ [] } />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
