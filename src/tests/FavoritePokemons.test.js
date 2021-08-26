44ad62b5f98d90275001a541a15edc0789f0c94e
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-25 21:52:18 -0300

    Atende requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+describe('Testing Favorite Pokemons', () => {
+  test('Testing if shows the following message: No favorite pokemon found', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+    expect(screen.getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  test('Testing if shows all the favorite pokemons', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </BrowserRouter>,
+    );
+    expect(screen.getAllByRole('link', {
+      name: /more details/i })).toHaveLength(pokemons.length);
+  });
+});
