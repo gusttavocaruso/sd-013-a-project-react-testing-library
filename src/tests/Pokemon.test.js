fd65b616546e9f2a80e3a69c66fff150419470b5
thomasravache <thomasravache31@gmail.com>
2021-08-26 09:24:37 -0300

    05-testando se nome do pokemon e mostrado na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testando componente Pokemon.js', () => {
+  const POKEMON_NAME = 'pokemon-name';
+
+  it('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId(POKEMON_NAME);
+
+    expect(pokemonName.textContent).toBe('Pikachu');
+  });
+});
