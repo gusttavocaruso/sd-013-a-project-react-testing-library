5a8b9d16189b496df794b8784e4f02e142b50fb2
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-08-26 10:36:14 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import Favoritepomenos from '../components/FavoritePokemons';
+
+describe('Testa favoritePokemons', () => {
+  test('No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<Favoritepomenos />);
+    history.push('/favorites');
+    const renderText = screen.getByText('No favorite pokemon found');
+    expect(renderText).toBeInTheDocument();
+  });
+
+});
