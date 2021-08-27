7523a37aed7fa2ec1c6d3afca21aa678ab9d9d44
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-08-27 14:21:08 -0300

    Requisito 6.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,10 +4,10 @@ import renderWithRouter from './renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
 
-describe('Tests Pokedex componente', () => {
-  it('Looks for a particular h2', () => {
+describe('Tests for the Pokedex.js componente', () => {
+  it('Checks if the page has a "H2" element with particular text', () => {
     renderWithRouter(<App />);
-    // this h2 should be in the page
+    // Finds the "H2" element with the desired text, by using the RegExp method (/string/i), and providing this information to a new const through a screen.getByRole method and informing that we need a 'heading' with the propretys name and level as such.
     const messageDisplays = /encountered pokémons/i;
     const headingElement = screen.getByRole('heading', {
       name: messageDisplays,
@@ -15,66 +15,66 @@ describe('Tests Pokedex componente', () => {
     });
     expect(headingElement).toBeInTheDocument();
   });
-  it('Tests the next Pokémon button functionality', () => {
+  it('Tests if the "Next Pokémon" button works', () => {
     renderWithRouter(<App />);
-    // Fetches the current Pokémon
+    // Fetches the current Pokémon by using RegExp and textContent methods.
     const firstPoke = screen.getByTestId(/pokemon-name/i);
     const firstPokeName = firstPoke.textContent;
     expect(firstPoke).toBeInTheDocument();
-    // Fetches next Pokémon button and then clicks on it
+    // Fetches the "Next Pokémon" button and then clicks on it by using the fireEvent.click method.
     const nextButton = screen.getByRole('button', {
       name: /próximo pokémon/i,
     });
     expect(nextButton).toBeInTheDocument();
     fireEvent.click(nextButton);
-    // Fetches next Pokémon
+    // Fetches the next Pokémon by using the same methods as before.
     const secondPoke = screen.getByTestId(/pokemon-name/i);
     const secondPokeName = secondPoke.textContent;
     expect(secondPoke).toBeInTheDocument();
-    // Compares first Pokémon to the second one
+    // Finally, it compares the first Pokémon with the second one and expects it to be true.
     expect(firstPokeName !== secondPokeName).toBeTruthy();
   });
   it('Tests if the page shows a single Pokémon', () => {
     renderWithRouter(<App />);
-    // Fetches every Pokémon in the page
+    // Fetches every Pokémon in the page by using the getAllByTestId method, combined with RegExp and expects it to show only one pokemon.
     const everyPoke = screen.getAllByTestId(/pokemon-name/i);
     expect(everyPoke).toHaveLength(1);
   });
-  it('Checks for every type filter button', () => {
+  it('Checks if the Pokédex has all of the filter buttons', () => {
     renderWithRouter(<App />);
-    // Fetches every Pokémon type the same way it's done in Pokedex.js
+    // Fetches every Pokémon type the same way it's done in Pokedex.js.
     const everyType = [...new Set(pokemons
       .reduce((types, { type }) => [...types, type], []))];
-    // Fetches every filter button
+    // Fetches every filter button.
     const everyFilterButton = screen.getAllByTestId(/pokemon-type-button/i);
     everyFilterButton.forEach((filterButton, index) => {
       expect(filterButton).toBeInTheDocument();
-      // Clicks on type filter button
+      // Clicks on type filter button.
       fireEvent.click(filterButton);
-      // Fetches current Pokémon type displayed
+      // Fetches the current Pokémon type displayed.
       const currentType = screen.getByTestId('pokemon-type');
-      // Checks if every Pokémon type has got its own filter
-      // Checks if filter type clicked matches Pokémon type displayed
+      // Checks if every Pokémon type has got its own filter.
+      // Checks if filter type clicked matches Pokémon type displayed.
       expect(filterButton.innerHTML === everyType[index]
         && filterButton.innerHTML === currentType.textContent)
         .toBeTruthy();
     });
-    // Fetches 'all Pokémons' button
+    // Fetches the "all Pokémons" button.
     const allButton = screen.getByRole('button', {
       name: /all/i });
     expect(allButton).toBeInTheDocument();
   });
   it('Checks for all button and tests its functionality', () => {
     renderWithRouter(<App />);
-    // Fetches 'all Pokémons' button
+    // Fetches 'all Pokémons' button.
     const allButton = screen.getByRole('button', {
       name: /all/i });
     expect(allButton).toBeInTheDocument();
-    // Fetches first Pokémon in data
+    // Fetches first Pokémon in data.
     const firstPokeType = pokemons[0].type;
-    // Clicks on All Button
+    // Clicks on All Button.
     fireEvent.click(allButton);
-    // Checks if Pokémon displayed matches first Pokémon on data
+    // Checks if the Pokémon displayed matches with the first Pokémon on data.
     const currentType = screen.getByTestId('pokemon-type');
     expect(firstPokeType === currentType.textContent);
   });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Tests for the Pokemon.js component', () => {
+  it('Checks if the correct information about a single Pokémon is shown', () => {
+    renderWithRouter(<App />);
+    // Fetchs the "More Details" link and than clicks on it using the fireEvent method.
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    fireEvent.click(moreDetails);
+    // Fetches the name of the Pokémon displayed.
+    const displayedName = screen.getByTestId(/pokemon-name/i);
+    expect(displayedName).toBeInTheDocument();
+    // Filters the database by the Pokémon name.
+    const filteredPoke = pokemons
+      .filter((pokemon) => pokemon.name === displayedName.textContent)[0];
+    // Destructuring filtered Pokémon.
+    const { name, type, averageWeight: { value, measurementUnit }, image } = filteredPoke;
+    // Checks if the Pokémon name on screen matches the one filtered.
+    expect(displayedName.textContent).toEqual(name);
+    // Checks if the Pokémon type on screen matches the one filtered.
+    const displayedType = screen.getByTestId(/pokemon-type/i);
+    expect(displayedType).toBeInTheDocument();
+    expect(displayedType.textContent).toEqual(type);
+    // Checks if the average weigth on screen matches the one filtered.
+    const displayedWeight = screen.getByTestId(/pokemon-weight/i);
+    expect(displayedWeight).toBeInTheDocument();
+    const pokeWeight = `Average weight: ${value} ${measurementUnit}`;
+    expect(displayedWeight.textContent).toEqual(pokeWeight);
+    // Checks if the image displayed on screen matches the Pokémon sprite and if the alt text is correct.
+    const pokeSprite = screen.getByAltText(/sprite/i);
+    expect(pokeSprite.src).toEqual(image);
+    expect(pokeSprite.alt).toEqual(`${name} sprite`);
+  });
+
+  it('Checks if the "More Details" link is correct', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    // Fetches the name of the Pokémon displayed.
+    const displayedName = screen.getByTestId(/pokemon-name/i);
+    // Filters the database by the Pokémon name.
+    const filteredPoke = pokemons
+      .filter((pokemon) => pokemon.name === displayedName.textContent)[0];
+    expect(moreDetails.href).toEqual(`http://localhost/pokemons/${filteredPoke.id}`);
+  });
+  it('Checks if the pathname is correct acordding to Pokémon id', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    fireEvent.click(moreDetails);
+    // Fetches the name of the Pokémon displayed.
+    const displayedName = screen.getByTestId(/pokemon-name/i);
+    // Filters the database by the Pokémon name.
+    const filteredPoke = pokemons
+      .filter((pokemon) => pokemon.name === displayedName.textContent)[0];
+    history.push(`/pokemons/${filteredPoke.id}`);
+    expect(history.location.pathname).toBe(`/pokemons/${filteredPoke.id}`);
+  });
+  it('Checks if theres an star icon displayed on a "Favorite Pokémon"', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    fireEvent.click(moreDetails);
+    // Fetches the favorite option.
+    const favoriteOption = screen.getByLabelText(/favoritado/i);
+    fireEvent.click(favoriteOption);
+    // Fetches the name of the Pokémon displayed.
+    const displayedName = screen.getByTestId(/pokemon-name/i);
+    // Filters the database by the Pokémon name.
+    const filteredPoke = pokemons
+      .filter((pokemon) => pokemon.name === displayedName.textContent)[0];
+    // Fetches favorite star.
+    const favoriteStr = screen.getByAltText(`${filteredPoke.name} is marked as favorite`);
+    expect(favoriteStr).toBeInTheDocument();
+    expect(favoriteStr.src).toBe('http://localhost/star-icon.svg');
+  });
+});
