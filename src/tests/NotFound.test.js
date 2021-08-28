e1c9c2316ed936b7c5808815d0d9acb0126badf1
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-08-28 14:50:54 -0300

    desafio 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('se página contém um heading h2 com o texto Page requested not found 😭;', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2NotFound = screen.getByRole('heading', { level: 2 });
+    expect(h2NotFound).toBeInTheDocument();
+    expect(h2NotFound).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+
+    // tem duas imagens na página, contando com o emogi 😭
+    const image = screen.getAllByRole('img')[1];
+    const linkImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', linkImage);
+  });
+});
