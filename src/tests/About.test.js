22d014391889b95e37c1c62badbcee5f24e27003
João Paulo <jp.viegas@hotmail.com>
2021-08-28 21:25:38 -0300

    requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  // acessar os elementos da tela
+  render(<About />);
+  // interagir se precisar
+  const homeText = screen.getByText('About Pokemon');
+  // fazer seu teste
+  expect(homeText).toBeInTheDocument();
+  expect(homeText.type).toBe();
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,85 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+import { NotFound } from '../components';
+
+describe('Requisito 1: Teste o componente <App.js />', () => { // descrição do teste
+  test('1.1 - Teste se a aplicação contém um conjunto fixo de links de navegação', () => { // teste do requisito 1
+    // acessar os elementos da tela
+    render(
+      <BrowserRouter>
+        {/* renderiza todo o componente App */}
+        <App />
+      </BrowserRouter>,
+    );
+    // const homeText = screen.getByText('Home');
+    const homeLink = screen.getByRole('link', { // procura o link com o nome home
+      name: /home/i,
+    });
+    expect(homeLink).toBeInTheDocument(); // espera estar no documento
+
+    const aboutLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  test('1.2 - Teste se a aplicação é redirecionada para a página inicial', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const homeLink = screen.getByRole('link', { // procura o link com o nome home
+      name: /home/i,
+    });
+    userEvent.click(homeLink);
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  test('1.3 - Teste se a aplicação é redirecionada para a página de About', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const aboutLink = screen.getByRole('link', {
+      name: /about/i,
+    });
+    userEvent.click(aboutLink);
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  test('1.4 - Teste se a aplicação é redirecionada para a página dos Favoritados', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    userEvent.click(favoritePokemonsLink);
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  test('1.5 - Teste se a aplicação é redirecionada para a página Not Found', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const pageNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(pageNotFound).toBeInTheDocument();
+  });
+});
