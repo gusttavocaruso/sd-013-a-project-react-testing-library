5fc44673352b945962b11696b5b1a5331f4ac280
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-08-29 16:56:12 -0300

    Inicia Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Testing PokemonDetails.js', () => {
+  test('testing the text with pokemons names', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const details = screen.getByRole('link', { name: /more details/i });
+    fireEvent.click(details);
+    expect(screen.getByText(`${pokemons[0].name} Details`)).toBeInTheDocument();
+    expect(details).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Summary', level: 2 }))
+      .toBeInTheDocument();
+    expect(screen.getByText(pokemons[0].summary)).toBeInTheDocument();
+  });
+});
