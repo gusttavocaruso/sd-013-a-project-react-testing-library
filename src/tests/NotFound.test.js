7affb3fd0dc3b1688254efb70730012d8d4a451b
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-08-29 19:58:14 -0300

    quarto requisito completo 2

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import NotFound from '../components/NotFound';
 
-test('Verifica se contem um texto h2 Page requested not found 😭', () => {
-  render(<NotFound />);
-  const noFavorite = screen.getByRole('heading', {
-    level: 2,
-    name: 'Page requested not found Crying emoji',
+describe('Testa o componente NotFound', () => {
+  test('Verifica se contem um texto h2 Page requested not found 😭', () => {
+    render(<NotFound />);
+    const noFavorite = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(noFavorite).toBeInTheDocument();
   });
-  expect(noFavorite).toBeInTheDocument();
-});
 
-test('Verifica se contem uma imagem', () => {
-  render(<NotFound />);
-  const img = screen.getAllByRole('img')[1];
-  expect(img.src).toStrictEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  test('Verifica se contem uma imagem', () => {
+    render(<NotFound />);
+    const img = screen.getAllByRole('img')[1];
+    expect(img.src).toStrictEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
