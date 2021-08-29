b6f0341abdd4f5e61e938236faa52ac2d2028b4b
ArthurMGoulart <budiarthur@gmail.com>
2021-08-29 09:53:40 -0300

    Realizando requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('NotFound.js tests', () => {
+  test('if not found renders', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+
+    const image = screen.getByRole(
+      'img', { name: 'Pikachu crying because the page requested was not found' },
+    );
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toStrictEqual(src);
+  });
+});
