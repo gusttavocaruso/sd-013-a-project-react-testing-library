f505c9ada7e31fb8cd3db3d9dd4a97ac6a9ec46a
Emídio Rebouças <emidioreb@gmail.com>
2021-08-30 12:12:18 -0300

    requeriment 1 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,22 @@
-test('', () => {});
-// teste
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testar topo da aplicação', () => {
+  test('Teste se a aplicação é redirecionada para a página Home', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Home'));
+    expect(history.location.pathname).toBe('/');
+  });
+  test('Teste se a aplicação é redirecionada para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('About'));
+    expect(history.location.pathname).toBe('/about');
+  });
+  test('Teste se a aplicação é redirecionada para a página Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
