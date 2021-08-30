3c1c90761ca5ca47e34eec8b4609101f1a56807e
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-30 17:22:26 -0300

    Requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,43 @@
-// Primeiro commit!
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+beforeEach(() => render(<About />));
+
+describe('Testa o componente About', () => {
+  it('Testa o título', () => {
+    const titulo = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+
+    expect(titulo).toBeInTheDocument();
+  });
+
+  it('Testa os dois paragrafos', () => {
+    const paragrafo1 = screen.getByTestId('paragraph-1');
+    const paragrafo2 = screen.getByTestId('paragraph-2');
+
+    expect(paragrafo1).toBeInTheDocument();
+    expect(paragrafo2).toBeInTheDocument();
+  });
+
+  it('Testa a imagem', () => {
+    const imagem = screen.getByRole('img');
+
+    expect(imagem).toBeInTheDocument();
+
+    expect(imagem).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
+
+// 2. Teste o componente `<About.js />.`
+// - Teste se a página contém as informações sobre a Pokédex.
+// - Teste se a página contém um heading `h2` com o texto `About Pokédex`.
+// - Teste se a página contém dois parágrafos com texto sobre a Pokédex.
+// - Teste se a página contém a seguinte imagem de uma Pokédex: `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`.
+//* *O que será verificado:**
+// - Será avaliado se o arquivo teste `About.test.js` contemplam 100% dos casos de uso criados pelo Stryker.
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,80 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente App', () => {
+  it('verifica os links', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = screen.getByRole('link', {
+      name: /favorite/i,
+    });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  test('Teste das rotas', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(home);
+
+    const titleHome = screen.getByRole('heading', {
+      name: /pokédex/i,
+    });
+    expect(titleHome).toBeInTheDocument();
+
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+    userEvent.click(about);
+
+    const titleAbout = screen.getByRole('heading', {
+      name: /about pokédex/i,
+    });
+    expect(titleAbout).toBeInTheDocument();
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite/i,
+    });
+    userEvent.click(favorite);
+
+    const titleFavorites = screen.getByRole('heading', {
+      name: /favorite/i,
+    });
+    expect(titleFavorites).toBeInTheDocument();
+  });
+
+  test('testa página não encontrada', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/rota-qualquer');
+    const notFoundText = screen.getByRole('heading', {
+      name: /page requested not found /i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
+
+// 1. Teste o componente `<App.js />`
+// - Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
+// - O primeiro link deve possuir o texto `Home`.
+// - O segundo link deve possuir o texto `About`.
+// - O terceiro link deve possuir o texto `Favorite Pokémons`.
+// - Teste se a aplicação é redirecionada para a página inicial, na URL `/` ao clicar no link `Home` da barra de navegação.
+// - Teste se a aplicação é redirecionada para a página de `About`, na URL `/about`, ao clicar no link `About` da barra de navegação.
+// - Teste se a aplicação é redirecionada para a página de `Pokémons Favoritados`, na URL `/favorites`, ao clicar no link `Favorite Pokémons` da barra de navegação.
+// - Teste se a aplicação é redirecionada para a página `Not Found` ao entrar em uma URL desconhecida.
+//* *O que será verificado:**
+// - Será avaliado se o arquivo teste `App.test.js` contemplam 100% dos casos de uso criados pelo Stryker.
