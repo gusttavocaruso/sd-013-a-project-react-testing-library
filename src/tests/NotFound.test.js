263264f75c72d685bd9898d77b9222951df7dfdf
Emídio Rebouças <emidioreb@gmail.com>
2021-09-03 09:44:37 -0300

    requeriment 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testar página NotFoundTest', () => {
+  test('Página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/i,
+      }),
+    ).toBeInTheDocument();
+  });
+  test('A página contém a imagem de uma Pokemon chorando', () => {
+    renderWithRouter(<NotFound />);
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(
+      screen.getByRole('img', {
+        name: 'Pikachu crying because the page requested was not found',
+      }).src,
+    ).toBe(src);
+  });
+});
