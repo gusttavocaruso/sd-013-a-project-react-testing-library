0693c0d843f1f0398b631ee6eb0da95ded1a5bb0
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-09-02 21:59:56 -0300

    Atende requisito 7 e finaliza o Projeto

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -19,4 +19,41 @@ describe('Testing PokemonDetails.js', () => {
       .toBeInTheDocument();
     expect(screen.getByText(pokemons[0].summary)).toBeInTheDocument();
   });
+
+  test('testing pokemon map', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    expect(screen.getByRole('heading', {
+      name: new RegExp(`game locations of ${pokemons[0].name}`, 'i'),
+      level: 2,
+    })).toBeInTheDocument();
+
+    const pokemonLocation = screen.getAllByAltText(`${pokemons[0].name} location`);
+    pokemons[0].foundAt.forEach((pokemon, index) => {
+      expect(screen.getByText(pokemon.location)).toBeInTheDocument();
+      expect(pokemonLocation[index].src).toContain(pokemon.map);
+    });
+  });
+
+  test('', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const favoritChecked = screen.getByRole('checkbox');
+    expect(favoritChecked).toBeInTheDocument();
+    fireEvent.click(favoritChecked);
+    const star = screen.getByAltText(`${pokemons[0].name} is marked as favorite`);
+    expect(star.src).toContain('/star-icon.svg');
+    fireEvent.click(favoritChecked);
+    expect(screen.getByLabelText('Pokémon favoritado?')).toBeInTheDocument();
+    const favorite = screen.getByRole('link', { name: /favorite/i });
+    fireEvent.click(favorite);
+    expect(screen.getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
 });
