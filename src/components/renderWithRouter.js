b9febdc7050678de6482e34ddbd00aed11fe3e0b
Jéssica Queiroz <gnr0034@gmail.com>
2021-09-05 20:07:05 -0300

    Requisito 7

--- src/components/renderWithRouter.js
+++ src/components/renderWithRouter.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{ component }</Router>), history,
-  });
-};
+function renderWithRouter(component) {
+  const historyMock = createMemoryHistory();
+  const objectRender = render(
+    <Router history={ historyMock }>
+      {component}
+    </Router>,
+  );
+  return {
+    ...objectRender,
+    history: historyMock,
+  };
+}
 
 export default renderWithRouter;
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,80 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+const {
+  name,
+  foundAt,
+  summary,
+} = pokemons[0];
+
+describe('PokemonDetails.js Tests',
+  () => {
+    const textLink = 'More details';
+    test('tests if a card with the information of each pokemon is rendered', () => {
+      renderWithRouter(<App />);
+      const details = screen.getByText(textLink);
+      userEvent.click(details);
+
+      const hText = screen.getByRole('heading', {
+        name: `${name} Details`,
+        level: 2,
+      });
+      expect(hText).toBeInTheDocument();
+      expect(details).not.toBeInTheDocument();
+
+      const hTextSummary = screen.getByRole('heading', {
+        name: 'Summary',
+        level: 2,
+      });
+      expect(hTextSummary).toBeInTheDocument();
+
+      const textSummary = screen.getByText(summary);
+      expect(textSummary).toBeInTheDocument();
+    });
+
+    test('test if there is a section on the page with maps containing the locations',
+      () => {
+        renderWithRouter(<App />);
+
+        const details = screen.getByText(textLink);
+        userEvent.click(details);
+
+        const location = screen.getByRole('heading', { name:
+          `Game Locations of ${name}` });
+        expect(location).toBeInTheDocument();
+
+        const altNamePok = screen.getAllByAltText(`${name} location`);
+        expect(altNamePok).toHaveLength(foundAt.length);
+
+        foundAt.forEach((item, index) => {
+          const p = screen.getByText(item.location);
+          expect(p).toBeInTheDocument();
+
+          const pokAlt = screen.getAllByAltText(`${name} location`);
+          expect(pokAlt[index]).toHaveAttribute('src', item.map);
+        });
+      });
+
+    test('tests if the user can bookmark a pokemon on the details page', () => {
+      renderWithRouter(<App />);
+
+      const details = screen.getByText(textLink);
+      userEvent.click(details);
+
+      const checkBox = screen.getByRole('checkbox');
+      expect(checkBox).toBeInTheDocument();
+
+      userEvent.click(checkBox);
+      expect(checkBox).toBeChecked();
+
+      userEvent.click(checkBox);
+      expect(checkBox).not.toBeChecked();
+
+      const label = screen.getByLabelText('Pokémon favoritado?');
+      expect(label).toBeInTheDocument();
+    });
+  });
