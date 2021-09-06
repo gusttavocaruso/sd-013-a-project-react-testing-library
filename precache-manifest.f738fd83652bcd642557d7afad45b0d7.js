f4050dd9ae1b9a84fd910408fb3bb8e182f98393
Gessé Carlos <gesse.dev@outlook.com>
2021-09-06 04:30:00 -0300

    Updates

---
+++ precache-manifest.f738fd83652bcd642557d7afad45b0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b232a7ada257fb8cb6f2f6a5e39f67bc",
+    "url": "/rtl-pokedex/index.html"
+  },
+  {
+    "revision": "6d0ae0f1b31416dc74a3",
+    "url": "/rtl-pokedex/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/rtl-pokedex/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/rtl-pokedex/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0ae0f1b31416dc74a3",
+    "url": "/rtl-pokedex/static/js/main.06356c71.chunk.js"
+  },
+  {
+    "revision": "adf33efc2561ad2297ad",
+    "url": "/rtl-pokedex/static/js/runtime-main.a02bcde4.js"
+  }
+]);
