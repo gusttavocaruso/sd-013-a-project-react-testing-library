cc343635daeb6fd8847de7d9af5493417566971a
EmmanuelBMS <acquabriall2020@gmail.com>
2021-09-15 22:01:57 -0300

    resolve requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,15 +9,31 @@ describe('Testa o componente Pokemon.js', () => {
   test('Testa se é renderizado um card com as informações de determinado pokémon', () => {
     renderWithRouter(<App />);
 
-    pokemons.forEach((pokemon) => {
-      const pokemonName = screen.getByText(pokemon.name);
-      const pokemonType = screen.getByText(pokemon.type);
-      const pokemonWeigth = screen.getByText(`Average weight: ${pokemon.pokemonWeigth}`);
-      expect(pokemonName).toBeInTheDocument();
-      expect(pokemonType).toBeInTheDocument();
-      expect(pokemonWeigth).toBeInTheDocument();
+    pokemons.forEach(({ name,
+      type, averageWeight: { value, measurementUnit }, image }) => {
+      const pokemonName = screen.getByTestId('pokemon-name');
+      const pokemonType = screen.getByTestId('pokemon-type');
+      const pokemonWeigth = screen.getByTestId('pokemon-weight');
+      const pokemonWeightValue = `Average weight: ${value} ${measurementUnit}`;
+      const pokemonImg = screen.getByAltText(`${name} sprite`);
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toHaveTextContent(type);
+      expect(pokemonWeigth).toHaveTextContent(pokemonWeightValue);
+      expect(pokemonImg).toHaveAttribute('src', image);
 
       userEvent.click(screen.getByRole('button', { name: /próximo pokémon/i }));
     });
+  });
+  test('Testa redirecionamento do link More Details e icone de favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const getMoreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(getMoreDetailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    const getCheckBoxItem = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+    userEvent.click(getCheckBoxItem);
+    const pokemonImg = screen.getByAltText('Pikachu is marked as favorite');
+    expect(pokemonImg).toHaveAttribute('src', '/star-icon.svg');
   });
 });
