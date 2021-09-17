2bb21360a852010a28b956656edd075286bc1ac2
Lucas Santos <lucassantos_dev@outlook.com>
2021-09-17 20:19:07 -0300

    req 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRoute from './renderWithRoute';
+import About from '../components/About';
+
+describe('Teste do componente About', () => {
+  it('Teste se a página contém um heading h2 com texto >About Pokédex<', () => {
+    renderWithRoute(<About />);
+    const elementHeading = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(elementHeading).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com o texto sobre a Pokédex', () => {
+    renderWithRoute(<About />);
+    const elementsP = screen.getAllByText(/Pokémons/);
+    expect(elementsP.length).toBe(2);
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRoute(<About />);
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg.src).toStrictEqual('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
