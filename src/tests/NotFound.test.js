4c1bb89f54277d69e94d76a097fc94f569fe2f7c
Lucas Santos <lucassantos_dev@outlook.com>
2021-09-18 15:54:17 -0300

    req NotFound done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste do componente NotFound', () => {
+  it('Contém um heading h2 com texto >Page requested not found 😭<', () => {
+    render(<NotFound />);
+    const titleNotFound = screen.getByText('Page requested not found');
+    expect(titleNotFound).toBeInTheDocument();
+  });
+  it('Teste se a página contém pikachu chorando', () => {
+    render(<NotFound />);
+    const imagePokedex = screen
+      .getByAltText('Pikachu crying because the page requested was not found').src
+      .includes('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(imagePokedex).toBeTruthy();
+  });
+});
