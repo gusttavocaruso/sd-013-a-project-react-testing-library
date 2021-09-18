f9f72f882a901a94f1e529df993a612cee12326b
Lucas Santos <lucassantos_dev@outlook.com>
2021-09-18 15:39:27 -0300

    req FavoritePokemons done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRoute from './renderWithRoute';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste do componente FavoritePokemons', () => {
+  test('Verifica se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRoute(<FavoritePokemons />);
+    const msgNotFoundPoke = screen.getByText('No favorite pokemon found');
+    expect(msgNotFoundPoke).toBeInTheDocument();
+  });
+  test('Verifica se há pokemons favoritados', () => {
+    renderWithRoute(<App />);
+    // após renderizar o App, clico no elemento link com name igual a 'More details'
+    userEvent.click(screen.getByRole('link', {
+      name: 'More details',
+    }));
+    // aqui pego a label no final da página de detalhes
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    // aqui vou p página de pokemons favoritados e vejo se tem algo com o data-testid
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    const namePoke = screen.getByTestId('pokemon-name');
+    expect(namePoke).toBeInTheDocument();
+  });
+});
