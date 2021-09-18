310a870b68ede3eea400d5106e86bf79af83581b
Lucas Santos <lucassantos_dev@outlook.com>
2021-09-18 19:56:36 -0300

    req pokedex 1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente Pokedex', () => {
+  it('este se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const msgEncounteredPokes = screen.getByText('Encountered pokémons');
+    expect(msgEncounteredPokes).toBeInTheDocument();
+  });
+});
