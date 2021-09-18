4752918ea07ca95a00901a29009e3018b5fd4753
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-09-18 14:00:02 -0300

    teste pokedex concluido 2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
+import pokemons from '../data';
 import App from '../App';
 
 describe('Testa se contém um heading h2 c/ texto Encountered pokémons', () => {
@@ -16,14 +17,15 @@ describe('Testa se contém um heading h2 c/ texto Encountered pokémons', () => 
 });
 
 describe('Testa se é exibido o próximo Pokemon da lista', () => {
-  test('Verifica se exibe o proximo pokemon quando clicado no botao', () => {
+  beforeEach(() => {
     render(<BrowserRouter><App /></BrowserRouter>);
+  });
+  test('Verifica o proximo pokemon quando clicado no botao', () => {
     const btnNextPokemon = screen.getByText(/Próximo pokémon/i);
     expect(btnNextPokemon).toBeInTheDocument();
   });
 
-  test('Testa se é mostrado apenas um Pokémon por vez', () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+  test('Verifica se é mostrado apenas um Pokémon por vez', () => {
     userEvent.click(screen.getByText(/Próximo pokémon/i));
 
     const charmander = screen.getByText('Charmander');
@@ -34,36 +36,47 @@ describe('Testa se é exibido o próximo Pokemon da lista', () => {
     const caterpie = screen.getByText('Caterpie');
     expect(caterpie).toBeInTheDocument();
   });
+
+  test('Verifica se aparace o primeiro pokemon se estiver no último da lista', () => {
+    const clicks = pokemons.length;
+    for (let click = 0; click < clicks; click += 1) {
+      userEvent.click(screen.getByText(/Próximo pokémon/i));
+    }
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
 });
 
-test('Testa se a Pokedex tem os botões de filtro', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
-  const types = [
-    'Electric',
-    'Fire',
-    'Bug',
-    'Poison',
-    'Psychic',
-    'Normal',
-    'Dragon',
-  ];
+describe('', () => {
+  beforeEach(() => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+  });
 
-  const buttons = screen.getAllByTestId('pokemon-type-button');
-  const buttonList = buttons.map((button) => button.innerHTML);
-  expect(buttonList).toEqual(types);
-});
+  test('Testa se a Pokedex tem os botões de filtro', () => {
+    const types = [
+      'Electric',
+      'Fire',
+      'Bug',
+      'Poison',
+      'Psychic',
+      'Normal',
+      'Dragon',
+    ];
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    const buttonList = buttons.map((button) => button.innerHTML);
+    expect(buttonList).toEqual(types);
+  });
 
-test('A partir da seleção de um botão de tipo, retorna o pokemon do tipo', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
-  userEvent.click(screen.getByText(/Próximo pokémon/i));
-  const type = screen.getByTestId('pokemon-type');
-  const btnAll = screen.getByText('All');
-  expect('Fire').toEqual(type.innerHTML);
-  expect(btnAll).toBeInTheDocument();
-});
+  test('A partir da seleção de um botão de tipo, retorna o pokemon do tipo', () => {
+    userEvent.click(screen.getByText(/Próximo pokémon/i));
+    const type = screen.getByTestId('pokemon-type');
+    const btnAll = screen.getByText('All');
+    expect('Fire').toEqual(type.innerHTML);
+    expect(btnAll).toBeInTheDocument();
+  });
 
-test('Verifica que ao carregar a pagina o filtro é All', () => {
-  render(<BrowserRouter><App /></BrowserRouter>);
-  const pikachu = screen.getByText('Pikachu');
-  expect(pikachu).toBeInTheDocument();
+  test('Verifica que ao carregar a pagina o filtro é All', () => {
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
 });
