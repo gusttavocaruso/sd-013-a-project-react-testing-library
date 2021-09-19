2ef0593f428faa1d352727eaaebf2879f38e3114
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-09-18 21:09:45 -0300

    concluido requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
+import pokemons from '../data';
 /* import { Pokemon } from '../components';
 import pokemons from '../data'; */
 
@@ -13,7 +14,7 @@ describe('Testa as informações detalhadas do Pokémon selecionado', () => {
     const link = screen.getByRole('link', { name: details });
 
     userEvent.click(link);
-    const name = screen.getByText('Pikachu');
+    const name = screen.getByRole('heading', { name: 'Pikachu Details', level: 2 });
 
     expect(name).toBeInTheDocument();
     expect(link).not.toBeInTheDocument();
@@ -33,21 +34,27 @@ describe('Teste se existe na página uma seção com as localizações do pokém
       level: 2, name: /Game Locations of Pikachu/i });
     expect(heading).toBeInTheDocument();
 
-    const locationForest = screen.getByText('Kanto Viridian Forest');
-    const locationPlant = screen.getByText('Kanto Power Plant');
-
-    expect(locationForest).toBeInTheDocument();
-    expect(locationPlant).toBeInTheDocument();
-
+    // resolução copiada do notion da turma
+    // https://www.notion.so/Gabaritos-e-Solu-es-de-Exerc-cios-de-Programa-o-7651c81ce6624473a1d66ffb0a2bd053?p=332f9886963a4491bf58da84a11f1a6b
     const images = screen.getAllByAltText('Pikachu location');
-    images.map((image) => expect(image).toHaveAttribute('src'));
-    images.map((image) => expect('Pikachu location').toEqual(image.alt));
+    images.forEach((image, index) => {
+      expect(image).toHaveAttribute('src', pokemons[0].foundAt[index].map);
+    });
   });
 });
 
-describe('', () => {
-  test('test', () => {
+describe('Testa se o usuário pode favoritar um pokémon na página de detalhes.', () => {
+  test('A página deve exibir um checkbox que permite favoritar o Pokémon', () => {
     render(<BrowserRouter><App /></BrowserRouter>);
-   
+    const txtFavorite = 'Pokémon favoritado?';
+    const checkbox = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    expect(checkbox).toBeInTheDocument();
+
+    userEvent.click(screen.getByText(txtFavorite));
+    const favorite = screen.getByRole('img', { name: /marked as favorite/i });
+    expect(favorite).toBeInTheDocument();
+
+    userEvent.click(screen.getByText(txtFavorite));
+    expect(favorite).not.toBeInTheDocument();
   });
 });
