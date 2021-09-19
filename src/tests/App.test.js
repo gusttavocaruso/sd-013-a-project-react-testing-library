81bdf1b825ff80e9d01402b70a964d484754b5c8
FernandoLimas <nando_filipe@hotmail.com>
2021-09-19 16:04:17 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRoute from './renderWithRoute';
+
+describe('Teste do componente App', () => {
+  renderWithRoute(<App />);
+  it('O primeiro link deve possuir o text Home', () => {
+    const home = screen.getByText(/Home/i);
+    expect(home).toBeInTheDocument('');
+  });
+  it('Testa o about', () => {
+    renderWithRoute(<App />);
+    const about = screen.getByText(/About/i);
+    expect(about).toBeInTheDocument('');
+  });
+  it('', () => {
+    renderWithRoute(<App />);
+    const FavPokemons = screen.getByText(/Favorite Pokémons/i);
+    expect(FavPokemons).toBeInTheDocument('');
+  });
+});
