22c3ad50cb54d2e59404b2f6d3b0cb8b8e502da3
Matheus Macedo <matheus.castro.macedo.xi@gmail.com>
2021-09-19 16:03:36 -0300

    Requisito 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Requisito 3 - Verificando fluxo do componente <FavoritePokemons />', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  it('Veriifica se a mensagem "No favorite pokemon found" é renderizada', () => {
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Verifica se todos os cards de pokémons favoritados são exibidos', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByText('Poison'));
+    fireEvent.click(screen.getByText('More details'));
+    fireEvent.click(screen.getByText('Pokémon favoritado?'));
+
+    fireEvent.click(screen.getByText('Home'));
+
+    fireEvent.click(screen.getByText('Dragon'));
+    fireEvent.click(screen.getByText('More details'));
+    fireEvent.click(screen.getByText('Pokémon favoritado?'));
+
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+
+    const ekans = screen.getByText('Ekans');
+    expect(ekans).toBeInTheDocument();
+    const dragonair = screen.getByText('Dragonair');
+    expect(dragonair).toBeInTheDocument();
+  });
+});
