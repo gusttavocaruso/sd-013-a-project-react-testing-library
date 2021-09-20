f8b289e785ef7332f7ff8de391cf9ddbef2b75f6
Matheus Macedo <matheus.castro.macedo.xi@gmail.com>
2021-09-20 14:45:00 -0300

    Toques finais e referências

--- src/index.js
+++ src/index.js
@@ -15,4 +15,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+// Projeto realizado com a ajuda do Notion da tribo 13A https://www.notion.so/Gabaritos-e-Solu-es-de-Exerc-cios-de-Programa-o-7651c81ce6624473a1d66ffb0a2bd053?p=332f9886963a4491bf58da84a11f1a6b 
 serviceWorker.unregister();
