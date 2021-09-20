31efcade8c68bc61ca668af063c6ebb0b4780587
FernandoLimas <nando_filipe@hotmail.com>
2021-09-19 22:50:30 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRoute from './renderWithRoute';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testa o componente favorite pokémons', () => {
+  test('Testa se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRoute(<FavoritePokemons />);
+    const notFoundFavorite = screen.getByText('No favorite pokemon found');
+    expect(notFoundFavorite).toBeInTheDocument('');
+  });
+  test('Testa se é exibido todos os cards', () => {
+    renderWithRoute(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'More details',
+    }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toBeInTheDocument('');
+  });
+});
