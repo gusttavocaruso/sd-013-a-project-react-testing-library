e0796f458404b699067ff0737f5fa020d1f70751
FernandoLimas <nando_filipe@hotmail.com>
2021-09-20 20:37:06 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRoute from './renderWithRoute';
+
+describe('Teste o componente notFound', () => {
+  test('Página com heading h2 com o texto Page requested not found', () => {
+    renderWithRoute(<NotFound />);
+    const h2NotFound = screen.getByText('Page requested not found');
+    expect(h2NotFound).toBeInTheDocument('');
+  });
+  test('Testa se a página mostra a imagem', () => {
+    renderWithRoute(<NotFound />);
+    const pageNotFound = screen.getAllByRole('img')[1];
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(pageNotFound).toBeInTheDocument('');
+    expect(pageNotFound.src).toContain(src);
+  });
+});
