50ed464c8588a61d126e0342b1a60408a6648cb6
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2021-09-21 19:27:09 -0300

    Final

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
-import { FavoritePokemons } from '../components';
+import { screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
 
-describe('Requisito 3 - Verificando fluxo do componente <FavoritePokemons />', () => {
-  renderWithRouter(<FavoritePokemons />);
+describe('Testa a página FavoritesPokemons', () => {
+  test('Testa se renderiza "No favorite pokemon found" se não existir pokemons', () => {
+    renderWithRouter(<FavoritePokemons />);
 
-  it('Veriifica se a mensagem "No favorite pokemon found" é renderizada', () => {
-    const notFound = screen.getByText('No favorite pokemon found');
-    expect(notFound).toBeInTheDocument();
+    const msg = screen.getByText('No favorite pokemon found');
+    expect(msg).toBeInTheDocument();
+
+    cleanup();
   });
 
-  it('Verifica se todos os cards de pokémons favoritados são exibidos', () => {
+  test('Testa se existem pokémons favoritos', () => {
     renderWithRouter(<App />);
 
-    const details = 'More details';
-    const favoritar = 'Pokémon favoritado?';
+    userEvent.click(screen.getByText('More details'));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
 
-    // Selecionei a categoria 'Dragon', e fui em mais detalhes no pokemon que apareceu, que no caso foi o Dragonair e ...
-    fireEvent.click(screen.getByText('Dragon'));
-    fireEvent.click(screen.getByText(details));
-    // ... cliquei em favoritar
-    fireEvent.click(screen.getByText(favoritar));
-
-    // Voltei para 'Home' e selecionei a categoria 'Normal', e fui em mais detalhes no pokemon que apareceu, que no caso foi o Snorlax e ...
-    fireEvent.click(screen.getByText('Home'));
-    fireEvent.click(screen.getByText('Normal'));
-    fireEvent.click(screen.getByText(details));
-    // ... cliquei em favoritar
-    fireEvent.click(screen.getByText(favoritar));
-
-    // Cliquei no botão para ver os favoritos
-    fireEvent.click(screen.getByText('Favorite Pokémons'));
-
-    // Verifico que os 2 pokemons favoritados estão em favoritos
-    const fav1 = screen.getByText('Snorlax');
-    expect(fav1).toBeInTheDocument();
-    const fav2 = screen.getByText('Dragonair');
-    expect(fav2).toBeInTheDocument();
-
-    // Volto na 'Home', vou na categoria 'Normal' vou nos detalhes do pokemon que apareceu (Snorlax) e clico em favoritar novamente, desfavoritando
-    fireEvent.click(screen.getByText('Home'));
-    fireEvent.click(screen.getByText('Normal'));
-    fireEvent.click(screen.getByText(details));
-    fireEvent.click(screen.getByText(favoritar));
-
-    // Vou nos favoritos
-    fireEvent.click(screen.getByText('Favorite Pokémons'));
-
-    // Verifico que 'Snrolax' saiu dos favoritos;
-    const newFav1 = screen.getByText('Dragonair');
-    expect(newFav1).toBeInTheDocument();
-    const remove = screen.getByText('Snorlax');
-    expect(remove).not.toBeInTheDocument();
+    expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
   });
 });
