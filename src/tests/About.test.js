09a22515f0d9e8ab2703733f7565c0ddba190fae
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-09-21 11:41:20 -0300

    Adiciona requisito 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import About from '../components/About';
+
+describe('About', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+    const texth2 = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(texth2).toBeInTheDocument();
+  });
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const Paragrafo1 = (
+      'This application simulates a Pokédex, '
+    + 'a digital encyclopedia containing all Pokémons'
+    );
+    expect(screen.getByText(Paragrafo1)).toBeInTheDocument();
+
+    const Paragrafo2 = (
+      'One can filter Pokémons by type, and see more details for each one of them'
+    );
+    expect(screen.getByText(Paragrafo2)).toBeInTheDocument();
+  });
+  test('Testa se a página contém a imagem de uma Pokédex.', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+    const imgPokedex = screen.getByRole('img');
+    expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Favorite Pokemon', () => {
+  test('Testa se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+  test('Testa se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const MORE_DETAILS = 'More details';
+    const FAVORITE = 'Pokémon favoritado?';
+    const PAGE_FAVORITES = 'Favorite Pokémons';
+    const PAGE_HOME = 'Home';
+
+    fireEvent.click(screen.getByText('Psychic '));
+    fireEvent.click(screen.getByText(MORE_DETAILS));
+    fireEvent.click(screen.getByText(FAVORITE));
+    fireEvent.click(screen.getByText(PAGE_HOME));
+
+    fireEvent.click(screen.getByText('Normal'));
+    fireEvent.click(screen.getByText(MORE_DETAILS));
+    fireEvent.click(screen.getByText(FAVORITE));
+    fireEvent.click(screen.getByText(PAGE_HOME));
+
+    fireEvent.click(screen.getByText(PAGE_FAVORITES));
+
+    const pokemonsFavorites = screen.getAllByText(/More details/i);
+    expect(pokemonsFavorites).toHaveLength(2);
+  });
+});
