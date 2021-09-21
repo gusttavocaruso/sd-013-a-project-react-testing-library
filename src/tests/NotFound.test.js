364d74ae5068660f40a6e728bd92d7a9f816e6a2
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-09-21 11:56:13 -0300

    Adiciona requisito 3 e 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound', () => {
+  test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const texth2 = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(texth2).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    // não consegui achar or getByRole('img');
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
