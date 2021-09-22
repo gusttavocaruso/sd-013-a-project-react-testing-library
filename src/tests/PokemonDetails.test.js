aeb4cf07246bc44af7c918918242d35f2498585a
gabrielpriss <gabrielpriss@hotmail.com>
2021-09-22 00:10:27 -0300

    fix PokemonDetails test

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,42 +1,69 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
+import pokemons from '../data';
 
-describe('Testando o Componente FavoritePokemons:', () => {
-  test('Teste se é exibida a mensagem "No favorite pokemon found"', () => {
-    renderWithRouter(<FavoritePokemons />);
-    expect(
-      screen.getByText('No favorite pokemon found'),
-    ).toBeDefined();
-  });
-
-  test('Teste se todos os cards de pokémons favoritados são exibidos', () => {
+describe('Testa a pokemonDetails.js', () => {
+  const { name, foundAt, summary } = pokemons[0];
+  beforeEach(() => {
     renderWithRouter(<App />);
-    const moreDetails = 'More details';
-    const favoritar = 'Pokémon favoritado?';
-    const THREE = 3;
+  });
+  test('Testa se as infos detalhadas são mostradas', () => {
+    const details = screen.getByRole('link', { name: /more details/i });
+    fireEvent.click(details);
 
-    fireEvent.click(screen.getByText('Dragon'));
-    fireEvent.click(screen.getByText(moreDetails));
-    fireEvent.click(screen.getByText(favoritar));
+    const headingDetails = screen.getByRole('heading', { name: `${name} Details` });
+    expect(headingDetails).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('Home'));
+    expect(details).not.toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('Normal'));
-    fireEvent.click(screen.getByText(moreDetails));
-    fireEvent.click(screen.getByText(favoritar));
+    const headingSummary = screen.getByRole('heading', { name: /summary/i });
+    expect(headingSummary).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('Home'));
+    expect(screen.getByText(summary)).toBeInTheDocument();
+  });
+  test('Testa se existem os mapas na página', () => {
+    const locations = ['Kanto Viridian Forest', 'Kanto Power Plant'];
+    const details = screen.getByRole('link', { name: /more details/i });
 
-    fireEvent.click(screen.getByText('Fire'));
-    fireEvent.click(screen.getByText(moreDetails));
-    fireEvent.click(screen.getByText(favoritar));
+    fireEvent.click(details);
 
-    fireEvent.click(screen.getByText('Favorite Pokémons'));
+    const headingLocation = screen.getByRole('heading',
+      { name: `Game Locations of ${name}` });
+    expect(headingLocation).toBeInTheDocument();
 
-    const favs = screen.getAllByText(/Average weight/i);
-    expect(favs).toHaveLength(THREE);
+    foundAt.forEach((item, index) => {
+      const imgs = screen.getAllByRole('img',
+        { name: `${name} location` });
+
+      expect(imgs[index]).toHaveAttribute('src', item.map);
+    });
+    locations.forEach((item, index) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+      const imgs = screen.getAllByRole('img',
+        { name: `${name} location` });
+
+      expect(imgs[index]).toHaveAttribute('src', foundAt[index].map);
+    });
+  });
+  test('Testa se um usuário pode favoritar através dos detalhes', () => {
+    const details = screen.getByRole('link', { name: /more details/i });
+    fireEvent.click(details);
+
+    const checkBox = screen.getByRole('checkbox', 'Pokémon favoritado?');
+    expect(checkBox).toBeDefined();
+    fireEvent.click(checkBox);
+
+    let favorited = screen.getByRole('img', { name: `${name} is marked as favorite` });
+    expect(favorited).toHaveAttribute('src', '/star-icon.svg');
+    fireEvent.click(checkBox);
+
+    favorited = screen.queryByRole('img', { name: `${name} is marked as favorite` });
+    expect(favorited).not.toBeInTheDocument();
+
+    expect(screen.getByText(/Pokémon favoritado?/i)).toBeInTheDocument();
   });
 });
+
+// consulta no repositório : https://github.com/tryber/sd-013-a-project-react-testing-library/blob/1f10a253b67383dc358790eb99fc4e5bb26d1e94/src/tests/PokemonDetails.test.js
