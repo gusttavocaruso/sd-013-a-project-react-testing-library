34bcb70d2b927bf9504dececc06d820f0dfb3932
Yuri <yurifranco18@live.com>
2021-09-22 04:34:29 -0300

    foi feito req. 3, passou no stryker mas nao fiz o segundo teste

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import historyComponent from '../components/historyComponent';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Verifica se aparece a mensagem quando não há pokemons favoritos', () => {
+    historyComponent(<FavoritePokemons pokemons={ [] } />);
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
+
+// Não fiz o teste do se é exibido todos os cards de pokémons favoritados.
