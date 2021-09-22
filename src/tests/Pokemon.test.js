0abf7c7fca7e273084ac101b622dc2db3254d3a9
Rodrigo Pova <rodrigopova94@gmail.com>
2021-09-22 02:28:01 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { Pokemon } from '../components';
+import pokemons from '../data';
+// Desenvolvido com ajuda de Ygor Maia
+describe('6- Testa o componente Pokemon.js', () => {
+  const {
+    id,
+    name,
+    type,
+    averageWeight: { value, measurementUnit },
+    image,
+  } = pokemons[0];
+
+  test('É renderizado um card com as informações de determinado pokémon.', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite={ false } />);
+
+    const pkmnName = screen.getByTestId('pokemon-name');
+    expect(pkmnName).toHaveTextContent(name);
+
+    const pkmnType = screen.getByTestId('pokemon-type');
+    expect(pkmnType).toHaveTextContent(type);
+
+    const pkmnWeight = screen.getByTestId('pokemon-weight');
+    expect(pkmnWeight).toHaveTextContent(
+      `Average weight: ${value} ${measurementUnit}`,
+    );
+
+    const pkmnImg = screen.getByAltText(`${name} sprite`);
+    expect(pkmnImg).toHaveAttribute('src', image);
+  });
+
+  test('O card contém um link de navegação para exibir detalhes', () => {
+    const { history } = renderWithRouter(
+      <Pokemon pokemon={ pokemons[0] } isFavorite={ false } />,
+    );
+    const linkToDetails = screen.getByRole('link', { name: /More Details/i });
+    expect(linkToDetails).toBeInTheDocument();
+    userEvent.click(linkToDetails);
+    expect(history.location.pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test('Existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+
+    const favoritePokemon = screen.getByRole('img', {
+      name: `${name} is marked as favorite`,
+    });
+    expect(favoritePokemon.src).toBe('http://localhost/star-icon.svg');
+  });
+});
