26d0a1f3920725e1541b5e117c083ff228fe1b19
gabrielpriss <gabrielpriss@hotmail.com>
2021-09-21 23:56:04 -0300

    tests for NouFound and PokemonDetails

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import NotFound from '../components';
-import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from './renderWithRouter';
 
-describe('Testa componente "NotFound"', () => {
-  it('Testa se a página tem o texto "Page requested not found"', () => {
-    renderWithRouter(<NotFound />);
-    const heading = screen.getByRole('heading', {
+describe('Testa o componente NotFound', () => {
+  test('Testa se NotFound tem um heading h2 com o texto específico', () => {
+    RenderWithRouter(<NotFound />);
+
+    const getHeading = screen.getByRole('heading', {
       name: /Page requested not found/i,
       level: 2,
     });
-    expect(heading).toBeInTheDocument();
+    expect(getHeading).toBeInTheDocument();
   });
 
-  it('Testa se a página tem a imagem', () => {
-    renderWithRouter(<NotFound />);
-    const img = screen.getByAltText(
-      'Pikachu crying because the page requested was not found',
-    );
+  test('Testa se a página contém uma imagem', () => {
+    RenderWithRouter(<NotFound />);
+
+    const getGif = screen.getAllByRole('img');
     const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-    expect(img[1].src).toBe(src);
+
+    expect(getGif[1].src).toBe(src);
   });
 });
+
+// feito com consulta ao repositório : https://github.com/tryber/sd-013-a-project-react-testing-library/blob/f38b7d9fd1a21c803077a1221844aeb6051f49dd/src/tests/NotFound.test.js
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando o Componente FavoritePokemons:', () => {
+  test('Teste se é exibida a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(
+      screen.getByText('No favorite pokemon found'),
+    ).toBeDefined();
+  });
+
+  test('Teste se todos os cards de pokémons favoritados são exibidos', () => {
+    renderWithRouter(<App />);
+    const moreDetails = 'More details';
+    const favoritar = 'Pokémon favoritado?';
+    const THREE = 3;
+
+    fireEvent.click(screen.getByText('Dragon'));
+    fireEvent.click(screen.getByText(moreDetails));
+    fireEvent.click(screen.getByText(favoritar));
+
+    fireEvent.click(screen.getByText('Home'));
+
+    fireEvent.click(screen.getByText('Normal'));
+    fireEvent.click(screen.getByText(moreDetails));
+    fireEvent.click(screen.getByText(favoritar));
+
+    fireEvent.click(screen.getByText('Home'));
+
+    fireEvent.click(screen.getByText('Fire'));
+    fireEvent.click(screen.getByText(moreDetails));
+    fireEvent.click(screen.getByText(favoritar));
+
+    fireEvent.click(screen.getByText('Favorite Pokémons'));
+
+    const favs = screen.getAllByText(/Average weight/i);
+    expect(favs).toHaveLength(THREE);
+  });
+});
