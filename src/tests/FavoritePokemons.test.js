a684f2125b9216e135bb715a1b8505427a857cda
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-09-23 12:48:02 -0300

    Inicia projeto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('FavoritePokémon.js Test', () => {
+  test('Aplicação avisa se não há pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFav = screen.getByText('No favorite pokemon found');
+
+    expect(noFav).toBeInTheDocument();
+  });
+
+  test('Aplicação exibe pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
